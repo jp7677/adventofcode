a149d8f66d6122d871944782d38c8661a37d410b
Jens Peters <jp7677@gmail.com>
2020-12-04 18:31:24 +0100

    Switch to TDD.

--- day1.cpp
+++ day1.cpp
@@ -1,10 +1,12 @@
+
 #include<iostream>
 #include<vector>
 
+#include "./catch_amalgamated.hpp"
+
 using namespace std;
 
-int main()
-{
+int day1() {
    cout << "Day 1 from https://adventofcode.com/2020/day/1\n";
 
    vector<int> list { 1721, 979, 366, 299, 675, 1456 };
@@ -12,9 +14,14 @@ int main()
    for (auto const element1 : list)
       for (auto const element2 : list)
          if (element1 + element2 == 2020) {
-            cout << "Result " << element1 * element2 << "\n";
-            return 0;
+            auto result = element1 * element2;
+            cout << "Result: " << result << "\n";
+            return result;
          }
 
    return 0;
+}
+
+TEST_CASE("Day 1") {
+   REQUIRE( day1() == 514579);
 }
