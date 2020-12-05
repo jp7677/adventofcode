f0d9e85d56219a035f7d79f51001db8d9c73d43d
Jens Peters <jp7677@gmail.com>
2020-12-05 10:17:35 +0100

    Compile main separately.

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-g++ inc/catch_amalgamated.cpp -c
+g++ inc/catch_amalgamated.cpp src/main.cpp -c
 g++ catch_amalgamated.o \
-    src/main.cpp \
+    main.o \
     src/day0.cpp \
     src/day1.cpp \
     -o adventofcode
