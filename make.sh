05b2eaaa3adafce97d76be31d27cae2b788806ae
Jens Peters <jp7677@gmail.com>
2020-12-05 19:16:00 +0100

    Use wildcard.

--- make.sh
+++ make.sh
@@ -2,7 +2,5 @@
 g++ inc/catch_amalgamated.cpp src/main.cpp -c
 g++ catch_amalgamated.o \
     main.o \
-    src/day0.cpp \
-    src/day1.cpp \
-    src/day2.cpp \
+    src/day*.cpp \
     -o adventofcode
