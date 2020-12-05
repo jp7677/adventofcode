327f766891907653010ade05947a89f113f0d2ec
Jens Peters <jp7677@gmail.com>
2020-12-05 12:02:46 +0100

    Solve Day 1, part 1 and 2.

--- src/day1.cpp
+++ src/day1.cpp
@@ -1,22 +1,55 @@
 #include<iostream>
+#include <fstream>
 #include<vector>
 #include "../inc/catch_amalgamated.hpp"
 
 using namespace std;
 
-int day1() {
-   cout << "Day 1 from https://adventofcode.com/2020/day/1" << endl;
+vector<int> loadEntriesFromFile() {
+   vector<int> entries;
+   fstream inputStream;
+   
+   inputStream.open("day1-input.txt",ios::in);
+   if (!inputStream.is_open())
+      return entries;
 
-   vector<int> list { 1721, 979, 366, 299, 675, 1456 };
+   string line;
+   while(getline(inputStream, line))
+      entries.push_back(stoi(line));
 
-   for (auto const& element1 : list)
-      for (auto const& element2 : list)
+   inputStream.close();
+   return entries;
+}
+
+int day1Part1() {
+   cout << "Day 1 - Part 1 from https://adventofcode.com/2020/day/1" << endl;
+
+   auto entries = loadEntriesFromFile();
+   for (auto const& element1 : entries)
+      for (auto const& element2 : entries)
          if (element1 + element2 == 2020)
             return element1 * element2;
 
    return 0;
 }
 
-TEST_CASE("Day 1") {
-   REQUIRE( day1() == 514579);
+TEST_CASE("Day 1 - Part 1") {
+   REQUIRE( day1Part1() == 964875);
+}
+
+int day1Part2() {
+   cout << "Day 1 - Part 2 from https://adventofcode.com/2020/day/1" << endl;
+
+   auto entries = loadEntriesFromFile();
+   for (auto const& element1 : entries)
+      for (auto const& element2 : entries)
+         for (auto const& element3 : entries)
+            if (element1 + element2 + element3 == 2020)
+               return element1 * element2 * element3;
+
+   return 0;
+}
+
+TEST_CASE("Day 1 - Part 2") {
+   REQUIRE( day1Part2() == 158661360);
 }
