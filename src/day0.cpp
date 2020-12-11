20721ed4d51cdf50ad751b7ea1e58c5d7b52c150
Jens Peters <jp7677@gmail.com>
2020-12-11 20:31:02 +0100

    Improve template for other days.

--- src/day0.cpp
+++ src/day0.cpp
@@ -2,10 +2,18 @@
 
 using namespace std;
 
-string helloWorld() {
-   return "Hello World!";
+int day0Part1() {
+   return 1;
 }
 
-TEST_CASE("Hello World") {
-   REQUIRE( helloWorld() == "Hello World!");
+TEST_CASE("Day 0 - Part 1") {
+   REQUIRE( day0Part1() == 1);
+}
+
+int day0Part2() {
+   return 2;
+}
+
+TEST_CASE("Day 0 - Part 2") {
+   REQUIRE( day0Part2() == 2);
 }
