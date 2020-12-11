b3b490b015f615e505e0037a79dd8aad127aba9d
Jens Peters <jp7677@gmail.com>
2020-12-11 21:03:11 +0100

    Add day 3 part 2.

--- src/day3.cpp
+++ src/day3.cpp
@@ -5,8 +5,8 @@
 
 using namespace std;
 
-vector<vector<char>> loadGridFromFile() {
-   vector<vector<char>> grid;
+vector<string> loadGridFromFile() {
+   vector<string> grid;
    fstream inputStream;
    
    inputStream.open("day3-input.txt",ios::in);
@@ -14,24 +14,16 @@ vector<vector<char>> loadGridFromFile() {
       throw("file is not open");
 
    string line;
-   while(getline(inputStream, line)) {
-      vector<char> gridLine;
-      for (auto const& character : line)
-         gridLine.push_back(character);
-
-      grid.push_back(gridLine);
-   }
+   while(getline(inputStream, line))
+      grid.push_back(line);
 
    inputStream.close();
    return grid;
 }
 
-int day3Part1() {
-   cout << "Day 3 - Part 1 from https://adventofcode.com/2020/day/3" << endl;
-
-   auto grid = loadGridFromFile();
-   auto width = grid.at(0).size();
-   auto heigth = grid.size();
+ulong runSlope(vector<string>* grid, int right, int down) {
+   auto width = grid->at(0).size();
+   auto heigth = grid->size();
    
    auto hits = 0U;
    auto posX = 0U;
@@ -41,24 +33,38 @@ int day3Part1() {
       if (posX >= width)
          posX = posX - width;
 
-      if (grid.at(posY).at(posX) == '#')
+      if (grid->at(posY).at(posX) == '#')
          hits++;
 
-      posY++;
-      posX = posX + 3;
+      posY = posY + down;
+      posX = posX + right;
    }
    
    return hits;
+}
+
+int day3Part1() {
+   cout << "Day 3 - Part 1 from https://adventofcode.com/2020/day/3" << endl;
+
+   auto grid = loadGridFromFile();
+   return runSlope(&grid, 3, 1);
 }
 
 TEST_CASE("Day 3 - Part 1") {
    REQUIRE( day3Part1() == 265);
 }
 
-int day3Part2() {
-   return 2;
+ulong day3Part2() {
+   cout << "Day 3 - Part 2 from https://adventofcode.com/2020/day/3" << endl;
+
+   auto grid = loadGridFromFile();
+   return runSlope(&grid, 1, 1) *
+      runSlope(&grid, 3, 1) *
+      runSlope(&grid, 5, 1) *
+      runSlope(&grid, 7, 1) *
+      runSlope(&grid, 1, 2);
 }
 
 TEST_CASE("Day 3 - Part 2") {
-   REQUIRE( day3Part2() == 2);
+   REQUIRE( day3Part2() == 3154761400);
 }
