51cbb6ed528a8937aded0443e90399534b826e5d
Jens Peters <jp7677@gmail.com>
2020-12-14 10:00:24 +0100

    Compile with C++17.

--- make.sh
+++ make.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-g++ inc/catch_amalgamated.cpp src/main.cpp -c
-g++ catch_amalgamated.o \
+g++ -std=c++17 inc/catch_amalgamated.cpp src/main.cpp -c
+g++ -std=c++17 \
+    catch_amalgamated.o \
     main.o \
     src/day*.cpp \
     -o adventofcode
