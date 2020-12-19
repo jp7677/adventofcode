6ba9eccbd91b81dd7a3aabf0d0c0eeeb89b49cce
Jens Peters <jp7677@gmail.com>
2020-12-19 10:44:48 +0100

    Treat warnings as errors. Exit scripts on failure.

--- make.sh
+++ make.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-g++ -std=c++17 inc/catch_amalgamated.cpp src/main.cpp -c
-g++ -std=c++17 \
+set -e
+
+g++ -std=c++17 -Werror inc/catch_amalgamated.cpp src/main.cpp -c
+g++ -std=c++17 -Werror \
     catch_amalgamated.o \
     main.o \
     src/day*.cpp \
--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-g++ -std=c++17 \
+set -e
+
+g++ -std=c++17 -Werror \
     catch_amalgamated.o \
     main.o \
     src/day*.cpp \
