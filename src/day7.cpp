2df50af8683f2c3674e90b9d3195f9b847f9f012
Jens Peters <jp7677@gmail.com>
2020-12-19 20:47:36 +0100

    Reuse pre-processing.

--- src/day7.cpp
+++ src/day7.cpp
@@ -6,18 +6,45 @@
 using namespace std;
 
 namespace day7 {
-   const auto containKeyword = string(" contain ");
+   unordered_map<string, vector<pair<uint, string>>> loadRules() {
+      auto rulesInput = util::loadInputFile("day7-input.txt");
 
-   bool holdsOwnBag(const unordered_map<string, vector<string>>* rules, const string bag) {
+      unordered_map<string, vector<pair<uint, string>>> rules;
+      transform(rulesInput.begin(), rulesInput.end(), inserter(rules, rules.end()),
+         [](const auto& ruleLine) {
+            auto elements = util::split(ruleLine, ' ');
+            auto key = elements.at(0) + "-" + elements.at(1);
+
+            const auto containKeyword = string(" contain ");
+            auto contains = ruleLine.substr(ruleLine.find(containKeyword) + containKeyword.size());
+            auto splitted = util::split(contains, ',');
+
+            vector<pair<uint, string>> values;
+            transform(splitted.begin(), splitted.end(), inserter(values, values.end()),
+               [](const auto& value){
+                  auto splitted = util::split(value, ' ');
+
+                  return make_pair(
+                     splitted.at(0) == "no" ? 0 : stoi(splitted.at(0)),
+                     splitted.at(splitted.size() - 3) + "-" + splitted.at(splitted.size() - 2));
+               });
+
+            return make_pair(key, values);
+      });
+      
+      return rules;
+   }
+
+   bool containsShinyGoldBag(const unordered_map<string, vector<pair<uint, string>>>* rules, const string bag) {
       auto it = rules->find(bag);
       if (it == rules->end())
          return false;
 
-      for (const auto& value : it->second)
-         if (value == "shiny-gold")
+      for (const auto& luagage : it->second)
+         if (luagage.second == "shiny-gold")
             return true;
          else
-            if (holdsOwnBag(rules, value))
+            if (containsShinyGoldBag(rules, luagage.second))
                return true;
 
       return false;
@@ -26,29 +53,11 @@ namespace day7 {
    uint day7Part1() {
       cout << "Day 7 - Part 1 from https://adventofcode.com/2020/day/7" << endl;
 
-      auto rulesInput = util::loadInputFile("day7-input.txt");
-
-      unordered_map<string, vector<string>> rules;
-      transform(rulesInput.begin(), rulesInput.end(), inserter(rules, rules.end()),
-         [](const auto& ruleLine) {
-            auto elements = util::split(ruleLine, ' ');
-            auto key = elements.at(0) + "-" + elements.at(1);
-
-            auto contains = ruleLine.substr(ruleLine.find(containKeyword) + containKeyword.size());
-            auto values = util::split(contains, ',');
-
-            transform(values.begin(), values.end(), values.begin(),
-               [](const auto& value){
-                  auto splitted = util::split(value, ' ');
-                  return splitted.at(splitted.size() - 3) + "-" + splitted.at(splitted.size() - 2);
-               });
-
-            return make_pair(key, values);
-      });
+      auto rules = loadRules();
 
       return count_if(rules.begin(), rules.end(),
          [rules](const auto& rule) {
-            return holdsOwnBag(&rules, rule.first);
+            return containsShinyGoldBag(&rules, rule.first);
          });
    }
 
@@ -56,49 +65,21 @@ namespace day7 {
       REQUIRE(day7Part1() == 268);
    }
 
-   struct Luggage {
-      uint count;
-      string bag;
-   };
-
-    uint countBags(const unordered_map<string, vector<Luggage>>* rules, const string bag) {
+   uint countBags(const unordered_map<string, vector<pair<uint, string>>>* rules, const string bag) {
       auto it = rules->find(bag);
       if (it == rules->end())
          return 0;
      
       return accumulate(it->second.begin(), it->second.end(), 0U,
          [rules](const auto sum, auto& luagage) {
-            return sum + luagage.count + (luagage.count * countBags(rules, luagage.bag));
+            return sum + luagage.first + (luagage.first * countBags(rules, luagage.second));
          });
    }
 
    uint day7Part2() {
       cout << "Day 7 - Part 2 from https://adventofcode.com/2020/day/7#part2" << endl;
 
-      auto rulesInput = util::loadInputFile("day7-input.txt");
-
-      unordered_map<string, vector<Luggage>> rules;
-      transform(rulesInput.begin(), rulesInput.end(), inserter(rules, rules.end()),
-         [](const auto& ruleLine) {
-            auto elements = util::split(ruleLine, ' ');
-            auto key = elements.at(0) + "-" + elements.at(1);
-
-            auto contains = ruleLine.substr(ruleLine.find(containKeyword) + containKeyword.size());
-            auto splitted = util::split(contains, ',');
-
-            vector<Luggage> values;
-            transform(splitted.begin(), splitted.end(), inserter(values, values.end()),
-               [](const auto& value){
-                  auto splitted = util::split(value, ' ');
-
-                  struct Luggage luggage;
-                  luggage.bag = splitted.at(splitted.size() - 3) + "-" + splitted.at(splitted.size() - 2);
-                  luggage.count = splitted.at(0) == "no" ? 0 : stoi(splitted.at(0));
-                  return luggage;
-               });
-
-            return make_pair(key, values);
-      });
+      auto rules = loadRules();
 
       return countBags(&rules, "shiny-gold");
    }
