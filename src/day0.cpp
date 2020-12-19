fd393ebcc91e0003b9c3fb7b13cb22d6adee29e0
Jens Peters <jp7677@gmail.com>
2020-12-19 14:18:49 +0100

    Simplify template.

--- src/day0.cpp
+++ src/day0.cpp
@@ -6,20 +6,11 @@ using namespace std;
 
 namespace day0 {
    uint day0Part1() {
-      cout << "Day 0 - Part 1 from https://adventofcode.com/2020/day/0" << endl;
+      cout << "Day 0 - Part 1 from https://github.com/jp7677/adventofcode" << endl;
       return 1;
    }
 
    TEST_CASE("Day 0 - Part 1") {
       REQUIRE(day0Part1() == 1);
-   }
-
-   uint day0Part2() {
-      cout << "Day 0 - Part 2 from https://adventofcode.com/2020/day/0" << endl;
-      return 2;
-   }
-
-   TEST_CASE("Day 0 - Part 2") {
-      REQUIRE(day0Part2() == 2);
    }
 }
