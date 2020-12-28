045049ef624602c379c3921336f8752a49400253
Jens Peters <jp7677@gmail.com>
2020-12-28 09:23:34 +0100

    Build with optimizations.

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-g++ -std=c++17 -Werror inc/catch_amalgamated.cpp src/main.cpp -c
-g++ -std=c++17 -Werror \
+g++ -O2 -s -DNDEBUG -std=c++17 -Werror inc/catch_amalgamated.cpp src/main.cpp -c
+g++ -O2 -s -DNDEBUG -std=c++17 -Werror \
     catch_amalgamated.o \
     main.o \
     src/day*.cpp \
