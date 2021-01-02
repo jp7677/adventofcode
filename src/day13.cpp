8c03988ef5ad8839b8fd52ddfaa8d777fd595ff8
Jens Peters <jp7677@gmail.com>
2021-01-02 10:11:42 +0100

    Add day 13 part 2. Disabled for know since it runs at least for 10 minutes.

--- src/day13.cpp
+++ src/day13.cpp
@@ -26,4 +26,43 @@ namespace day13 {
 
       REQUIRE(result == 2545);
    }
+
+   TEST_CASE("Day 13 - Part 2 from https://adventofcode.com/2020/day/13#part2", "[.]" /* Disabled */ ) {
+      auto notesData = util::loadInputFile("day13-input.txt");
+
+      auto busIdData = util::split(notesData.at(1), ',');
+
+      vector<pair<uint, uint>> busIds;
+      auto offset = 0U;
+      for (const auto& id : busIdData) {
+         if (id != "x")
+            busIds.push_back(make_pair(stoi(id), offset));
+
+         offset++;
+      }
+
+      auto product = accumulate(busIds.begin(), busIds.end(), 1UL,
+         [](const auto product, const auto& id) {
+            return product * id.first;
+         });
+
+      auto first = busIds.at(0);
+      auto it = find_if(busIds.begin(), busIds.end(),
+         [&first](const auto& id) {
+            return id.second == first.first;
+         });
+      auto inc = first.first * (it != busIds.end() ? (*it).first : 1);
+      auto result = product - first.first;
+      while (result > inc) {
+         result -= inc;
+         for (auto i = 1; i < busIds.size(); i++)
+            if ((result + busIds.at(i).second) % busIds.at(i).first > 0)
+               goto next;
+
+         break;
+         next:;
+      }
+
+      REQUIRE(result == 266204454441577);
+   }
 }
