a967af97e53dfe77a9e94bf4c6b53fba4de79df6
Jens Peters <jp7677@gmail.com>
2021-01-09 08:05:12 +0100

    Create regex only once. Use count_if().

--- src/day02.cpp
+++ src/day02.cpp
@@ -6,10 +6,11 @@ namespace day02 {
    TEST_CASE("Day 02 - Part 1 from https://adventofcode.com/2020/day/2") {
       auto passwordEntriesInput = util::loadInputFile("day02-input.txt");
 
+      auto re = regex(R"((\d+)\-(\d+)\ (\w)\:\s(\w+))");
       auto result = 0U;
       for (const auto& passwordEntry : passwordEntriesInput) {
          smatch matches;
-         auto success = regex_search(passwordEntry, matches, regex(R"((\d+)\-(\d+)\ (\w)\:\s(\w+))"));
+         auto success = regex_search(passwordEntry, matches, re);
          if (!success)
             throw runtime_error("Invalid data found");
 
@@ -33,28 +34,28 @@ namespace day02 {
    TEST_CASE("Day 02 - Part 2 from https://adventofcode.com/2020/day/2#part2") {
       auto passwordEntriesInput = util::loadInputFile("day02-input.txt");
 
-      auto result = 0U;
-      for (const auto& passwordEntry : passwordEntriesInput) {
-         smatch matches;
-         auto success = regex_search(passwordEntry, matches, regex(R"((\d+)\-(\d+)\ (\w)\:\s(\w+))"));
-         if (!success)
-            throw runtime_error("Invalid data found");
+      auto re = regex(R"((\d+)\-(\d+)\ (\w)\:\s(\w+))");
+      auto result = count_if(passwordEntriesInput.begin(), passwordEntriesInput.end(),
+         [&re](const auto& passwordEntry) {
+            smatch matches;
+            auto success = regex_search(passwordEntry, matches, re);
+            if (!success)
+               throw runtime_error("Invalid data found");
 
-         auto first = stoi(matches[1].str());
-         auto second = stoi(matches[2].str());
-         auto character = matches[3].str().at(0);
-         auto password = matches[4].str();
+            auto first = stoi(matches[1].str());
+            auto second = stoi(matches[2].str());
+            auto character = matches[3].str().at(0);
+            auto password = matches[4].str();
 
-         auto match = 0U;
-         if (password.at(first - 1) == character)
-            match++; 
+            auto match = 0U;
+            if (password.at(first - 1) == character)
+               match++;
 
-         if (password.at(second - 1) == character)
-            match++;
+            if (password.at(second - 1) == character)
+               match++;
 
-         if (match == 1)
-            result++;
-      }
+            return match == 1;
+         });
 
       REQUIRE(result == 451);
    }
