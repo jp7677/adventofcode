11596c750a1131d2928a2daa97fbc82cebde5c9a
Jens Peters <jp7677@gmail.com>
2021-01-13 21:02:38 +0100

    Add day 15 part 2.

--- src/day15.cpp
+++ src/day15.cpp
@@ -3,27 +3,44 @@
 using namespace std;
 
 namespace day15 {
-    TEST_CASE("Day 15 - Part 1 from https://adventofcode.com/2020/day/15") {
-        auto numbersInputData = util::loadInputFile("day15-input.txt");
-
-        auto numbersInput = util::split(numbersInputData.at(0), ',');
+    uint playRounds(uint rounds) {
+        auto numbersInput = util::split(util::loadInputFile("day15-input.txt").at(0), ',');
 
         vector<uint> numbers;
         transform(numbersInput.begin(), numbersInput.end(), back_inserter(numbers),
-            [](const auto& number) {
-                return stoi(number);
-            });
+              [](const auto& number) {
+                  return stoi(number);
+              });
 
-        for (auto i = numbers.size(); i < 2020; i++) {
-            auto last = find(next(numbers.rbegin()), numbers.rend(), numbers.at(i - 1));
-            if (last == numbers.rend())
-                numbers.push_back(0);
+        unordered_map<uint, uint> lasts;
+        for (auto i = 0U; i < numbers.size() - 2; i++)
+            lasts.insert_or_assign(numbers.at(i), i);
+
+        auto previousLastSpoken = *next(numbers.rbegin());
+        auto lastSpoken = numbers.back();
+        for (auto i = numbers.size(); i < rounds; i++) {
+            lasts.insert_or_assign(previousLastSpoken, i - 2);
+            previousLastSpoken = lastSpoken;
+
+            auto last = lasts.find(lastSpoken);
+            if (last == lasts.end())
+                lastSpoken = 0;
             else
-                numbers.push_back(distance(numbers.rbegin(), last));
+                lastSpoken = i - last->second - 1;
         }
 
-        auto result = numbers.back();
+        return lastSpoken;
+    }
+
+    TEST_CASE("Day 15 - Part 1 from https://adventofcode.com/2020/day/15") {
+        auto result = playRounds(2020);
 
         REQUIRE(result == 289);
+    }
+
+    TEST_CASE("Day 15 - Part 2 from https://adventofcode.com/2020/day/15#part2") {
+        auto result = playRounds(30000000);
+
+        REQUIRE(result == 1505722);
     }
 }
