04e69c5898881611cfe3dd9fc5c0c53ecc9df1dc
Jens Peters <jp7677@gmail.com>
2021-01-16 13:28:26 +0100

    Add day 16 part 2.

--- src/day16.cpp
+++ src/day16.cpp
@@ -3,12 +3,8 @@
 using namespace std;
 
 namespace day16 {
-    TEST_CASE("Day 16 - Part 1 from https://adventofcode.com/2020/day/16") {
+    void loadNotes(vector<pair<string, vector<pair<uint, uint>>>>& ticketFields, vector<uint>& yourTicket, vector<vector<uint>>& nearbyTickets) {
         auto notesData = util::loadInputFile("day16-input.txt");
-
-        unordered_map<string, vector<pair<uint, uint>>> fieldValues;
-        vector<uint> yourTicket;
-        vector<vector<uint>> nearbyTickets;
 
         auto notesSection = 0U;
         for (const auto& notesLine : notesData) {
@@ -21,10 +17,10 @@ namespace day16 {
 
                     auto lineElements = util::split(notesLine, ':');
                     auto valueElements = util::split(lineElements[1], ' ', '-');
-                    fieldValues.emplace(lineElements[0], vector<pair<uint, uint>> {
+                    ticketFields.emplace_back(make_pair(lineElements[0], vector<pair<uint, uint>> {
                         make_pair(stoi(valueElements[0]), stoi(valueElements[1])),
                         make_pair(stoi(valueElements[3]), stoi(valueElements[4]))
-                    });
+                    }));
                     break;
                 }
                 case 1: {
@@ -59,22 +55,98 @@ namespace day16 {
                 default: throw runtime_error("invalid data");
             }
         }
+    }
+
+    bool isValidTicketValue(const uint& ticketValue, const pair<string, vector<pair<uint, uint>>>& fieldValue) {
+        return (ticketValue >= fieldValue.second[0].first && ticketValue <= fieldValue.second[0].second)
+            || (ticketValue >= fieldValue.second[1].first && ticketValue <= fieldValue.second[1].second);
+    }
+
+    pair<bool, uint> getInvalidTicketValue(const vector<uint>& nearbyTicket, const vector<pair<string, vector<pair<uint, uint>>>>& ticketFields) {
+        for (const auto& nearbyTicketValue : nearbyTicket) {
+            for (const auto &ticketField : ticketFields)
+                if (isValidTicketValue(nearbyTicketValue, ticketField))
+                    goto next;
+
+            return make_pair(true, nearbyTicketValue);
+            next:;
+        }
+
+        return make_pair(false, 0);
+    }
+
+    TEST_CASE("Day 16 - Part 1 from https://adventofcode.com/2020/day/16") {
+        vector<pair<string, vector<pair<uint, uint>>>> ticketFields;
+        vector<uint> yourTicket;
+        vector<vector<uint>> nearbyTickets;
+
+        loadNotes(ticketFields, yourTicket, nearbyTickets);
 
         auto result = accumulate(nearbyTickets.begin(), nearbyTickets.end(), 0U,
-            [&fieldValues](const auto sum, const auto& nearbyTicket) {
-                for (const auto& nearbyTicketValue : nearbyTicket) {
-                    for (const auto &fieldValue : fieldValues)
-                        for (const auto &value : fieldValue.second)
-                            if (nearbyTicketValue >= value.first && nearbyTicketValue <= value.second)
-                                goto next;
-
-                    return sum + nearbyTicketValue;
-                    next:;
-                }
-
-                return sum;
+            [&ticketFields](const auto sum, const auto& nearbyTicket) {
+                return sum + getInvalidTicketValue(nearbyTicket, ticketFields).second;
             });
 
         REQUIRE(result == 28884);
+    }
+
+    TEST_CASE("Day 16 - Part 2 from https://adventofcode.com/2020/day/16#part2") {
+        vector<pair<string, vector<pair<uint, uint>>>> ticketFields;
+        vector<uint> yourTicket;
+        vector<vector<uint>> nearbyTickets;
+
+        loadNotes(ticketFields, yourTicket, nearbyTickets);
+
+        nearbyTickets.erase(remove_if(nearbyTickets.begin(), nearbyTickets.end(),
+                [&ticketFields](const auto& nearbyTicket) {
+                    return getInvalidTicketValue(nearbyTicket, ticketFields).first;
+                }),
+            nearbyTickets.end());
+
+        vector<vector<string>> ticketFieldOptions(ticketFields.size());
+        for (const auto& nearbyTicket : nearbyTickets)
+            for (auto i = 0U; i < nearbyTicket.size(); i++)
+                for (const auto& ticketField : ticketFields)
+                    if (!isValidTicketValue(nearbyTicket.at(i), ticketField)
+                        && find(ticketFieldOptions.at(i).begin(), ticketFieldOptions.at(i).end(), ticketField.first) == ticketFieldOptions.at(i).end())
+                        ticketFieldOptions.at(i).push_back(ticketField.first);
+
+        vector<string> fieldNames;
+        transform(ticketFields.begin(), ticketFields.end(), back_inserter(fieldNames),
+            [](const auto& field) {
+                return field.first;
+            });
+        sort(fieldNames.begin(), fieldNames.end());
+
+        for (auto& fieldOption : ticketFieldOptions) {
+            vector<string> possibleFieldOptions;
+            sort(fieldOption.begin(), fieldOption.end());
+            set_difference(fieldNames.begin(), fieldNames.end(), fieldOption.begin(), fieldOption.end(), back_inserter(possibleFieldOptions));
+            fieldOption = possibleFieldOptions;
+        }
+
+        vector<string> finalTicketPositions{ticketFieldOptions.size(), string()};
+        while (find(finalTicketPositions.begin(), finalTicketPositions.end(), string()) != finalTicketPositions.end()) {
+            auto findFinalIt = find_if(ticketFieldOptions.begin(), ticketFieldOptions.end(),
+               [](const auto& fieldPosition){
+                    return fieldPosition.size() == 1;
+                });
+
+            auto fieldName = (*findFinalIt).at(0);
+            finalTicketPositions.at(distance(ticketFieldOptions.begin(), findFinalIt)) = fieldName;
+
+            for (auto& fieldOption : ticketFieldOptions) {
+                auto findNameIt = find(fieldOption.begin(), fieldOption.end(), fieldName);
+                if (findNameIt != fieldOption.end())
+                    fieldOption.erase(findNameIt);
+            }
+        }
+
+        auto result = 1UL;
+        for (auto i = 0U; i < finalTicketPositions.size(); ++i)
+            if (finalTicketPositions.at(i).rfind("departure", 0) == 0)
+                result *= yourTicket.at(i);
+
+        REQUIRE(result == 1001849322119);
     }
 }
