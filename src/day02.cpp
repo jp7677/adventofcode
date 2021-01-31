41ffcfd4c9338436a195064ec6d15ffee9760c81
Jens Peters <jp7677@gmail.com>
2021-01-31 11:25:52 +0100

    Declare regex's as static.

--- src/day02.cpp
+++ src/day02.cpp
@@ -6,9 +6,9 @@ namespace day02 {
     TEST_CASE("Day 02 - Part 1 from https://adventofcode.com/2020/day/2") {
         auto passwordEntriesInput = util::loadInputFile("day02-input.txt");
 
-        auto re = regex(R"((\d+)\-(\d+)\ (\w)\:\s(\w+))");
+        static const auto re = regex(R"((\d+)\-(\d+)\ (\w)\:\s(\w+))");
         auto result = count_if(passwordEntriesInput.begin(), passwordEntriesInput.end(),
-            [&re](const auto& passwordEntry) {
+            [](const auto& passwordEntry) {
                 smatch matches;
                 auto success = regex_search(passwordEntry, matches, re);
                 if (!success)
@@ -33,9 +33,9 @@ namespace day02 {
     TEST_CASE("Day 02 - Part 2 from https://adventofcode.com/2020/day/2#part2") {
         auto passwordEntriesInput = util::loadInputFile("day02-input.txt");
 
-        auto re = regex(R"((\d+)\-(\d+)\ (\w)\:\s(\w+))");
+        static const auto re = regex(R"((\d+)\-(\d+)\ (\w)\:\s(\w+))");
         auto result = count_if(passwordEntriesInput.begin(), passwordEntriesInput.end(),
-            [&re](const auto& passwordEntry) {
+            [](const auto& passwordEntry) {
                 smatch matches;
                 auto success = regex_search(passwordEntry, matches, re);
                 if (!success)
