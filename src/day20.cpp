15f29861953c83f0e0f3007e8c28d5ea836e0f18
Jens Peters <jp7677@gmail.com>
2021-03-05 22:31:07 +0100

    Simplify adjacent count.

--- src/day20.cpp
+++ src/day20.cpp
@@ -3,23 +3,8 @@
 using namespace std;
 
 namespace day00 {
-
     struct border {
         string top, right, bottom, left;
-    };
-
-    struct adjacentBorderCount {
-        uint top, right, bottom, left;
-
-        [[nodiscard]] uint getAdjacentCount() const {
-            auto count = 0U;
-            if (top > 0) count++;
-            if (right > 0) count++;
-            if (bottom > 0) count++;
-            if (left > 0) count++;
-
-            return count;
-        }
     };
 
     border getTileBorder(const vector<string>& tile) {
@@ -44,39 +29,30 @@ namespace day00 {
 
         unordered_map<uint, vector<string>> tiles;
         uint currentTile;
-        for (const auto& tileData : tilesData) {
+        for (const auto& tileData : tilesData)
             if (tileData[0] == 'T') {
                 currentTile = stoul(util::split(tileData, ' ', ':')[1]);
                 tiles.emplace(currentTile, vector<string>());
             } else if (!tileData.empty())
                 tiles[currentTile].emplace_back(tileData);
-        }
 
-        vector<uint> cornerTileIds;
-        for (const auto& tile : tiles) {
-            auto border = getTileBorder(tile.second);
-            auto count = adjacentBorderCount{0, 0, 0, 0};
+        auto result = accumulate(tiles.begin(), tiles.end(), 1UL,
+            [&tiles](const auto product, const auto& tile) {
+                auto border = getTileBorder(tile.second);
+                auto adjacentBorders = count_if(tiles.begin(), tiles.end(),
+                    [&tile, &border](const auto& other) {
+                        if (tile.first == other.first)
+                            return false;
 
-            for (const auto& testTile : tiles) {
-                if (tile.first == testTile.first)
-                    continue;
+                        auto otherBorder = getTileBorder(other.second);
+                        return anyAdjacentBorder(border.top, otherBorder)
+                            || anyAdjacentBorder(border.bottom, otherBorder)
+                            || anyAdjacentBorder(border.right, otherBorder)
+                            || anyAdjacentBorder(border.left, otherBorder);
+                    });
 
-                auto testTileBorders = getTileBorder(testTile.second);
-                if (anyAdjacentBorder(border.top, testTileBorders))
-                    count.top++;
-                if (anyAdjacentBorder(border.bottom, testTileBorders))
-                    count.bottom++;
-                if (anyAdjacentBorder(border.right, testTileBorders))
-                    count.right++;
-                if (anyAdjacentBorder(border.left, testTileBorders))
-                    count.left++;
-            }
-
-            if (count.getAdjacentCount() == 2)
-                cornerTileIds.emplace_back(tile.first);
-        }
-
-        auto result = accumulate(cornerTileIds.begin(), cornerTileIds.end(), 1UL, multiplies<>());
+                return adjacentBorders == 2 ? product * tile.first : product;
+            });
 
         REQUIRE(result == 7492183537913);
     }
