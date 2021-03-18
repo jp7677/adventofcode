e19a74cf5cc0bfd7cd0e1218da7268719ca77ada
Jens Peters <jp7677@gmail.com>
2021-03-18 23:16:17 +0100

    Add day 22 part 1.

--- src/day22.cpp
+++ src/day22.cpp
@@ -3,11 +3,9 @@
 using namespace std;
 
 namespace day22 {
-    TEST_CASE("Day 22 - Part 1 from https://adventofcode.com/2020/day/22") {
+    void loadDecks(vector<uint>& player1Cards, vector<uint>& player2Cards) {
         auto decksData = util::loadInputFile("day22-input.txt");
 
-        vector<uint> player1Cards;
-        vector<uint> player2Cards;
         bool player2Section = false;
         for (const auto& deckData : decksData) {
             if (deckData.empty())
@@ -21,6 +19,20 @@ namespace day22 {
             else
                 player2Cards.push_back(stoi(deckData));
         }
+    }
+
+    uint calculateScore(vector<uint>& winningCards) {
+        auto score = 0U;
+        for (auto i = winningCards.size(); i > 0; i--)
+            score += winningCards[winningCards.size() - i] * i;
+
+        return score;
+    }
+
+    TEST_CASE("Day 22 - Part 1 from https://adventofcode.com/2020/day/22") {
+        vector<uint> player1Cards;
+        vector<uint> player2Cards;
+        loadDecks(player1Cards, player2Cards);
 
         while (!player1Cards.empty() && !player2Cards.empty()) {
             if (player1Cards.front() > player2Cards.front()) {
@@ -35,12 +47,59 @@ namespace day22 {
             player2Cards.erase(player2Cards.begin(), next(player2Cards.begin()));
         }
 
-        auto winningCards = !player1Cards.empty() ? player1Cards : player2Cards;
-
-        auto result = 0U;
-        for (auto i = winningCards.size(); i > 0; i--)
-            result += winningCards[winningCards.size() - i] * i;
+        auto result = calculateScore(!player1Cards.empty() ? player1Cards : player2Cards);
 
         REQUIRE(result == 34664);
+    }
+
+    bool playGame(vector<uint>& player1Cards, vector<uint>& player2Cards) {
+        vector<pair<vector<uint>, vector<uint>>> previousGames;
+
+        while (!player1Cards.empty() && !player2Cards.empty()) {
+            auto previouslyPlayed = false;
+            for (const auto& previousGame : previousGames)
+                if (equal(previousGame.first.begin(), previousGame.first.end(), player1Cards.begin())
+                    && equal(previousGame.second.begin(), previousGame.second.end(), player2Cards.begin())) {
+                    previouslyPlayed = true;
+                    break;
+                }
+
+            previousGames.emplace_back(make_pair(player1Cards, player2Cards));
+
+            if (previouslyPlayed)
+                return true;
+
+            auto player1Wins = false;
+            if (player1Cards.size() - 1 >= player1Cards.front() && player2Cards.size() - 1 >= player2Cards.front()) {
+                auto player1CardsCopy = vector<uint>(player1Cards.begin() + 1, player1Cards.begin() + player1Cards.front() + 1);
+                auto player2CardsCopy = vector<uint>(player2Cards.begin() + 1, player2Cards.begin() + player2Cards.front() + 1);
+                player1Wins = playGame(player1CardsCopy, player2CardsCopy);
+            } else if (player1Cards.front() > player2Cards.front())
+                player1Wins = true;
+
+            if (player1Wins) {
+                player1Cards.push_back(player1Cards.front());
+                player1Cards.push_back(player2Cards.front());
+            } else {
+                player2Cards.push_back(player2Cards.front());
+                player2Cards.push_back(player1Cards.front());
+            }
+
+            player1Cards.erase(player1Cards.begin(), next(player1Cards.begin()));
+            player2Cards.erase(player2Cards.begin(), next(player2Cards.begin()));
+        }
+
+        return !player1Cards.empty();
+    }
+
+    TEST_CASE("Day 22 - Part 2 from https://adventofcode.com/2020/day/22#part2") {
+        vector<uint> player1Cards;
+        vector<uint> player2Cards;
+        loadDecks(player1Cards, player2Cards);
+
+        auto player1Wins = playGame(player1Cards, player2Cards);
+        auto result = calculateScore(player1Wins ? player1Cards : player2Cards);
+
+        REQUIRE(result == 32018);
     }
 }
