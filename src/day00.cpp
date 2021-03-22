22b011d5deb128c1557ff7c7eaa487648dddd322
Jens Peters <jp7677@gmail.com>
2021-03-22 22:36:19 +0100

    Add day 25 part 1.

--- src/day00.cpp
+++
@@ -1,9 +0,0 @@
-#include "days_private.h"
-
-using namespace std;
-
-namespace day00 {
-    TEST_CASE("Day 00 - Part 1 from https://github.com/jp7677/adventofcode") {
-        REQUIRE(true);
-    }
-}
