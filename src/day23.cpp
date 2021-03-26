a49c97659f521e429404f9ab87d12c7fbffe043d
Jens Peters <jp7677@gmail.com>
2021-03-26 22:23:10 +0100

    Solve day 23 within normal time frame. Unfortunately a tiny hint from the Internet was needed to get the idea with the linked list like construction.

--- src/day23.cpp
+++ src/day23.cpp
@@ -15,7 +15,7 @@ namespace day23 {
         return cups;
     }
 
-    void playRounds(vector<uint>& cups, uint numberOfRounds) {
+    void playRoundsWithRotations(vector<uint>& cups, uint numberOfRounds) {
         auto offset = 0U;
         for (auto round = 0U; round < numberOfRounds; round++) {
             auto currentIndex = (round + offset) % cups.size();
@@ -40,7 +40,7 @@ namespace day23 {
     TEST_CASE("Day 23 - Part 1 from https://adventofcode.com/2020/day/23") {
         auto cups = loadCups();
 
-        playRounds(cups, 100);
+        playRoundsWithRotations(cups, 100);
 
         string result;
         auto it = find(cups.begin(), cups.end(), 1);
@@ -56,17 +56,59 @@ namespace day23 {
         REQUIRE(result == "36472598");
     }
 
-    TEST_CASE("Day 23 - Part 2 from https://adventofcode.com/2020/day/23#part2","[.skip-due-to-horrible-performance]") {
+    uint nextCup(unordered_map<uint, pair<uint, uint>>& cups, uint current, uint steps = 1) {
+        auto it = cups.find(current);
+        for (auto i = 1U; i < steps; i++)
+            it = cups.find(it->second.second);
+
+        return it->second.second;
+    }
+
+    void playRoundsWithLinkedElements(unordered_map<uint, pair<uint, uint>>& cups, uint first, uint numberOfRounds) {
+        auto current = first;
+        array<uint, 3> pickup{0, 0, 0};
+        for (auto round = 0U; round < numberOfRounds; round++) {
+            for (auto i = 0U; i < 3; i++)
+                pickup[i] = nextCup(cups, current, i + 1);
+
+            auto destination = current;
+            auto it = pickup.begin();
+            while (it != pickup.end()) {
+                destination = destination == 1 ? cups.size() : destination - 1;
+                it = find(pickup.begin(), pickup.end(), destination);
+            }
+
+            cups[current].second = nextCup(cups, current, 4);
+            cups[nextCup(cups, current)].first = current;
+            cups[pickup[2]].second = nextCup(cups, destination);
+            cups[nextCup(cups, destination)].first = pickup[2];
+            cups[destination].second = pickup[0];
+            cups[pickup[0]].first = destination;
+
+            current = cups[current].second;
+        }
+    }
+
+    TEST_CASE("Day 23 - Part 2 from https://adventofcode.com/2020/day/23#part2") {
         auto initialCups = loadCups();
 
-        auto cups = vector<uint>(initialCups);
-        for (auto i = initialCups.size() + 1; i <= 1000000; i++)
-            cups.push_back(i);
+        constexpr uint numberOfCups = 1000000;
+        unordered_map<uint, pair<uint, uint>> cups;
+        for (auto i = 0U; i < initialCups.size(); i++)
+            cups.insert({
+                initialCups[i], {
+                    i > 0 ? initialCups[i - 1] : numberOfCups,
+                    i + 1 < initialCups.size() ? initialCups[i + 1] : i + 2}});
 
-        playRounds(cups, 10000000);
+        for (auto i = initialCups.size() + 1; i <= numberOfCups; i++)
+            cups.insert({
+                i, {
+                    i - 1 > initialCups.size() ? i - 1 : initialCups[initialCups.size() - 1],
+                    i < numberOfCups ? i + 1 : initialCups[0]}});
 
-        auto it = find(cups.begin(), cups.end(), 1);
-        auto result = static_cast<ulong>(*next(it) * *next(it, 2));
+        playRoundsWithLinkedElements(cups, initialCups[0], 10000000);
+
+        auto result = static_cast<ulong>(nextCup(cups, 1)) * nextCup(cups, 1, 2);
 
         REQUIRE(result == 90481418730);
     }
