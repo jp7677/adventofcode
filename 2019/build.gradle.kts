acbfc8fb3aaa40c0f8b9bbe04e6baaad65af7fad
Jens Peters <jp7677@gmail.com>
2021-08-25 12:01:22 +0200

    2019: Add basic structure for year 2019 This time it's Kotlin.

---
+++ 2019/build.gradle.kts
@@ -0,0 +1,25 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+    id("com.adarshr.test-logger") version "3.0.0"
+}
+
+group = "me.jp7677"
+version = "0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
--- README.md
+++ README.md
@@ -4,6 +4,7 @@ From <https://adventofcode.com/>. Better late than never ;)
 
 ## The years
 
+- Advent of Code 2019 Kotlin <https://github.com/jp7677/adventofcode/tree/main/2019>
 - Advent of Code 2020 C++ <https://github.com/jp7677/adventofcode/tree/main/2020>
 
 Enjoy the stars...
