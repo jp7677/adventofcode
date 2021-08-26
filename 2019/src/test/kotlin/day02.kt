5bd2b12aa8c160677daec17a2c2210269f4512c5
Jens Peters <jp7677@gmail.com>
2021-08-27 00:01:31 +0200

    2019: Add day 2 part2

--- 2019/src/test/kotlin/day02.kt
+++ 2019/src/test/kotlin/day02.kt
@@ -2,28 +2,45 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Day02 {
-    private val plus: Int.(Int) -> Int = Int::plus
-    private val times: Int.(Int) -> Int = Int::times
 
     @Test
     fun runPart01() {
-        val program = Util.getInputAsListOfInt("day02-input.txt", ",")
+        val memory = Util.getInputAsListOfInt("day02-input.txt", ",")
             .toTypedArray()
 
-        program[1] = 12
-        program[2] = 2
-        program
+        val result = runProgram(memory, 1202)
+        assertEquals (5866663, result)
+    }
+
+    @Test
+    fun runPart02() {
+        val memory = Util.getInputAsListOfInt("day02-input.txt", ",")
+            .toTypedArray()
+
+        val result = (0..9999)
+            .dropWhile { runProgram(memory.copyOf(), it) != 19690720 }
+            .first()
+
+        assertEquals (4259, result)
+    }
+
+    private val add: Int.(Int) -> Int = Int::plus
+    private val multiply: Int.(Int) -> Int = Int::times
+
+    private fun runProgram(memory: Array<Int>, nounVerb: Int): Int {
+        memory[1] = (nounVerb - nounVerb % 100) / 100
+        memory[2] = nounVerb % 100
+        memory
             .toList()
             .chunked(4)
             .takeWhile { it[0] != 99 }
             .forEach {
-                val operation = if (it[0] == 1) plus else times
-                val input1 = program[it[1]]
-                val input2 = program[it[2]]
-                program[it[3]] = operation(input1, input2)
+                val operation = if (it[0] == 1) add else multiply
+                val input1 = memory[it[1]]
+                val input2 = memory[it[2]]
+                memory[it[3]] = operation(input1, input2)
             }
 
-        val result = program.first()
-        assertEquals (5866663, result)
+        return memory.first()
     }
 }
