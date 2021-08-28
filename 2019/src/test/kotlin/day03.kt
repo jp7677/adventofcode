d5dc35751b32c45d4732c7e63c434780383f550c
Jens Peters <jp7677@gmail.com>
2021-08-28 14:55:09 +0200

    2019: Add day 3 part 1

---
+++ 2019/src/test/kotlin/day03.kt
@@ -0,0 +1,50 @@
+import java.lang.IllegalArgumentException
+import kotlin.math.absoluteValue
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day03 {
+
+    enum class Direction { R, L, U, D }
+    data class Location(val x: Int, val y: Int) { fun toManhattenDistance() = x.absoluteValue + y.absoluteValue }
+    data class Path(val direction: Direction, val length: Int)
+
+    @Test
+    fun runPart01() {
+        val wireData = Util.getInputAsListOfString("day03-input.txt")
+
+        val wireLocations = wireData
+            .map { wires -> wires
+                .split(",")
+                .map { it.toPath() }
+                .fold(listOf(Location(0, 0))) { locations, path ->
+                    locations + walk(locations.last(), path)
+                }
+                .filter { it != Location(0, 0) }
+            }
+
+        val distance = wireLocations.first()
+            .intersect(wireLocations.last())
+            .minOf { it.toManhattenDistance() }
+
+        assertEquals(245, distance)
+    }
+
+    private fun String.toPath(): Path = Path(
+        when (this[0]) {
+            'R' -> Direction.R
+            'L' -> Direction.L
+            'U' -> Direction.U
+            'D' -> Direction.D
+            else -> throw IllegalArgumentException()
+        },
+        this.substring(1).toInt()
+    )
+
+    private fun walk(start: Location, path: Path): List<Location> = when (path.direction){
+        Direction.R -> (1..path.length).map { Location(start.x + it, start.y) }
+        Direction.L -> (1..path.length).map { Location(start.x - it, start.y) }
+        Direction.U -> (1..path.length).map { Location(start.x, start.y + it) }
+        Direction.D -> (1..path.length).map { Location(start.x, start.y - it) }
+    }
+}
