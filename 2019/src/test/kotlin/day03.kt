b2fc36977c6c14d01402dfe3c35da829d869d3a7
Jens Peters <jp7677@gmail.com>
2021-08-28 22:05:12 +0200

    2019: Use minOf directly

--- 2019/src/test/kotlin/day03.kt
+++ 2019/src/test/kotlin/day03.kt
@@ -32,8 +32,7 @@ class Day03 {
         val distance = wireLocations.first()
             .intersect(wireLocations.last())
             .filter { it != Location(0, 0) }
-            .map { wireLocations.first().indexOf(it) + wireLocations.last().indexOf(it) }
-            .minOf{ it }
+            .minOf { wireLocations.first().indexOf(it) + wireLocations.last().indexOf(it) }
 
         assertEquals(48262, distance)
     }
