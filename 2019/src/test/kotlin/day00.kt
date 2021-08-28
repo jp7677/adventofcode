9d237a472cbfa4cd9bbf9272e584aa952dcaa745
Jens Peters <jp7677@gmail.com>
2021-08-28 14:58:03 +0200

    2019: Add template

---
+++ 2019/src/test/kotlin/day00.kt
@@ -0,0 +1,12 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day00 {
+
+    @Test
+    fun runPart01() {
+        val input = Util.getInputAsListOfString("day00-input.txt")
+
+        assertEquals(1, input.size)
+    }
+}
