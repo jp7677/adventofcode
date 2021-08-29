541ad3c2391a354f32b5bcd9b088cc1581387f79
Jens Peters <jp7677@gmail.com>
2021-08-29 10:21:00 +0200

    2019: Add day4 part 2

--- 2019/src/test/kotlin/day04.kt
+++ 2019/src/test/kotlin/day04.kt
@@ -15,10 +15,32 @@ class Day04 {
                     .zipWithNext()
 
                 digitWithNext.any { it.first == it.second }
-                        && digitWithNext.all { it.first <= it.second }
+                    && digitWithNext.all { it.first <= it.second }
         }
 
         assertEquals(979, count)
+    }
+
+    @Test
+    fun runPart02() {
+        val count = Util.getInputAsString("day04-input.txt")
+            .split('-')
+            .map(String::toInt)
+            .let { it.first()..it.last() }
+            .count { password ->
+                val digitWithNext = password
+                    .digits()
+                    .zipWithNext()
+
+                digitWithNext.any { it.first == it.second }
+                    && digitWithNext.all { it.first <= it.second }
+                    && digitWithNext
+                        .filter { it.first == it.second }
+                        .groupBy { it }
+                        .any { it.value.size == 1 }
+            }
+
+        assertEquals(635, count)
     }
 
     private fun Int.digits() : List<Int> = this.toString()
