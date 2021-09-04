cdf823ac910caa8d135fb1434daa4a57ad948970
Jens Peters <jp7677@gmail.com>
2021-09-04 19:41:55 +0200

    2019: Improve readability

--- 2019/src/test/kotlin/day05.kt
+++ 2019/src/test/kotlin/day05.kt
@@ -1,15 +1,10 @@
-import java.lang.IllegalStateException
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Day05 {
     enum class Mode { POSITION, IMMEDIATE }
-    enum class Op(val code: Int) {
-        ADD(1), MULTIPLY(2), INPUT(3), OUTPUT(4),
-        JUMP_IF_TRUE(5), JUMP_IF_FALSE(6), LESS_THAN(7), EQUALS(8),
-        EXIT(99)
-    }
-    data class Instruction(val op: Op, val param1Mode: Mode, val param2Mode: Mode, val param3Mode: Mode)
+    enum class Op(val code: Int) { ADD(1), MUL(2), IN(3), OUT(4), JNZ(5), JZ(6), SETL(7), SETE(8), ESC(99) }
+    data class Instruction(val op: Op, val param1Mode: Mode, val param2Mode: Mode)
 
     @Test
     fun runPart01() {
@@ -31,48 +26,39 @@ class Day05 {
         assertEquals (5525561, output)
     }
 
-    private fun runProgram(memory: Array<Int>, input: Int): Int {
+    private fun runProgram(mem: Array<Int>, input: Int): Int {
         var output = 0
-        var index = 0
+        var idx = 0
         while (true) {
-            val instr = memory[index].toInstruction()
+            val instr = mem[idx].toInstruction()
             when (instr.op) {
-                Op.EXIT -> break
-                Op.INPUT,
-                Op.OUTPUT -> {
-                    when (instr.op) {
-                        Op.INPUT  -> memory[memory[index + 1]] = input
-                        Op.OUTPUT -> output = getParamValue(instr.param1Mode, memory, index + 1)
-                        else -> throw IllegalStateException()
-                    }
-                    index += 2
-                }
+                Op.ESC  -> break
+                Op.OUT  -> output = mem[mem[idx + 1]]
+                Op.IN   -> mem[mem[idx + 1]] = input
                 else -> {
-                    val param1Value = getParamValue(instr.param1Mode, memory, index + 1)
-                    val param2Value = getParamValue(instr.param2Mode, memory, index + 2)
+                    val param1Value = if (instr.param1Mode == Mode.POSITION) mem[mem[idx + 1]] else mem[idx + 1]
+                    val param2Value = if (instr.param2Mode == Mode.POSITION) mem[mem[idx + 2]] else mem[idx + 2]
+                    @Suppress("NON_EXHAUSTIVE_WHEN")
                     when (instr.op) {
-                        Op.JUMP_IF_TRUE  -> index = if (param1Value != 0) param2Value else index + 3
-                        Op.JUMP_IF_FALSE -> index = if (param1Value == 0) param2Value else index + 3
-                        else -> {
-                            when (instr.op) {
-                                Op.ADD       -> memory[memory[index + 3]] = param1Value + param2Value
-                                Op.MULTIPLY  -> memory[memory[index + 3]] = param1Value * param2Value
-                                Op.LESS_THAN -> memory[memory[index + 3]] = if (param1Value < param2Value) 1 else 0
-                                Op.EQUALS    -> memory[memory[index + 3]] = if (param1Value == param2Value) 1 else 0
-                                else -> throw IllegalStateException()
-                            }
-                            index += 4
-                        }
+                        Op.ADD  -> mem[mem[idx + 3]] = param1Value + param2Value
+                        Op.MUL  -> mem[mem[idx + 3]] = param1Value * param2Value
+                        Op.SETL -> mem[mem[idx + 3]] = if (param1Value < param2Value) 1 else 0
+                        Op.SETE -> mem[mem[idx + 3]] = if (param1Value == param2Value) 1 else 0
+                        Op.JNZ  -> idx = if (param1Value != 0) param2Value else idx + 3
+                        Op.JZ   -> idx = if (param1Value == 0) param2Value else idx + 3
                     }
                 }
+            }
+
+            idx += when (instr.op) {
+                Op.ADD, Op.MUL, Op.SETL, Op.SETE -> 4
+                Op.IN, Op.OUT                    -> 2
+                else                             -> 0
             }
         }
 
         return output
     }
-
-    private fun getParamValue(paramMode: Mode, memory: Array<Int>, index: Int) =
-        if (paramMode == Mode.POSITION) memory[memory[index]] else memory[index]
 
     private fun Int.toInstruction(): Instruction = this.toString()
         .padStart(5, '0')
@@ -81,8 +67,7 @@ class Day05 {
             Instruction(
                 Op.values().single { it.code == "${digits[3]}${digits[4]}".toInt() },
                 if (digits[2] == 0) Mode.POSITION else Mode.IMMEDIATE,
-                if (digits[1] == 0) Mode.POSITION else Mode.IMMEDIATE,
-                if (digits[0] == 0) Mode.POSITION else Mode.IMMEDIATE
+                if (digits[1] == 0) Mode.POSITION else Mode.IMMEDIATE
             )
         }
 }
