4de547fa29a91493f424485353751d67f7294653
Jens Peters <jp7677@gmail.com>
2021-09-07 21:14:07 +0200

    2019: Remove not needed return type declaration

--- 2019/src/test/kotlin/util.kt
+++ 2019/src/test/kotlin/util.kt
@@ -1,13 +1,13 @@
 class Util {
     companion object {
-        fun getInputAsString(name: String): String =
+        fun getInputAsString(name: String) =
             Util::class.java.getResource(name)?.readText()
                 ?: throw Exception("Invalid input name")
 
-        fun getInputAsListOfString(name: String, separator: String = System.lineSeparator()): List<String> =
+        fun getInputAsListOfString(name: String, separator: String = System.lineSeparator()) =
             getInputAsString(name).split(separator)
 
-        fun getInputAsListOfInt(name: String, separator: String = System.lineSeparator()): List<Int> =
+        fun getInputAsListOfInt(name: String, separator: String = System.lineSeparator()) =
             getInputAsListOfString(name, separator).map(String::toInt)
     }
 }
