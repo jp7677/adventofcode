d19c1d0fab7e349b40daf369fc972f00b6397c06
Jens Peters <jp7677@gmail.com>
2021-09-10 23:25:10 +0200

    2019: Add day 6 part 2

--- 2019/src/test/kotlin/day06.kt
+++ 2019/src/test/kotlin/day06.kt
@@ -5,21 +5,37 @@ class Day06 {
 
     @Test
     fun runPart01() {
-        val map = Util.getInputAsListOfString("day06-input.txt")
-            .map { it.split(")") }
-            .map { Pair(it[0],it[1]) }
+        val map = getInputAsMap()
 
         val result = map
             .flatMap { listOf(it.first, it.second) }
             .distinct()
-            .sumOf { map.numberOfOrbits(it) }
+            .sumOf { map.traverseOrbits(it).size }
 
         assertEquals(241064, result)
     }
 
-    private tailrec fun List<Pair<String, String>>.numberOfOrbits(orbit: String, sum: Int = 0): Int =
-        if (this.any { orbit == it.second })
-            numberOfOrbits(this.single { orbit == it.second }.first, sum + 1)
-        else
-            sum
+    @Test
+    fun runPart02() {
+        val map = getInputAsMap()
+
+        val result = listOf("YOU", "SAN")
+            .map { map.traverseOrbits(it) }
+            .let { it.first().count() + it.last().count() - 2 - 2 * (it.first() intersect it.last()).count() }
+
+        assertEquals(418, result)
+    }
+
+    private tailrec fun List<Pair<String, String>>.traverseOrbits(
+        orbit: String,
+        path: MutableList<Pair<String, String>> = mutableListOf()
+    ): List<Pair<String, String>> {
+        val next = this.firstOrNull { orbit == it.second } ?: return path
+        path += next
+        return traverseOrbits(this.single { orbit == it.second }.first, path)
+    }
+
+    private fun getInputAsMap() = Util.getInputAsListOfString("day06-input.txt")
+        .map { it.split(")") }
+        .map { Pair(it[0], it[1]) }
 }
