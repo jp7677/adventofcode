195e54651fde487f10ec760818c2b51c70de73f5
Jens Peters <jp7677@gmail.com>
2021-09-11 00:11:38 +0200

    2019: Use generateSequence

--- 2019/src/test/kotlin/day06.kt
+++ 2019/src/test/kotlin/day06.kt
@@ -10,7 +10,7 @@ class Day06 {
         val result = map
             .flatMap { listOf(it.first, it.second) }
             .distinct()
-            .sumOf { map.traverseOrbits(it).size }
+            .sumOf { map.traverseOrbits(it).count() }
 
         assertEquals(241064, result)
     }
@@ -20,20 +20,14 @@ class Day06 {
         val map = getInputAsMap()
 
         val result = listOf("YOU", "SAN")
-            .map { map.traverseOrbits(it) }
+            .map { map.traverseOrbits(it).toList() }
             .let { it.first().count() + it.last().count() - 2 - 2 * (it.first() intersect it.last()).count() }
 
         assertEquals(418, result)
     }
 
-    private tailrec fun List<Pair<String, String>>.traverseOrbits(
-        orbit: String,
-        path: MutableList<Pair<String, String>> = mutableListOf()
-    ): List<Pair<String, String>> {
-        val next = this.firstOrNull { orbit == it.second } ?: return path
-        path += next
-        return traverseOrbits(this.single { orbit == it.second }.first, path)
-    }
+    private fun List<Pair<String, String>>.traverseOrbits(orbit: String) =
+        generateSequence(singleOrNull { it.second == orbit }) { it2 -> firstOrNull { it.second == it2.first } }
 
     private fun getInputAsMap() = Util.getInputAsListOfString("day06-input.txt")
         .map { it.split(")") }
