70ea06f1f0131c810925b913b5ac2c00c1d78b1c
Jens Peters <jp7677@gmail.com>
2021-09-16 21:37:23 +0200

    2019: Add day 7 part 1

--- 2019/src/test/kotlin/util.kt
+++ 2019/src/test/kotlin/util.kt
@@ -11,3 +11,27 @@ class Util {
             getInputAsListOfString(name, separator).map(String::toInt)
     }
 }
+
+fun <V> List<V>.permutations(): List<List<V>> {
+    val listOfPermutations: MutableList<List<V>> = mutableListOf()
+
+    fun swap(list: MutableList<V>, i: Int, j: Int) {
+        list[i] = list.set(j, list[i])
+    }
+
+    fun generate(k: Int, list: MutableList<V>) {
+        if (k == 1)
+            listOfPermutations.add(list.toList())
+        else
+            for (i in 0 until k) {
+                generate(k - 1, list)
+                if (k % 2 == 0)
+                    swap(list, i, k - 1)
+                else
+                    swap(list, 0, k - 1)
+            }
+    }
+
+    generate(this.count(), this.toMutableList())
+    return listOfPermutations
+}
