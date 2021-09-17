d4a3b1e0f7d181e1349ba8f92fb99b64506cc0a2
Jens Peters <jp7677@gmail.com>
2021-09-17 13:50:01 +0200

    2019: Extract InCode computer

--- 2019/src/test/kotlin/day02.kt
+++ 2019/src/test/kotlin/day02.kt
@@ -7,7 +7,12 @@ class Day02 {
     fun runPart01() {
         val program = Util.getInputAsListOfInt("day02-input.txt", ",")
 
-        val value = runProgram(program, 12, 2)
+        val value = IntCodeComputer(program.toTypedArray())
+            .apply { noun = 12 }
+            .apply { verb = 2 }
+            .also { it.run() }
+            .positionZero
+
         assertEquals (5866663, value)
     }
 
@@ -17,33 +22,17 @@ class Day02 {
 
         val nounVerb = (0..99)
             .crossJoin()
-            .first { runProgram(program, it.first, it.second) == 19690720 }
+            .first {
+                IntCodeComputer(program.toTypedArray())
+                    .apply { noun = it.first }
+                    .apply { verb = it.second }
+                    .also { it.run() }
+                    .positionZero == 19690720
+            }
             .let { it.first * 100 + it.second }
 
         assertEquals (4259, nounVerb)
     }
 
     private fun IntRange.crossJoin() = flatMap { it1 -> map { it2 -> it1 to it2} }
-
-    private fun runProgram(program: List<Int>, noun: Int, verb: Int): Int {
-        val memory = program.toTypedArray()
-        memory[1] = noun
-        memory[2] = verb
-
-        memory
-            .toList()
-            .chunked(4)
-            .takeWhile { it[0] != 99 }
-            .forEach {
-                val operation = if (it[0] == 1) add else multiply
-                val input1 = memory[it[1]]
-                val input2 = memory[it[2]]
-                memory[it[3]] = operation(input1, input2)
-            }
-
-        return memory.first()
-    }
-
-    private val add: Int.(Int) -> Int = Int::plus
-    private val multiply: Int.(Int) -> Int = Int::times
 }
