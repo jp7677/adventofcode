1c19a25729f91d226f3468237078ad7d6530a848
Jens Peters <jp7677@gmail.com>
2021-09-19 10:39:39 +0200

    2019: Add day 8 part 2

--- 2019/src/test/kotlin/day08.kt
+++ 2019/src/test/kotlin/day08.kt
@@ -16,4 +16,25 @@ class Day08 {
 
         assertEquals(1215, number)
     }
+
+    @Test
+    fun runPart02() {
+        val image = Util.getInputAsString("day08-input.txt")
+
+        val decoded = image
+            .chunked(25 * 6)
+            .reduce { acc, layer ->
+                acc.zip(layer) { a, b -> if (a == '2') b else a }
+                    .toCharArray().concatToString()
+            }
+            .replaceMultiple("0" to " ", "1" to "#")
+            .chunked(25)
+
+        assertEquals("#    #  #  ##  ###  #  # ", decoded[0])
+        assertEquals("#    #  # #  # #  # #  # ", decoded[1])
+        assertEquals("#    #### #    #  # #### ", decoded[2])
+        assertEquals("#    #  # #    ###  #  # ", decoded[3])
+        assertEquals("#    #  # #  # #    #  # ", decoded[4])
+        assertEquals("#### #  #  ##  #    #  # ", decoded[5])
+    }
 }
