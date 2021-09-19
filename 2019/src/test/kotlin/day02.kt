520a2302a21e91a82458e391cf327ab91cd40544
Jens Peters <jp7677@gmail.com>
2021-09-19 20:56:19 +0200

    2019: Add day 9 part 1 & 2

--- 2019/src/test/kotlin/day02.kt
+++ 2019/src/test/kotlin/day02.kt
@@ -5,9 +5,9 @@ class Day02 {
 
     @Test
     fun runPart01() {
-        val memory = Util.getInputAsListOfInt("day02-input.txt", ",")
+        val memory = Util.getInputAsListOfLong("day02-input.txt", ",")
 
-        val positionZero = IntCodeComputer(memory.toTypedArray())
+        val positionZero = IntCodeComputer(memory.toLongArray())
             .apply { noun = 12; verb = 2 }
             .also { it.runUntilExit() }
             .positionZero
@@ -17,20 +17,20 @@ class Day02 {
 
     @Test
     fun runPart02() {
-        val memory = Util.getInputAsListOfInt("day02-input.txt", ",")
+        val memory = Util.getInputAsListOfLong("day02-input.txt", ",")
 
-        val nounVerb = (0..99)
+        val nounVerb = (0L..99L)
             .crossJoin()
             .first {
-                IntCodeComputer(memory.toTypedArray())
+                IntCodeComputer(memory.toLongArray())
                     .apply { noun = it.first; verb = it.second }
                     .also { it.runUntilExit() }
-                    .positionZero == 19690720
+                    .positionZero == 19690720L
             }
             .let { it.first * 100 + it.second }
 
         assertEquals (4259, nounVerb)
     }
 
-    private fun IntRange.crossJoin() = flatMap { it1 -> map { it2 -> it1 to it2} }
+    private fun LongRange.crossJoin() = flatMap { it1 -> map { it2 -> it1 to it2} }
 }
--- 2019/src/test/kotlin/day05.kt
+++ 2019/src/test/kotlin/day05.kt
@@ -5,7 +5,7 @@ class Day05 {
 
     @Test
     fun runPart01() {
-        val memory = Util.getInputAsListOfInt("day05-input.txt", ",").toTypedArray()
+        val memory = Util.getInputAsListOfLong("day05-input.txt", ",").toLongArray()
 
         val output = IntCodeComputer(memory).runUntilExit(1).last()
 
@@ -14,7 +14,7 @@ class Day05 {
 
     @Test
     fun runPart02() {
-        val memory = Util.getInputAsListOfInt("day05-input.txt", ",").toTypedArray()
+        val memory = Util.getInputAsListOfLong("day05-input.txt", ",").toLongArray()
 
         val output = IntCodeComputer(memory).runUntilExit(5).last()
 
