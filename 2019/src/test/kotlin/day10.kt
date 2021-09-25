5315b539e47d37681457af7edaacbc9ae5b34452
Jens Peters <jp7677@gmail.com>
2021-09-25 20:35:02 +0200

    2019: Add day 10 part 2

--- 2019/src/test/kotlin/day10.kt
+++ 2019/src/test/kotlin/day10.kt
@@ -8,10 +8,7 @@ class Day10 {
 
     @Test
     fun runPart01() {
-        val coords = Util.getInputAsListOfString("day10-input.txt")
-            .flatMapIndexed { indexY, line -> line.mapIndexed { indexX, char -> Triple(indexX, indexY, char) } }
-            .filter { it.third == '#' }
-            .map { Coord(it.first, it.second) }
+        val coords = getInputAsCoords()
 
         val detected = coords.maxOf {
             coords
@@ -21,6 +18,41 @@ class Day10 {
 
         assertEquals(274, detected)
     }
+
+    @Test
+    fun runPart02() {
+        val coords = getInputAsCoords()
+
+        val laser = coords.maxByOrNull {
+            coords
+                .filter { other -> it != other }
+                .count { other -> it.isVisibleFor(other, coords) }
+        } ?: throw IllegalStateException()
+
+        val asteroids = coords
+            .filter { it != laser }
+            .sortedBy { laser.angleTo(it) }
+            .toMutableList()
+
+        val hits = mutableListOf<Coord>()
+        while (asteroids.isNotEmpty()) {
+            asteroids.onEach {
+                if (it.isVisibleFor(laser, asteroids)) hits.add(it)
+            }
+            asteroids.removeAll(hits)
+        }
+
+        val result = hits[200 - 1]
+            .let { it.x * 100 + it.y }
+            .toInt()
+
+        assertEquals(305, result)
+    }
+
+    private fun getInputAsCoords() = Util.getInputAsListOfString("day10-input.txt")
+        .flatMapIndexed { indexY, line -> line.mapIndexed { indexX, char -> Triple(indexX, indexY, char) } }
+        .filter { it.third == '#' }
+        .map { Coord(it.first, it.second) }
 
     private fun Coord.isVisibleFor(other: Coord, coords: List<Coord>) =
         (this.pathTo(other).trim() intersect coords).isEmpty()
@@ -35,4 +67,9 @@ class Day10 {
 
     private fun solveLinearEquationFromTwoPoints(x: Double, p: Coord, q: Coord) =
         p.y + (x - p.x) / (q.x - p.x) * (q.y - p.y)
+
+    private fun Coord.angleTo(asteroid: Coord) =
+        Math.toDegrees(kotlin.math.atan2(asteroid.y - this.y, asteroid.x - this.x))
+            .plus(90)
+            .let { if (it < 0) it + 360 else it }
 }
