8501429b2557b47ca3d3119c7c3f18d03fa5c40c
Jens Peters <jp7677@gmail.com>
2021-09-28 23:59:30 +0200

    2019: Add day 11 part 2

--- 2019/src/test/kotlin/day11.kt
+++ 2019/src/test/kotlin/day11.kt
@@ -7,30 +7,66 @@ class Day11 {
 
     @Test
     fun runPart01() {
+        val result = getPaintedPanels()
+            .groupBy { Pair(it.x, it.y) }
+            .count()
+
+        assertEquals(2041, result)
+    }
+
+    @Test
+    fun runPart02() {
+        val panels = getPaintedPanels(1)
+        val minX = panels.minOf { it.x }
+        val maxX = panels.maxOf { it.x }
+
+        val id = panels
+            .groupBy { Pair(it.x, it.y) }
+            .map { it.value.last() }
+            .filter { it.color == 1L }
+            .groupBy { it.y }
+            .toSortedMap()
+            .map {
+                val builder = StringBuilder()
+                builder.append(" ".repeat(maxX - minX))
+                it.value.forEach { panel -> builder[panel.x - minX] = '#' }
+                builder.toString()
+            }
+            .reversed()
+
+        assertEquals(" #### ###  #### ###  #  # #### #### ###   ", id[0])
+        assertEquals("    # #  #    # #  # # #  #       # #  #  ", id[1])
+        assertEquals("   #  #  #   #  #  # ##   ###    #  #  #  ", id[2])
+        assertEquals("  #   ###   #   ###  # #  #     #   ###   ", id[3])
+        assertEquals(" #    # #  #    #    # #  #    #    # #   ", id[4])
+        assertEquals(" #### #  # #### #    #  # #### #### #  #  ", id[5])
+    }
+
+    private fun getPaintedPanels(initialPanelColor: Long = 0L): List<Panel> {
         val program = Util.getInputAsListOfLong("day11-input.txt", ",").toLongArray()
         val brain = IntCodeComputer(program)
 
         var direction = Direction.UP
-        val painted = mutableListOf(Panel(0, 0, 0))
+        val painted = mutableListOf(Panel(0, 0, initialPanelColor))
         while (true) {
             painted.last().color = brain.runWithBreakOnOutput(painted.last().color) ?: break
             val turn = brain.runWithBreakOnOutput() ?: throw IllegalStateException()
 
-            direction = when(direction) {
+            direction = when (direction) {
                 Direction.UP -> if (turn == 0L) Direction.LEFT else Direction.RIGHT
                 Direction.RIGHT -> if (turn == 0L) Direction.UP else Direction.DOWN
                 Direction.DOWN -> if (turn == 0L) Direction.RIGHT else Direction.LEFT
                 Direction.LEFT -> if (turn == 0L) Direction.DOWN else Direction.UP
             }
 
-            val nextX = when(direction) {
+            val nextX = when (direction) {
                 Direction.RIGHT -> painted.last().x + 1
                 Direction.LEFT -> painted.last().x - 1
                 Direction.UP,
                 Direction.DOWN -> painted.last().x
             }
 
-            val nextY = when(direction) {
+            val nextY = when (direction) {
                 Direction.UP -> painted.last().y + 1
                 Direction.DOWN -> painted.last().y - 1
                 Direction.RIGHT,
@@ -44,11 +80,6 @@ class Day11 {
             painted.add(Panel(nextX, nextY, nextColor))
         }
 
-        val result = painted
-            .dropLast(1)
-            .groupBy { Pair(it.x, it.y) }
-            .count()
-
-        assertEquals(2041, result)
+        return painted.dropLast(1)
     }
 }
