8eb6308a860ca0037bb2cfa8e771314e954e0fef
Jens Peters <jp7677@gmail.com>
2021-12-01 12:33:17 +0100

    2021: Add day 01 part 2

--- 2021/src/test/kotlin/day01.kt
+++ 2021/src/test/kotlin/day01.kt
@@ -11,7 +11,19 @@ class Day01 {
             .zipWithNext()
             .count { it.second > it.first }
 
-
         assertEquals(1162, increments)
+    }
+
+    @Test
+    fun `run part 02`() {
+        val sweeps = Util.getInputAsListOfInt("day01-input.txt")
+
+        val increments = sweeps
+            .windowed(3, 1)
+            .map { it.sum() }
+            .zipWithNext()
+            .count { it.second > it.first }
+
+        assertEquals(1190, increments)
     }
 }
