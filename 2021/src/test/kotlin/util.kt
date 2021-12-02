7d892191c29ee72cee20c6e0dd2fca49e9c77890
Jens Peters <jp7677@gmail.com>
2021-12-02 07:19:32 +0100

    2021: Use object instead of class No need for a class since it contained only static methods.

--- 2021/src/test/kotlin/util.kt
+++ 2021/src/test/kotlin/util.kt
@@ -1,16 +1,15 @@
-class Util {
-    companion object {
-        fun getInputAsString(name: String) =
-            Util::class.java.getResource(name)?.readText()
-                ?: throw Exception("Invalid input name")
+object Util {
 
-        fun getInputAsListOfString(name: String, separator: String = System.lineSeparator()) =
-            getInputAsString(name).split(separator)
+    fun getInputAsString(name: String) =
+        Util::class.java.getResource(name)?.readText()
+            ?: throw Exception("Invalid input name")
 
-        fun getInputAsListOfInt(name: String, separator: String = System.lineSeparator()) =
-            getInputAsListOfString(name, separator).map(String::toInt)
+    fun getInputAsListOfString(name: String, separator: String = System.lineSeparator()) =
+        getInputAsString(name).split(separator)
 
-        fun getInputAsListOfLong(name: String, separator: String = System.lineSeparator()) =
-            getInputAsListOfString(name, separator).map(String::toLong)
-    }
+    fun getInputAsListOfInt(name: String, separator: String = System.lineSeparator()) =
+        getInputAsListOfString(name, separator).map(String::toInt)
+
+    fun getInputAsListOfLong(name: String, separator: String = System.lineSeparator()) =
+        getInputAsListOfString(name, separator).map(String::toLong)
 }
