492580c5b5bca0e4d5103d26c3db1120defe1e0a
Jens Peters <jp7677@gmail.com>
2021-12-03 13:47:04 +0100

    2021: Add day 3 part 2

--- 2021/src/test/kotlin/day03.kt
+++ 2021/src/test/kotlin/day03.kt
@@ -2,34 +2,65 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Day03 {
-    enum class RateType { GAMMA, EPSILON}
+    enum class RateType { GAMMA, EPSILON, OXYGEN, CO2 }
 
     @Test
     fun `run part 01`() {
         val report = Util.getInputAsListOfString("day03-input.txt")
 
-        val bitsList = List(report.first().length) { index ->
-            report
-                .map { it[index] }
-                .joinToString("")
-        }
-
-        val powerConsumption = bitsList.getPowerRating(RateType.GAMMA) * bitsList.getPowerRating(RateType.EPSILON)
+        val powerConsumption = report
+            .let { it.getPowerRating(RateType.GAMMA) * it.getPowerRating(RateType.EPSILON) }
 
         assertEquals(4103154, powerConsumption)
     }
 
-    private fun List<String>.getPowerRating(type: RateType) = this
-        .map { bit ->
-            val map = bit
-                .groupingBy { it }
-                .eachCount()
+    private fun List<String>.getPowerRating(type: RateType) =
+        this
+            .first()
+            .indices
+            .map { this.getCriteriaFromIndex(it, type).toString() }
+            .toInt()
 
-            if (type == RateType.GAMMA)
-                map.maxByOrNull { it.value }?.key
-            else
-                map.minByOrNull { it.value }?.key
-        }
+    @Test
+    fun `run part 02`() {
+        val report = Util.getInputAsListOfString("day03-input.txt")
+
+        val lifeSupport = report
+            .let { it.getLifeSupportRating(RateType.OXYGEN) * it.getLifeSupportRating(RateType.CO2) }
+
+        assertEquals(4245351, lifeSupport)
+    }
+
+    private fun List<String>.getLifeSupportRating(type: RateType): Int {
+        var filtered = this.toList()
+
+        this
+            .first()
+            .indices
+            .map { index ->
+                val criteria = filtered.getCriteriaFromIndex(index, type)
+                filtered = filtered.filter { it[index] == criteria }
+            }
+
+        return filtered.toInt()
+    }
+
+    private fun List<String>.getCriteriaFromIndex(index: Int, type: RateType): Char {
+        val grouped = this
+            .map { it[index] }
+            .joinToString("")
+            .groupingBy { it }
+            .eachCount()
+
+        val criteria = if (type == RateType.GAMMA || type == RateType.OXYGEN)
+            grouped.entries.sortedByDescending { it.key }.maxByOrNull { it.value }?.key
+        else
+            grouped.entries.sortedBy { it.key }.minByOrNull { it.value }?.key
+
+        return criteria ?: throw IllegalStateException()
+    }
+
+    private fun List<String>.toInt() = this
         .joinToString("")
         .toBigInteger(2)
         .toInt()
