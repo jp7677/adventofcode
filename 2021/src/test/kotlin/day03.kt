9a856a03c928b9f3dc83c30f13f28b74d7ddcbbc
Jens Peters <jp7677@gmail.com>
2021-12-03 18:15:55 +0100

    2021: Use same sorted and maxBy for all paths

--- 2021/src/test/kotlin/day03.kt
+++ 2021/src/test/kotlin/day03.kt
@@ -14,11 +14,10 @@ class Day03 {
         assertEquals(4103154, powerConsumption)
     }
 
-    private fun List<String>.getPowerRating(type: RateType) =
-        this
-            .first()
-            .mapIndexed { index, _ -> this.getBitFromIndex(index, type).toString() }
-            .toInt()
+    private fun List<String>.getPowerRating(type: RateType) = this
+        .first()
+        .mapIndexed { index, _ -> this.getCommonBit(index, type).toString() }
+        .toInt()
 
     @Test
     fun `run part 02`() {
@@ -36,32 +35,21 @@ class Day03 {
         this
             .first()
             .forEachIndexed { index, _ ->
-                val criteria = filtered.getBitFromIndex(index, type)
-                filtered = filtered.filter { it[index] == criteria }
+                val bit = filtered.getCommonBit(index, type)
+                filtered = filtered.filter { it[index] == bit }
             }
 
         return filtered.toInt()
     }
 
-    private fun List<String>.getBitFromIndex(index: Int, type: RateType): Char {
-        val grouped = this
-            .map { it[index] }
-            .joinToString("")
-            .groupingBy { it }
-            .eachCount()
-            .entries
-
-        return if (type == RateType.GAMMA || type == RateType.OXYGEN)
-            grouped
-                .sortedByDescending { it.key }
-                .maxByOrNull { it.value }
-                ?.key ?: throw IllegalStateException()
-        else
-            grouped
-                .sortedBy { it.key }
-                .minByOrNull { it.value }
-                ?.key ?: throw IllegalStateException()
-    }
+    private fun List<String>.getCommonBit(index: Int, type: RateType) = this
+        .map { it[index].digitToInt() }
+        .groupingBy { it }
+        .eachCount()
+        .entries
+        .sortedBy { if (type == RateType.EPSILON || type == RateType.CO2) it.key else it.key * -1 }
+        .maxByOrNull { if (type == RateType.GAMMA || type == RateType.OXYGEN) it.value else it.value * -1 }
+        ?.key?.digitToChar() ?: throw IllegalStateException()
 
     private fun List<String>.toInt() = this
         .joinToString("")
