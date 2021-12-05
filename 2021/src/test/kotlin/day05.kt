dd09161b856e87ffb199b2660a51950f47457cb8
Jens Peters <jp7677@gmail.com>
2021-12-05 12:07:01 +0100

    2021: Formatting

--- 2021/src/test/kotlin/day05.kt
+++ 2021/src/test/kotlin/day05.kt
@@ -21,19 +21,18 @@ class Day05 {
 
     private fun getCoords() = Util.getInputAsListOfString("day05-input.txt")
         .map {
-            it.split(" ", ",")
-                .let { s ->
-                    Coord(s[0].toInt(), s[1].toInt()) to Coord(s[3].toInt(), s[4].toInt())
-                }
+            it.split(" ", ",").let { s ->
+                Coord(s[0].toInt(), s[1].toInt()) to Coord(s[3].toInt(), s[4].toInt())
+            }
         }
 
-    private fun List<Pair<Coord, Coord>>.calcOverlaps(includeDiagonal: Boolean = false) = this
+    private fun List<Pair<Coord, Coord>>.calcOverlaps(includeDiagonals: Boolean = false) = this
         .flatMap {
             if (it.first.x == it.second.x)
                 (it.first.y towards it.second.y).map { y -> Coord(it.first.x, y) }
             else if (it.first.y == it.second.y)
                 (it.first.x towards it.second.x).map { x -> Coord(x, it.first.y) }
-            else if (includeDiagonal && abs(it.first.x - it.second.x) == abs(it.first.y - it.second.y))
+            else if (includeDiagonals && abs(it.first.x - it.second.x) == abs(it.first.y - it.second.y))
                 (it.first.x towards it.second.x).mapIndexed { index, x ->
                     Coord(x, (it.first.y towards it.second.y).elementAt(index))
                 }
@@ -44,6 +43,5 @@ class Day05 {
         .eachCount()
         .count { it.value >= 2 }
 
-    private infix fun Int.towards(to: Int) =
-        IntProgression.fromClosedRange(this, to, if (this > to) -1 else 1)
+    private infix fun Int.towards(to: Int) = IntProgression.fromClosedRange(this, to, if (this > to) -1 else 1)
 }
