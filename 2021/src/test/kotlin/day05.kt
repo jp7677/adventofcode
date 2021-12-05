d4a4c78fd51bffba84f0d0efdf961bdce2e74123
Jens Peters <jp7677@gmail.com>
2021-12-05 10:14:38 +0100

    2021: Add day 5 part 2

--- 2021/src/test/kotlin/day05.kt
+++ 2021/src/test/kotlin/day05.kt
@@ -1,3 +1,4 @@
+import kotlin.math.abs
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -6,27 +7,42 @@ class Day05 {
 
     @Test
     fun `run part 01`() {
-        val ventEndCoords = Util.getInputAsListOfString("day05-input.txt")
-            .map { it.split(" ", ",")
-                .let { s ->
-                    Coord(s[0].toInt(), s[1].toInt()) to Coord(s[3].toInt(), s[4].toInt()) }
-                }
-
-        val overlaps = ventEndCoords
-            .flatMap {
-                if (it.first.x == it.second.x)
-                    (it.first.y towards it.second.y).map { y -> Coord(it.first.x, y) }
-                else if (it.first.y == it.second.y)
-                    (it.first.x towards it.second.x).map { x -> Coord(x, it.first.y) }
-                else
-                    listOf()
-            }
-            .groupingBy { it }
-            .eachCount()
-            .count { it.value >= 2 }
+        val overlaps = getCoords().calcOverlaps()
 
         assertEquals(8111, overlaps)
     }
+
+    @Test
+    fun `run part 02`() {
+        val overlaps = getCoords().calcOverlaps(true)
+
+        assertEquals(22088, overlaps)
+    }
+
+    private fun getCoords() = Util.getInputAsListOfString("day05-input.txt")
+        .map {
+            it.split(" ", ",")
+                .let { s ->
+                    Coord(s[0].toInt(), s[1].toInt()) to Coord(s[3].toInt(), s[4].toInt())
+                }
+        }
+
+    private fun List<Pair<Coord, Coord>>.calcOverlaps(includeDiagonal: Boolean = false) = this
+        .flatMap {
+            if (it.first.x == it.second.x)
+                (it.first.y towards it.second.y).map { y -> Coord(it.first.x, y) }
+            else if (it.first.y == it.second.y)
+                (it.first.x towards it.second.x).map { x -> Coord(x, it.first.y) }
+            else if (includeDiagonal && abs(it.first.x - it.second.x) == abs(it.first.y - it.second.y))
+                (it.first.x towards it.second.x).mapIndexed { index, x ->
+                    Coord(x, (it.first.y towards it.second.y).elementAt(index))
+                }
+            else
+                listOf()
+        }
+        .groupingBy { it }
+        .eachCount()
+        .count { it.value >= 2 }
 
     private infix fun Int.towards(to: Int) =
         IntProgression.fromClosedRange(this, to, if (this > to) -1 else 1)
