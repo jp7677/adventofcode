a6d6833a1d8fe478238ec53a470bc32152d5c3f8
Jens Peters <jp7677@gmail.com>
2021-12-06 12:31:32 +0100

    2021: Use same solution for both parts

--- 2021/src/test/kotlin/day06.kt
+++ 2021/src/test/kotlin/day06.kt
@@ -5,44 +5,43 @@ class Day06 {
 
     @Test
     fun `run part 01`() {
-        val population = Util.getInputAsString("day06-input.txt")
-            .split(",").map { it.toInt() }
-            .toMutableList()
+        val countOfAllFish = getPopulation()
+            .runDays(80)
+            .sum()
 
-        repeat(80) { _ ->
-            val new = population.count { it == 0 }
-            population.onEachIndexed { index, it -> population[index] = if (it == 0) 6 else it - 1 }
-            repeat(new) { population.add(8) }
-        }
-
-        val countOfFish = population.size
-
-        assertEquals(350917, countOfFish)
+        assertEquals(350917, countOfAllFish)
     }
 
     @Test
     fun `run part 02`() {
+        val countOfAllFish = getPopulation()
+            .runDays(256)
+            .sum()
+
+        assertEquals(1592918715629, countOfAllFish)
+    }
+
+    private fun getPopulation(): LongArray {
         val ages = Util.getInputAsString("day06-input.txt")
             .split(",").map { it.toInt() }
             .toList()
 
-        val population = LongArray(9) { index -> ages.count { it == index }.toLong() }
+        return LongArray(9) { index -> ages.count { it == index }.toLong() }
+    }
 
-        repeat(256) {
-            val last = population.copyOf()
-            population[8] = last[0]
-            population[7] = last[8]
-            population[6] = last[7] + last[0]
-            population[5] = last[6]
-            population[4] = last[5]
-            population[3] = last[4]
-            population[2] = last[3]
-            population[1] = last[2]
-            population[0] = last[1]
+    private fun LongArray.runDays(days: Int): LongArray {
+        repeat(days) {
+            val last = this.copyOf()
+            this[8] = last[0]
+            this[7] = last[8]
+            this[6] = last[7] + last[0]
+            this[5] = last[6]
+            this[4] = last[5]
+            this[3] = last[4]
+            this[2] = last[3]
+            this[1] = last[2]
+            this[0] = last[1]
         }
-
-        val countOfAllFish = population.sum()
-
-        assertEquals(1592918715629, countOfAllFish)
+        return this
     }
 }
