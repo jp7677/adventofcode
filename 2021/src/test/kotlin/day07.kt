d96696bc3f5de4ba0d53f55eb0ffe8f47dde2b0d
Jens Peters <jp7677@gmail.com>
2021-12-07 07:50:07 +0100

    2021: Add day 7 part 2

--- 2021/src/test/kotlin/day07.kt
+++ 2021/src/test/kotlin/day07.kt
@@ -8,13 +8,28 @@ class Day07 {
     fun `run part 01`() {
         val positions = Util.getInputAsListOfInt("day07-input.txt", ",")
 
-        val min = positions.minOrNull() ?: throw IllegalStateException()
-        val max = positions.maxOrNull() ?: throw IllegalStateException()
-        val minDistance = (min..max)
+        val minDistance = getDistances(positions)
             .minOfOrNull { distance ->
-                positions.fold (0) { acc, it -> acc + abs(distance - it) }
+                positions.fold (0) { sum, it -> sum + abs(distance - it) }
             } ?: throw IllegalStateException()
 
         assertEquals(356992, minDistance)
     }
+
+    @Test
+    fun `run part 02`() {
+        val positions = Util.getInputAsListOfInt("day07-input.txt", ",")
+
+        val minDistance = getDistances(positions)
+            .minOfOrNull { distance ->
+                positions.fold (0) { sum, it ->
+                    sum + (1..abs(distance - it)).fold(0) { acc, i -> acc + i }
+                }
+            } ?: throw IllegalStateException()
+
+        assertEquals(101268110, minDistance)
+    }
+
+    private fun getDistances(positions: List<Int>) =
+        (positions.minOrNull() ?: throw IllegalStateException())..(positions.maxOrNull() ?: throw IllegalStateException())
 }
