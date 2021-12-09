a03a1f0f5600a3397e047d30f6932e230c7142a6
Jens Peters <jp7677@gmail.com>
2021-12-09 23:07:34 +0100

    2021: Use fold() Also formatting.

--- 2021/src/test/kotlin/day09.kt
+++ 2021/src/test/kotlin/day09.kt
@@ -2,8 +2,8 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Day09 {
-    data class Coord (val x: Int, val y: Int)
-    data class Location (val coord: Coord, val height: Int)
+    data class Coord(val x: Int, val y: Int)
+    data class Location(val coord: Coord, val height: Int)
     private val moves = listOf(Coord(-1, 0), Coord(1, 0), Coord(0, -1), Coord(0, 1))
 
     @Test
@@ -30,15 +30,14 @@ class Day09 {
     @Test
     fun `run part 02`() {
         val heatmap = getHeatmap()
-        val basins = mutableListOf<List<Coord>>()
 
-        heatmap
-            .onEach {
-                if (it.height != 9 && basins.none { b -> b.contains(it.coord) })
-                    basins.add(getAdjacentLowPoints(heatmap, it.coord).map { l -> l.coord })
+        val result = heatmap
+            .fold(listOf<List<Coord>>()) { acc, it ->
+                if (it.height != 9 && acc.none { b -> b.contains(it.coord) })
+                    acc + listOf(heatmap.getAdjacentLowPoints(it.coord).map { l -> l.coord })
+                else
+                    acc
             }
-
-        val result = basins
             .sortedByDescending { it.count() }
             .take(3)
             .fold(1) { acc, it -> acc * it.count() }
@@ -46,26 +45,24 @@ class Day09 {
         assertEquals(987840, result)
     }
 
-    private fun getAdjacentLowPoints(
-        heatmap: List<Location>,
+    private fun List<Location>.getAdjacentLowPoints(
         coord: Coord,
         visited: MutableList<Coord> = mutableListOf()
-    ): List<Location> {
-        if (visited.contains(coord) || heatmap.from(coord).height == 9)
-            return mutableListOf()
-
-        visited.add(coord)
-        return moves
-            .mapNotNull { heatmap.getAdjacents(coord, it) }
-            .flatMap { getAdjacentLowPoints(heatmap, it.coord, visited) }
-            .toMutableList() + heatmap.from(coord)
-    }
+    ): List<Location> =
+        if (visited.contains(coord) || this.from(coord).height == 9)
+            listOf()
+        else {
+            visited.add(coord)
+            moves
+                .mapNotNull { this.getAdjacents(coord, it) }
+                .flatMap { this.getAdjacentLowPoints(it.coord, visited) } + this.from(coord)
+        }
 
     private fun List<Location>.from(coord: Coord) = this
         .single { it.coord.x == coord.x && it.coord.y == coord.y }
 
     private fun List<Location>.getAdjacents(current: Coord, step: Coord) =
-        this.firstOrNull { it.coord.x == current.x + step.x &&  it.coord.y == current.y + step.y }
+        this.firstOrNull { it.coord.x == current.x + step.x && it.coord.y == current.y + step.y }
 
     private fun getHeatmap() = Util.getInputAsListOfString("day09-input.txt")
         .map { it.map { c -> c.digitToInt() } }
