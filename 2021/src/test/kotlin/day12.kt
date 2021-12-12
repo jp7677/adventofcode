d263741ea30fc98ad4a52eb79c28484be6fb2216
Jens Peters <jp7677@gmail.com>
2021-12-12 19:29:55 +0100

    2021: Simplify and renames

--- 2021/src/test/kotlin/day12.kt
+++ 2021/src/test/kotlin/day12.kt
@@ -2,72 +2,64 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Day12 {
-    data class Path(val from: String, val to: String)
-    data class Node(val from: String, val tos: List<String>)
+    data class Connection(val from: String, val to: String)
 
     @Test
     fun `run part 01`() {
-        val tree = getCavePathsTree()
+        val connections = getCaveConnections()
 
-        val journeys = mutableListOf<List<String>>()
-        tree.walk("start", 1, listOf("START"), journeys)
+        val path = mutableListOf<List<String>>()
+        connections.buildAllPaths("start", 1, path)
 
-        val count = journeys
-            .onEach { println(it) }
-            .count()
+        val count = path.count()
 
         assertEquals(4720, count)
     }
 
     @Test
     fun `run part 02`() {
-        val tree = getCavePathsTree()
+        val connections = getCaveConnections()
 
-        val journeys = mutableListOf<List<String>>()
-        tree.walk("start", 2, listOf("START"), journeys)
+        val paths = mutableListOf<List<String>>()
+        connections.buildAllPaths("start", 2, paths)
 
-        val count = journeys.count { it.none { s -> s == "start" } }
+        val count = paths.count()
 
         assertEquals(147848, count)
     }
 
-    private fun Set<Node>.walk(element: String, maxCount: Int = 1, journey: List<String> = listOf(), acc: MutableList<List<String>>) {
+    private fun Set<Connection>.buildAllPaths(
+        element: String,
+        maxVisits: Int = 1,
+        acc: MutableList<List<String>>,
+        path: List<String> = listOf()
+    ) {
         if (element == "end")
-            acc.add(journey)
+            acc += path
 
-        if (element == "end"
-            || (journey
-                    .filter { it.all { c -> c.isLowerCase() } }
-                    .groupingBy { it }.eachCount()
-                    .let {
-                        it.any { c -> c.value > maxCount } || (it.count { c -> c.value == 2 } > 1)
-                    }))
+        if (element == "end" || path.maxVisitsExceeded(maxVisits))
             return
 
         this
-            .single { it.from == element }
-            .tos.forEach { to -> this.walk(to, maxCount, journey + to, acc) }
+            .filter { it.from == element  }
+            .map { it.to }
+            .forEach { to -> this.buildAllPaths(to, maxVisits, acc, path + to) }
     }
 
-    private fun getCavePathsTree(): Set<Node> {
-        val cavesMap = Util.getInputAsListOfString("day12-input.txt")
-            .map { it.split("-").let { s -> Path(s.first(), s.last()) } }
+    private fun List<String>.maxVisitsExceeded(maxVisits: Int) = this
+        .filter { it.all { c -> c.isLowerCase() } }
+        .groupingBy { it }.eachCount()
+        .let {
+            it.any { c -> c.value > maxVisits } || (it.count { c -> c.value == 2 } > 1)
+        }
 
-        val paths = cavesMap
-            .filter { it.from != "start" && it.to != "end" }
-            .map { Path(it.to, it.from) }
-            .union(cavesMap)
-
-        return paths
-            .map { paths.getDestinations(it.from) }
-            .distinct()
-            .toSet()
-    }
-
-    private fun Set<Path>.getDestinations(element: String) =
-        Node(
-            element,
-            this
-                .filter { it.from == element }
-                .map { it.to })
+    private fun getCaveConnections(name: String = "day12-input.txt") = Util.getInputAsListOfString(name)
+        .map { it.split("-").let { s -> Connection(s.first(), s.last()) } }
+        .let { connections ->
+            connections
+                .map { Connection(it.to, it.from) }
+                .union(connections)
+                .filter { it.from != "end" && it.to != "start" }
+                .toSet()
+        }
 }
