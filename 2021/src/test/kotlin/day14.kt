ea0c7d9df16b9779c00a729b1418501bf415e4d4
Jens Peters <jp7677@gmail.com>
2021-12-18 19:50:30 +0100

    2021: Use part 2 solution for part 1 Also use functional approach for generating map with links

--- 2021/src/test/kotlin/day14.kt
+++ 2021/src/test/kotlin/day14.kt
@@ -7,27 +7,7 @@ class Day14 {
     fun `run part 01`() {
         val (template, insertions) = getInstructions()
 
-        val result = generateSequence(template) { intermediateTemplate ->
-            intermediateTemplate
-                .toList()
-                .windowed(2, 1)
-                .joinToString("") {
-                    it[0].toString() + insertions[it.joinToString("")]
-                } + intermediateTemplate.last()
-        }
-            .take(10 + 1)
-            .last()
-            .groupingBy { it }
-            .eachCount()
-            .let { counts ->
-                counts
-                    .map { it.value }
-                    .let {
-                        (it.maxOrNull() ?: throw IllegalStateException()) -
-                            (it.minOrNull() ?: throw IllegalStateException())
-                    }
-
-            }
+        val result = runSteps(template, insertions, 10)
 
         assertEquals(2435, result)
     }
@@ -36,34 +16,38 @@ class Day14 {
     fun `run part 02`() {
         val (template, insertions) = getInstructions()
 
-        val result = template
-            .windowed(2, 1)
-            .associateWith { 1L }
-            .let { templateLinks ->
-                generateSequence(templateLinks) { links ->
-                    val acc = mutableMapOf<String, Long>()
-                    links
-                        .forEach {
-                            val added = insertions[it.key]?.toString() ?: throw java.lang.IllegalStateException()
-                            acc[it.key.first() + added] = (acc[it.key.first() + added] ?: 0) + it.value
-                            acc[added + it.key.last()] = (acc[added + it.key.last()] ?: 0) + it.value
-                        }
-                    acc
-                }
-            }
-            .take(40 + 1)
-            .last()
-            .map { it.key.last() to it.value }
-            .groupBy { it.first }
-            // This is not completely correct, the totals for the first link should be counted for both characters
-            // Fortunately this is not the character we are interested in, so fine here
-            .map { it.key to it.value.sumOf { count -> count.second } }
-            .let {
-                it.maxOf { count -> count.second } - it.minOf { count -> count.second }
-            }
+        val result = runSteps(template, insertions, 40)
 
         assertEquals(2587447599164, result)
     }
+
+    private fun runSteps(template: String, insertions: Map<String, Char>, steps: Int) = template
+        .windowed(2, 1)
+        .associateWith { 1L }
+        .let { templateLinks ->
+            generateSequence(templateLinks) { links ->
+                links
+                    .flatMap {
+                        val insert = insertions[it.key]?.toString() ?: throw java.lang.IllegalStateException()
+                        listOf(
+                            (it.key.first() + insert) to it.value,
+                            (insert + it.key.last()) to it.value)
+                    }
+                    .groupBy { it.first }
+                    .map { it.key to it.value.sumOf { count -> count.second } }
+                    .toMap()
+            }
+        }
+        .take(steps + 1)
+        .last()
+        .map { it.key.last() to it.value }
+        .groupBy { it.first }
+        // This is not completely correct, the totals for the first link should be counted for both characters
+        // Fortunately this is not the character we are interested in, so fine here
+        .map { it.key to it.value.sumOf { count -> count.second } }
+        .let {
+            it.maxOf { count -> count.second } - it.minOf { count -> count.second }
+        }
 
     private fun getInstructions() = Util.getInputAsListOfString("day14-input.txt")
         .let { paper ->
