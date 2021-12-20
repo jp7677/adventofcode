5d6dcbbbbe8f174b4d179f7b3da8200637a5ea17
Jens Peters <jp7677@gmail.com>
2021-12-20 23:22:24 +0100

    2021: Renames and formatting

--- 2021/src/test/kotlin/day15.kt
+++ 2021/src/test/kotlin/day15.kt
@@ -8,42 +8,39 @@ class Day15 {
 
     @Test
     fun `run part 01`() {
-        val grid = getGrid()
+        val map = getMap()
 
-        val totalRisk = findTotalRiskForShortestPath(grid)
+        val totalRisk = findTotalRiskForShortestPath(map)
 
         assertEquals(720, totalRisk)
     }
 
     @Test
     fun `run part 02`() {
-        val grid = getGrid()
+        val map = getMap()
 
-        val maxX = grid.maxOf { it.key.x }
-        val maxY = grid.maxOf { it.key.y }
-
-        val additionalRight = grid.flatMap {
-            (1..4).map { x ->
-                Coord(it.key.x + (x * (maxX + 1)), it.key.y) to
-                    if (it.value + x > 9) (it.value + x) - 9 else it.value + x
+        val additionalRight = map
+            .flatMap {
+                (1..4).map { x ->
+                    Coord(it.key.x + (x * (map.maxX() + 1)), it.key.y) to
+                        if (it.value + x > 9) (it.value + x) - 9 else it.value + x
+                }
             }
-        }
 
-        val additionalDown = (grid + additionalRight).flatMap {
-            (1..4).map { y ->
-                Coord(it.key.x, it.key.y + (y * (maxY + 1))) to
-                    if (it.value + y > 9) (it.value + y) - 9 else it.value + y
+        val additionalDown = (map + additionalRight)
+            .flatMap {
+                (1..4).map { y ->
+                    Coord(it.key.x, it.key.y + (y * (map.maxY() + 1))) to
+                        if (it.value + y > 9) (it.value + y) - 9 else it.value + y
+                }
             }
-        }
 
-        val totalRisk = findTotalRiskForShortestPath(grid + additionalRight + additionalDown)
+        val totalRisk = findTotalRiskForShortestPath(map + additionalRight + additionalDown)
 
         assertEquals(3025, totalRisk)
     }
 
-    private fun findTotalRiskForShortestPath(grid: Map<Coord, Int>): Int {
-        val maxX = grid.maxOf { it.key.x }
-        val maxY = grid.maxOf { it.key.y }
+    private fun findTotalRiskForShortestPath(map: Map<Coord, Int>): Int {
         val visited = mutableSetOf<Coord>()
         val totalDistances = mutableMapOf(Coord(0,0) to 0)
         val queue = mutableSetOf(Coord(0,0))
@@ -56,7 +53,7 @@ class Day15 {
             directions
                 .mapNotNull { direction ->
                     val coord = Coord(current.key.x + direction.x, current.key.y + direction.y)
-                    grid[coord]?.let { coord to it }
+                    map[coord]?.let { coord to it }
                 }
                 .filterNot { visited.contains(it.first)  }
                 .forEach { coord ->
@@ -71,10 +68,13 @@ class Day15 {
             queue.remove(current.key)
         }
 
-        return totalDistances[Coord(maxX, maxY)] ?: throw IllegalStateException()
+        return totalDistances[Coord(map.maxX(), map.maxY())] ?: throw IllegalStateException()
     }
 
-    private fun getGrid() = Util.getInputAsListOfString("day15-input.txt")
+    private fun Map<Coord, Int>.maxX() = this.maxOf { it.key.x }
+    private fun Map<Coord, Int>.maxY() = this.maxOf { it.key.y }
+
+    private fun getMap() = Util.getInputAsListOfString("day15-input.txt")
         .map { it.map { c -> c.digitToInt() } }
         .let {
             it
