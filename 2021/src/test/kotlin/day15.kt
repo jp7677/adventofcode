3a971def4ee6591351a25b5376d3ebdaeb9f3a6a
Jens Peters <jp7677@gmail.com>
2021-12-20 23:13:51 +0100

    2021: Add day 15 part 2

--- 2021/src/test/kotlin/day15.kt
+++ 2021/src/test/kotlin/day15.kt
@@ -4,48 +4,74 @@ import kotlin.test.assertEquals
 
 class Day15 {
     data class Coord(val x: Int, val y: Int)
-    data class Risk(val coord: Coord, val level: Int)
-
-    data class Travel(val distance: Int?, val previous: Coord?)
-    private val directions = listOf(Coord(0, -1), Coord(1, 0), Coord(0, 1), Coord(-1, -1))
+    private val directions = listOf(Coord(1, 0), Coord(0, 1), Coord(-1, 0), Coord(0, -1))
 
     @Test
     fun `run part 01`() {
         val grid = getGrid()
 
-        val nodes = mutableMapOf<Coord, Travel>()
-        grid.forEach { nodes[it.coord] = Travel(null, null) }
+        val totalRisk = findTotalRiskForShortestPath(grid)
 
-        nodes[Coord(0, 0)] = Travel(0, null)
-        while (nodes.any { it.value.distance == null }) {
-            val current = nodes.minByOrNull { it.value.distance ?: Int.MAX_VALUE } ?: throw IllegalStateException()
+        assertEquals(720, totalRisk)
+    }
+
+    @Test
+    fun `run part 02`() {
+        val grid = getGrid()
+
+        val maxX = grid.maxOf { it.key.x }
+        val maxY = grid.maxOf { it.key.y }
+
+        val additionalRight = grid.flatMap {
+            (1..4).map { x ->
+                Coord(it.key.x + (x * (maxX + 1)), it.key.y) to
+                    if (it.value + x > 9) (it.value + x) - 9 else it.value + x
+            }
+        }
+
+        val additionalDown = (grid + additionalRight).flatMap {
+            (1..4).map { y ->
+                Coord(it.key.x, it.key.y + (y * (maxY + 1))) to
+                    if (it.value + y > 9) (it.value + y) - 9 else it.value + y
+            }
+        }
+
+        val totalRisk = findTotalRiskForShortestPath(grid + additionalRight + additionalDown)
+
+        assertEquals(3025, totalRisk)
+    }
+
+    private fun findTotalRiskForShortestPath(grid: Map<Coord, Int>): Int {
+        val maxX = grid.maxOf { it.key.x }
+        val maxY = grid.maxOf { it.key.y }
+        val visited = mutableSetOf<Coord>()
+        val totalDistances = mutableMapOf(Coord(0,0) to 0)
+        val queue = mutableSetOf(Coord(0,0))
+
+        while (queue.any()) {
+            val current = queue
+                .associateWith { totalDistances[it] }
+                .minByOrNull { it.value ?: 0 } ?: throw IllegalStateException()
 
             directions
                 .mapNotNull { direction ->
-                    grid.singleOrNull { risk ->
-                        risk.coord.x == current.key.x + direction.x && risk.coord.y == current.key.y + direction.y
-                    }
-                        ?.coord
-                }.forEach { coord ->
-                    nodes[coord]
-                        ?.let { node ->
-                            val distance = (current.value.distance ?: 0) + grid.single { it.coord == coord }.level
-                            if ((node.distance ?: Int.MAX_VALUE ) > distance) {
-                                nodes[coord] = Travel(distance, current.key)
-                            }
-                        }
+                    val coord = Coord(current.key.x + direction.x, current.key.y + direction.y)
+                    grid[coord]?.let { coord to it }
+                }
+                .filterNot { visited.contains(it.first)  }
+                .forEach { coord ->
+                    val distance = (totalDistances[current.key] ?: 0) + coord.second
+                    if ((totalDistances[coord.first] ?: Int.MAX_VALUE) > distance)
+                        totalDistances[coord.first] = distance
+
+                    queue.add(coord.first)
                 }
 
-            if (current.key == Coord(99, 99))
-                break
-
-            nodes.remove(current.key)
+            visited.add(current.key)
+            queue.remove(current.key)
         }
 
-        val totalRisk = nodes[Coord(99,99)]?.distance
-
-        assertEquals(720, totalRisk)
-
+        return totalDistances[Coord(maxX, maxY)] ?: throw IllegalStateException()
     }
 
     private fun getGrid() = Util.getInputAsListOfString("day15-input.txt")
@@ -55,8 +81,8 @@ class Day15 {
                 .first()
                 .indices
                 .flatMap { x ->
-                    List(it.size) { y -> Risk(Coord(x, y), it[y][x]) }
+                    List(it.size) { y -> Coord(x, y) to it[y][x] }
                 }
         }
-        .toSet()
+        .toMap()
 }
