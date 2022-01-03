90f5dfa63ea92274fdbbc034d9688de68904c960
Jens Peters <jp7677@gmail.com>
2022-01-03 20:15:49 +0100

    2021: Add day 20 part 2

--- 2021/src/test/kotlin/day20.kt
+++ 2021/src/test/kotlin/day20.kt
@@ -11,70 +11,79 @@ class Day20 {
 
     @Test
     fun `run part 01`() {
-        val (algorithm, image) = Util.getInputAsListOfString("day20-input.txt")
-            .let {
-                it.first().map { c -> c == '#' } to it
-                    .drop(2)
-                    .let { image ->
-                        image.first()
-                            .indices
-                            .flatMap { x ->
-                                List(image.size) { y -> if (image[y][x] == '#') Pixel(x, y) else null }
-                            }
-                    }
-                    .mapNotNull { pixel -> pixel }
-                    .toSet()
-            }
+        val (algorithm, image) = getProcessingData()
 
-        val processed = generateSequence(image) {
+        val count = image.processImage(algorithm, 2)
+            .count()
+
+        assertEquals(5498, count)
+    }
+
+    @Test
+    fun `run part 02`() {
+        val (algorithm, image) = getProcessingData()
+
+        val count = image.processImage(algorithm, 50)
+            .count()
+
+        assertEquals(16014, count)
+    }
+
+    private fun Set<Pixel>.processImage(algorithm: Array<Boolean>, rounds: Int) =
+        generateSequence(this to false) { (it, isLit) ->
+            val minX = it.minX()
+            val maxX = it.maxX()
+            val minY = it.minY()
+            val maxY = it.maxY()
+
             val processed = mutableSetOf<Pixel>()
-            (it.minY()..it.maxY()).forEach { y ->
-                (it.minX()..it.maxX()).forEach { x ->
-                    val index = it.getIndex(x ,y)
+            ((it.minY() - 1)..(it.maxY() + 1)).forEach { y ->
+                ((it.minX() - 1)..(it.maxX() + 1)).forEach { x ->
+                    val index = it.getIndex(x, y, minX, maxX, minY, maxY, isLit)
                     if (algorithm[index]) processed.add(Pixel(x, y))
                 }
             }
-            processed
+            processed to (if (isLit) algorithm.last() else algorithm.first())
         }
-            .take(2 + 1)
+            .map { it.first }
+            .take(rounds + 1)
             .last()
+
+    private fun Set<Pixel>.getIndex(x: Int, y: Int, minX: Int, maxX: Int, minY: Int, maxY: Int, isLit: Boolean) =
+        processingInput
+            .map {
+                if (((x + it.x) in minX..maxX) && ((y + it.y) in minY..maxY)) {
+                    if (this.contains(Pixel(x + it.x, y + it.y)))
+                        '1'
+                    else
+                        '0'
+                } else if (isLit)
+                    '1'
+                else
+                    '0'
+            }
+            .joinToString("")
             .let {
-                val minX = it.minX()
-                val maxX = it.maxX()
-                val minY = it.minY()
-                val maxY = it.maxY()
-
-                it.filterNot { pixel ->
-                    pixel.x in (minX..(minX+10)) ||
-                    pixel.x in ((maxX-10)..maxX) ||
-                    pixel.y in (minY..(minY+10)) ||
-                    pixel.y in ((maxY-10)..maxY)
-                }.toSet()
+                Integer.parseInt(it, 2)
             }
 
-        val lightPixelCount = processed.count()
+    private fun Set<Pixel>.minX() = this.minOf { it.x }
+    private fun Set<Pixel>.maxX() = this.maxOf { it.x }
+    private fun Set<Pixel>.minY() = this.minOf { it.y }
+    private fun Set<Pixel>.maxY() = this.maxOf { it.y }
 
-        assertEquals(5498, lightPixelCount)
-    }
-
-    private fun Set<Pixel>.getIndex(x: Int, y: Int) = processingInput.map {
-        if (this.contains(Pixel(x + it.x, y + it.y))) '1' else '0'
-    }
-        .joinToString("")
+    private fun getProcessingData() = Util.getInputAsListOfString("day20-input.txt")
         .let {
-            Integer.parseInt(it, 2)
+            it.first().map { c -> c == '#' }.toTypedArray() to it
+                .drop(2)
+                .let { image ->
+                    image.first()
+                        .indices
+                        .flatMap { x ->
+                            List(image.size) { y -> if (image[y][x] == '#') Pixel(x, y) else null }
+                        }
+                }
+                .mapNotNull { pixel -> pixel }
+                .toSet()
         }
-
-    private fun Set<Pixel>.debug() =
-        (this.minY()..this.maxY()).forEach { y ->
-            (this.minX()..this.maxX()).forEach { x ->
-                if (this.contains(Pixel(x, y))) print('#') else print('.')
-            }
-            println()
-        }
-
-    private fun Set<Pixel>.minX() = this.minOf { it.x - 5 }
-    private fun Set<Pixel>.maxX() = this.maxOf { it.x + 5 }
-    private fun Set<Pixel>.minY() = this.minOf { it.y - 5 }
-    private fun Set<Pixel>.maxY() = this.maxOf { it.y + 5 }
 }
