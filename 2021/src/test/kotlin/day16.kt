8c6d556f2cd247d1cf18cd14d02648b9f3dae92f
Jens Peters <jp7677@gmail.com>
2022-01-17 20:15:36 +0100

    2021: Use infix to create triples

--- 2021/src/test/kotlin/day16.kt
+++ 2021/src/test/kotlin/day16.kt
@@ -36,7 +36,7 @@ class Day16 {
         else
             getLiteral(bits, idx1).also { idx1 += it.second }.first
 
-        return Triple(version, value, idx1 - idx)
+        return version to value to idx1 - idx
     }
 
     private fun getVersion(bits: String, idx: Int) = bits
@@ -72,16 +72,12 @@ class Day16 {
             else -> throw IllegalStateException()
         }
 
-        return Triple(value, packets.sumOf { it.first }, idx1 - idx)
+        return value to packets.sumOf { it.first } to idx1 - idx
     }
 
     private fun getLength(bits: String, idx: Int): Triple<Boolean, Int, Int> {
         val length = if (bits[idx] == '0') 16 else 12
-        return Triple(
-            bits[idx] == '0',
-            bits.substring(idx.inc() until idx + length).toInt(2),
-            length
-        )
+        return (bits[idx] == '0') to bits.substring(idx.inc() until idx + length).toInt(2) to length
     }
 
     private fun getLiteral(bits: String, idx: Int): Pair<Long, Int> {
@@ -103,7 +99,7 @@ class Day16 {
     private fun getLiteralGroup(bits: String, idx: Int) =
         bits.substring(idx until idx + 5)
             .let {
-                Triple(it[0] == '1', it.substring(1), 5)
+                (it[0] == '1') to it.substring(1) to 5
             }
 
     private fun getTransmissionBits() = Util.getInputAsString("day16-input.txt")
@@ -115,4 +111,6 @@ class Day16 {
                 .toString(2)
                 .padStart(4, '0')
         }
+
+    infix fun <X : Pair<A, B>, A, B, C> X.to(that: C) = Triple(this.first, this.second, that)
 }
