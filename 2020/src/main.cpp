055800a15c8a43e7803640e4ff28a19c7c696600
Jens Peters <jp7677@gmail.com>
2022-07-02 22:06:41 +0200

    2020: Use namespace

--- 2020/src/main.cpp
+++ 2020/src/main.cpp
@@ -2,8 +2,10 @@
 #define CATCH_CONFIG_NO_POSIX_SIGNALS
 #include "../inc/catch.hpp"
 
-#define BACKWARD_HAS_LIBUNWIND 1
 #define BACKWARD_HAS_DW 1
+#define BACKWARD_HAS_LIBUNWIND 1
 #include "../inc/backward.hpp"
 
-[[maybe_unused]] backward::SignalHandling sh;
+namespace aoc {
+    [[maybe_unused]] backward::SignalHandling sh;
+}
