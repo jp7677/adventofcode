5fd5a687df049918d67e395e1dce7e8ee6ae5b7f
Jens Peters <jp7677@gmail.com>
2022-07-02 21:51:26 +0200

    2020: Add backward-cpp for better stack traces Configure backward with libunwind and libdw (from elfutils).

--- 2020/src/main.cpp
+++ 2020/src/main.cpp
@@ -1,2 +1,8 @@
 #define CATCH_CONFIG_MAIN
 #include "../inc/catch.hpp"
+
+#define BACKWARD_HAS_LIBUNWIND 1
+#define BACKWARD_HAS_DW 1
+#include "../inc/backward.hpp"
+
+[[maybe_unused]] backward::SignalHandling sh;
