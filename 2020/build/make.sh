1f11af674daa132034408bbac2a736ebde32f5ec
Jens Peters <jp7677@gmail.com>
2022-07-02 21:52:36 +0200

    2020: Include debug info in release build

--- 2020/build/make.sh
+++ 2020/build/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-cmake -DCMAKE_BUILD_TYPE=Release -B./cmake-build-release
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -B./cmake-build-release
 cmake --build ./cmake-build-release --target all -- -j 9
