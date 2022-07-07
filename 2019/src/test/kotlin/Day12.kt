ec602d739391634fd2a025da7e40af3142e68eef
Jens Peters <jp7677@gmail.com>
2022-07-08 01:13:55 +0200

    2019: Add day 12 part 2

--- 2019/src/test/kotlin/Day12.kt
+++ 2019/src/test/kotlin/Day12.kt
@@ -31,25 +31,56 @@ class Day12 {
 
     @Test
     fun runPart01() {
-        val re = "-?\\d+".toRegex()
-        var moons = Util.getInputAsListOfString("day12-input.txt")
-            .map {
-                val (x, y, z) = re.findAll(it)
-                    .map { match -> match.value.toInt() }
-                    .toList()
-
-                Moon(Vector(x, y, z), Vector(0, 0, 0))
-            }
+        var moons = getMoons()
 
         repeat(1000) {
-            moons = moons.map { moon ->
-                val velocity = moon.applyGravity(moons - moon)
-                Moon(moon.applyVelocity(velocity), velocity)
-            }
+            moons = turn(moons)
         }
 
         val energy = moons.sumOf { it.position.energy() * it.velocity.energy() }
 
         assertEquals(9743, energy)
     }
+
+    @Test
+    fun runPart02() {
+        var moons = getMoons()
+
+        var rx = 0L
+        var ry = 0L
+        var rz = 0L
+
+        var round = 0L
+        while (rx == 0L || ry == 0L || rz == 0L) {
+            moons = turn(moons).also { round++ }
+
+            if (moons.sumOf { abs(it.velocity.x) } == 0 && rx == 0L)
+                rx = round
+            if (moons.sumOf { abs(it.velocity.y) } == 0 && ry == 0L)
+                ry = round
+            if (moons.sumOf { abs(it.velocity.z) } == 0 && rz == 0L)
+                rz = round
+        }
+
+        val previousStateAt = 2 * rx.lcm(ry).lcm(rz)
+
+        assertEquals(288684633706728, previousStateAt)
+    }
+
+    private fun turn(moons: List<Moon>) = moons
+        .map { moon ->
+            val velocity = moon.applyGravity(moons - moon)
+            Moon(moon.applyVelocity(velocity), velocity)
+        }
+
+    private fun getMoons() = Util.getInputAsListOfString("day12-input.txt")
+        .map {
+            val (x, y, z) = re.findAll(it)
+                .map { match -> match.value.toInt() }
+                .toList()
+
+            Moon(Vector(x, y, z), Vector(0, 0, 0))
+        }
+
+    private val re = "-?\\d+".toRegex()
 }
