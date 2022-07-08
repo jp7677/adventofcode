117d0bf9dc6b46334a27454b40f3caeaf22dab0d
Jens Peters <jp7677@gmail.com>
2022-07-08 22:20:45 +0200

    2021: Update packages

--- 2021/build.gradle.kts
+++ 2021/build.gradle.kts
@@ -3,14 +3,14 @@ version = "0.1-SNAPSHOT"
 
 plugins {
     application
-    kotlin("jvm") version "1.6.10"
-    id("com.adarshr.test-logger") version "3.1.0"
-    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+    kotlin("jvm") version "1.7.10"
+    id("com.adarshr.test-logger") version "3.2.0"
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
 }
 
 repositories { mavenCentral() }
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.6.10")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.7.0")
 }
 
 application { mainClass.set("MainKt") }
