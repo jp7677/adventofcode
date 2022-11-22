375e624182ccc069d853a9bd9a83c804b41a6257
Jens Peters <jp7677@gmail.com>
2022-11-22 20:31:58 +0100

    2021: Improve readability

--- 2021/build.gradle.kts
+++ 2021/build.gradle.kts
@@ -3,7 +3,7 @@ version = "0.1-SNAPSHOT"
 
 plugins {
     application
-    kotlin("jvm") version "1.7.20"
+    kotlin("jvm") version "1.7.21"
     id("com.adarshr.test-logger") version "3.2.0"
     id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
 }
@@ -15,10 +15,12 @@ dependencies {
 
 application { mainClass.set("MainKt") }
 testlogger { theme = com.adarshr.gradle.testlogger.theme.ThemeType.PLAIN }
-tasks.jar { manifest { attributes["Main-Class"] = "MainKt" } }
-tasks.compileKotlin { kotlinOptions.jvmTarget = "17" }
-tasks.compileTestKotlin { kotlinOptions.jvmTarget = "17" }
-tasks.test {
-    useJUnitPlatform()
-    testLogging { showStandardStreams = true }
+tasks {
+    jar { manifest { attributes["Main-Class"] = "MainKt" } }
+    compileKotlin { kotlinOptions.jvmTarget = "17" }
+    compileTestKotlin { kotlinOptions.jvmTarget = "17" }
+    test {
+        useJUnitPlatform()
+        testLogging { showStandardStreams = true }
+    }
 }
