ee6024b9df9d27089244642d5291839f2e092198
Jens Peters <jp7677@gmail.com>
2022-12-01 13:28:41 +0100

    2022: Use method

--- 2022/src/test/kotlin/Day01.kt
+++ 2022/src/test/kotlin/Day01.kt
@@ -2,7 +2,7 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class Day01 : StringSpec({
-    val calories = Util.getInputAsListOfString("day01-input.txt")
+    fun getCalories() = Util.getInputAsListOfString("day01-input.txt")
         .fold(arrayOf(0)) { acc, it ->
             if (it.isNotEmpty())
                 acc.apply { this[lastIndex] += it.toInt() }
@@ -11,13 +11,13 @@ class Day01 : StringSpec({
         }
 
     "puzzle part 01" {
-        val maxCalories = calories.maxOf { it }
+        val maxCalories = getCalories().maxOf { it }
 
         maxCalories shouldBe 70374
     }
 
     "puzzle part 02" {
-        val max3Calories = calories
+        val max3Calories = getCalories()
             .sortedByDescending { it }
             .take(3)
             .sumOf { it }
