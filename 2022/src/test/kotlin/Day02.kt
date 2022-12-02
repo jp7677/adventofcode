999ac67f1bab62de2b008444caf013c356bd398f
Jens Peters <jp7677@gmail.com>
2022-12-02 09:14:22 +0100

    2022: Add day 02 part 2

--- 2022/src/test/kotlin/Day02.kt
+++ 2022/src/test/kotlin/Day02.kt
@@ -1,36 +1,84 @@
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-enum class Shape { ROCK, PAPER, SCISSORS;
+enum class Outcome {
+    WIN, LOOSE, DRAW;
+
     companion object {
-        fun from(input: String): Shape =
-            when(input) {
+        fun from(input: String) =
+            when (input) {
+                "X" -> LOOSE
+                "Y" -> DRAW
+                "Z" -> WIN
+                else -> throw IllegalArgumentException(input)
+            }
+    }
+
+    fun score() = when (this) {
+        WIN -> 6
+        LOOSE -> 0
+        DRAW -> 3
+    }
+}
+
+enum class Shape {
+    ROCK, PAPER, SCISSORS;
+
+    companion object {
+        fun from(input: String) =
+            when (input) {
                 "A", "X" -> ROCK
                 "B", "Y" -> PAPER
                 "C", "Z" -> SCISSORS
                 else -> throw IllegalArgumentException(input)
             }
-        }
-
-    fun wonOf(you: Shape): Boolean =
-        if (this == ROCK && you == SCISSORS) true
-        else if (this == SCISSORS && you == PAPER) true
-        else if (this == PAPER && you == ROCK) true
-        else false
-}
-
-data class Game(val other: Shape, val you: Shape) {
-    fun scoreForSelected() = when (you) {
-        Shape.ROCK -> 1
-        Shape.PAPER -> 2
-        Shape.SCISSORS -> 3
     }
 
-    fun scoreForOutcome() =
-        if (other == you) 3
-        else if (you.wonOf(other)) 6
-        else if (other.wonOf(you)) 0
-        else throw IllegalStateException()
+    fun score() = when (this) {
+        ROCK -> 1
+        PAPER -> 2
+        SCISSORS -> 3
+    }
+}
+
+data class Game(val other: Shape, val you: Shape?, val outcome: Outcome?) {
+    fun scoreForSelected() = you?.score() ?: throw IllegalStateException()
+    fun scoreForOutcome() = outcome?.score() ?: throw IllegalStateException()
+
+    fun calcOutcome() = Game(
+        other,
+        you,
+        when (you) {
+            other.forWin() -> Outcome.WIN
+            other.forLoose() -> Outcome.LOOSE
+            else -> Outcome.DRAW
+        }
+    )
+
+    fun decrypt() = Game(
+        other,
+        when (outcome) {
+            Outcome.DRAW -> other
+            Outcome.WIN -> other.forWin()
+            Outcome.LOOSE -> other.forLoose()
+            else -> throw IllegalStateException()
+        },
+        outcome
+    )
+
+    private fun Shape.forWin() =
+        when (this) {
+            Shape.ROCK -> Shape.PAPER
+            Shape.PAPER -> Shape.SCISSORS
+            Shape.SCISSORS -> Shape.ROCK
+        }
+
+    private fun Shape.forLoose() =
+        when (this) {
+            Shape.ROCK -> Shape.SCISSORS
+            Shape.PAPER -> Shape.ROCK
+            Shape.SCISSORS -> Shape.PAPER
+        }
 }
 
 class Day02 : StringSpec({
@@ -38,9 +86,20 @@ class Day02 : StringSpec({
     "puzzle part 01" {
         val totalScore = Util.getInputAsListOfString("day02-input.txt")
             .map { it.split(' ') }
-            .map { Game(Shape.from(it.first()), Shape.from(it.last())) }
+            .map { Game(Shape.from(it.first()), Shape.from(it.last()), null) }
+            .map { it.calcOutcome() }
             .sumOf { it.scoreForOutcome() + it.scoreForSelected() }
 
         totalScore shouldBe 14531
+    }
+
+    "puzzle part 02" {
+        val totalScore = Util.getInputAsListOfString("day02-input.txt")
+            .map { it.split(' ') }
+            .map { Game(Shape.from(it.first()), null, Outcome.from(it.last())) }
+            .map { it.decrypt() }
+            .sumOf { it.scoreForOutcome() + it.scoreForSelected() }
+
+        totalScore shouldBe 11258
     }
 })
