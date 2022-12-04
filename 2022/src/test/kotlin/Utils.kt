18e0af9368eeb1bc447afd48a3c3f4a7f13732b4
Jens Peters <jp7677@gmail.com>
2022-12-04 19:26:52 +0100

    2022: Get puzzle input as sequence

--- 2022/src/test/kotlin/Utils.kt
+++ 2022/src/test/kotlin/Utils.kt
@@ -1,12 +1,10 @@
-fun getPuzzleInputAsListOfInt(name: String, separator: String = System.lineSeparator()) =
-    getPuzzleInput(name, separator).map(String::toInt)
-
-fun getPuzzleInputAsListOfLong(name: String, separator: String = System.lineSeparator()) =
-    getPuzzleInput(name, separator).map(String::toLong)
-
 fun getPuzzleInput(name: String, separator: String = System.lineSeparator()) =
-    getPuzzleInputAsString(name).split(separator)
-
-fun getPuzzleInputAsString(name: String) =
-    object {}::class.java.getResource(name)?.readText()
+    object {}::class.java.getResourceAsStream(name)
+        ?.bufferedReader()
+        ?.let {
+            if (separator == System.lineSeparator())
+                it.lineSequence()
+            else
+                it.readText().split(separator).asSequence()
+        }
         ?: throw Exception("Invalid input name")
