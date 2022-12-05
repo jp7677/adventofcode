63d4210f3b4d5354b66ec7b09add61323a9d04d7
Jens Peters <jp7677@gmail.com>
2022-12-05 08:39:45 +0100

    2022: Add day 05 solution

--- 2022/src/test/kotlin/Utils.kt
+++ 2022/src/test/kotlin/Utils.kt
@@ -1,10 +1,12 @@
-fun getPuzzleInput(name: String, separator: String = System.lineSeparator()) =
+fun getPuzzleInput(name: String, separator: String = eol()) =
     object {}::class.java.getResourceAsStream(name)
         ?.bufferedReader()
         ?.let {
-            if (separator == System.lineSeparator())
+            if (separator == eol())
                 it.lineSequence()
             else
                 it.readText().split(separator).asSequence()
         }
         ?: throw Exception("Invalid input name")
+
+fun eol(): String = System.lineSeparator()
