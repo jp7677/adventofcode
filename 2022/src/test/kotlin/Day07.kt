30ca21bfd16d0cb9f68c8d215d9126ba51df8742
Jens Peters <jp7677@gmail.com>
2022-12-07 17:03:49 +0100

    2022: Improve readability

--- 2022/src/test/kotlin/Day07.kt
+++ 2022/src/test/kotlin/Day07.kt
@@ -4,9 +4,8 @@ import io.kotest.matchers.shouldBe
 class Day07 : StringSpec({
     "puzzle part 01" {
         val sumOfAtMost10K = getDirectorySizes()
-            .map { it.value }
-            .filter { it <= 100000 }
-            .sum()
+            .filterValues { it <= 100000 }
+            .values.sum()
 
         sumOfAtMost10K shouldBe 1778099
     }
@@ -16,9 +15,8 @@ class Day07 : StringSpec({
 
         val needed = 30000000 - 70000000L + sizes.getValue("/")
         val sizeOfCandidate = sizes
-            .map { it.value }
-            .filter { it >= needed }
-            .min()
+            .filterValues { it >= needed }
+            .values.min()
 
         sizeOfCandidate shouldBe 1623571
     }
@@ -28,29 +26,27 @@ private fun getDirectorySizes(): Map<String, Long> {
     val currentPath = ArrayDeque<String>(1)
     val sizes = getPuzzleInput("day07-input.txt")
         .mapNotNull {
-            if (it.startsWith("$ cd")) {
-                if (it.startsWith("$ cd .."))
-                    currentPath.removeLast()
-                else
-                    currentPath.add(it.drop(5))
-                null
-            } else if (it.startsWith("$ ls"))
-                null
-            else if (it.startsWith("dir"))
-                (currentPath.toArray() + it.drop(4)).join() to 0L
-            else
-                currentPath.toArray().join() to it.split(" ").first().toLong()
+            when {
+                it.startsWith("$ ls") -> null
+                it.startsWith("$ cd ..") -> null.also { currentPath.removeLast() }
+                it.startsWith("$ cd") -> null.also { _ -> currentPath.add(it.dirName()) }
+                it.startsWith("dir") -> currentPath.toArray().plus(it.dirName()).join() to 0L
+                else -> currentPath.toArray().join() to it.fileSize()
+            }
         }
         .groupingBy { (path, _) -> path }
         .fold(0L) { acc, (_, size) -> acc + size }
 
     return sizes
         .map { (path, size) ->
-            path to (sizes
-                .filterNot { it.key == path }
+            path to sizes
+                .filterKeys { it != path }
                 .map { if (it.key.startsWith(path)) it.value else 0 }
-                .sum() + size)
-        }.toMap()
+                .sum().plus(size)
+        }
+        .toMap()
 }
 
+private fun String.dirName() = drop(4).trim()
+private fun String.fileSize() = split(" ").first().toLong()
 private fun Array<Any?>.join() = joinToString("/").replace("//", "/")
