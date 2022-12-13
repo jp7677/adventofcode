3d0a7e19799c8ed37178ce5b8c9431d48b46879e
Jens Peters <jp7677@gmail.com>
2022-12-13 18:45:29 +0100

    2022: Add day 13 solution

--- 2022/src/test/kotlin/Utils.kt
+++ 2022/src/test/kotlin/Utils.kt
@@ -1,3 +1,5 @@
+import java.util.Stack
+
 fun getPuzzleInput(name: String, separator: String = eol) =
     object {}::class.java.getResourceAsStream(name)
         ?.bufferedReader()
@@ -10,3 +12,31 @@ fun getPuzzleInput(name: String, separator: String = eol) =
         ?: throw Exception("Invalid input name")
 
 val eol: String = System.lineSeparator()
+
+// Based on https://stackoverflow.com/a/45764969
+fun String.indexOfClosingBracket(): Int {
+    fun Char.matchesBracket(peek: Char) =
+        (this == '(' && peek == ')') ||
+            (this == ')' && peek == '(') ||
+            (this == '[' && peek == ']') ||
+            (this == ']' && peek == '[') ||
+            (this == '{' && peek == '}') ||
+            (this == '}' && peek == '{') ||
+            (this == '<' && peek == '>') ||
+            (this == '>' && peek == '<')
+
+    val stack = Stack<Char>()
+    this.withIndex()
+        .filter { (_, it) -> it in listOf('(', ')', '[', ']', '{', '}', '<', '>') }
+        .forEach { (index, it) ->
+            if (stack.isNotEmpty() && it.matchesBracket(stack.peek()))
+                stack.pop()
+            else
+                stack.push(it)
+
+            if (stack.isEmpty())
+                return index.inc()
+        }
+
+    throw IllegalStateException()
+}
