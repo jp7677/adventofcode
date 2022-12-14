f01c5290c9c293eb33198ed8608cb391a8bd67fd
Jens Peters <jp7677@gmail.com>
2022-12-14 18:15:08 +0100

    2022: Add day 14 solution

--- 2022/src/test/kotlin/Utils.kt
+++ 2022/src/test/kotlin/Utils.kt
@@ -40,3 +40,6 @@ fun String.indexOfClosingBracket(): Int {
 
     throw IllegalStateException()
 }
+
+// Based on https://stackoverflow.com/a/52986053
+infix fun Int.towards(to: Int) = IntProgression.fromClosedRange(this, to, if (this > to) -1 else 1)
