c5ba7869fb82b081e609f1b5fe1e3ae0bf1a23c1
Jens Peters <jp7677@gmail.com>
2022-12-24 08:04:39 +0100

    2022: Add day 21 part 2 solution

--- 2022/src/test/kotlin/Day21.kt
+++ 2022/src/test/kotlin/Day21.kt
@@ -1,28 +1,76 @@
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import kotlin.reflect.KFunction
 
-private interface Monkey21 {
-    fun solve(monkeys: Map<String, Monkey21>): Long
+private sealed interface Monkey21 {
+    var monkeys: Map<String, Monkey21>
+    fun yell(): Long
 }
 
 private data class NumMonkey21(val num: Long) : Monkey21 {
-    override fun solve(monkeys: Map<String, Monkey21>) = num
+    override lateinit var monkeys: Map<String, Monkey21>
+    override fun yell() = num
 }
 
 private data class OpMonkey21(val name1: String, val name2: String, val op: (Long, Long) -> Long) : Monkey21 {
-    override fun solve(monkeys: Map<String, Monkey21>) =
-        op(monkeys[name1]!!.solve(monkeys), monkeys[name2]!!.solve(monkeys))
+    override lateinit var monkeys: Map<String, Monkey21>
+    override fun yell() = op(monkeys[name1]!!.yell(), monkeys[name2]!!.yell())
+
+    fun revertYell(yell: Long, num: Long, r: Boolean) = when ((op as KFunction<*>).name) {
+        "plus" -> yell - num
+        "minus" -> if (r) yell + num else num - yell
+        "times" -> yell / num
+        "div" -> if (r) yell * num else num / yell
+        else -> throw IllegalStateException()
+    }
 }
 
 class Day21 : StringSpec({
     "puzzle part 01" {
         val monkeys = getMonkeys()
+        monkeys.onEach { it.value.monkeys = monkeys }
 
-        val number = monkeys["root"]!!.solve(monkeys)
+        val number = monkeys["root"]!!.yell()
 
         number shouldBe 145167969204648
     }
+
+    "puzzle part 02".config(enabled = true) {
+        val monkeys = getMonkeys()
+        monkeys.onEach { it.value.monkeys = monkeys }
+
+        val root = monkeys["root"]!! as OpMonkey21
+        val name1HasHuman = monkeys.hasHuman(root.name1)
+        val otherYell = monkeys[if (name1HasHuman) root.name1 else root.name2]!!.yell()
+        val name = if (name1HasHuman) root.name1 else root.name2
+        val number = root.revertYell(root.yell(), otherYell, name1HasHuman)
+
+        val foundYell = monkeys.findHuman(monkeys[name]!!, number)
+
+        foundYell shouldBe 3330805295850
+    }
 })
+
+private fun Map<String, Monkey21>.findHuman(monkey: Monkey21, yell: Long): Long {
+    if (monkey !is OpMonkey21) throw IllegalStateException()
+
+    val name1HasHuman = this.hasHuman(monkey.name1)
+    val otherYell = this[if (name1HasHuman) monkey.name2 else monkey.name1]!!.yell()
+    val name = if (name1HasHuman) monkey.name1 else monkey.name2
+    val number = monkey.revertYell(yell, otherYell, name1HasHuman)
+
+    if (name == "humn") return number
+    return findHuman(this[name]!!, number)
+}
+
+private fun Map<String, Monkey21>.hasHuman(name: String): Boolean {
+    if (name == "humn") return true
+
+    return when (val monkey = this[name]!!) {
+        is OpMonkey21 -> hasHuman(monkey.name1) || hasHuman(monkey.name2)
+        is NumMonkey21 -> false
+    }
+}
 
 private fun getMonkeys() = getPuzzleInput("day21-input.txt")
     .map {
