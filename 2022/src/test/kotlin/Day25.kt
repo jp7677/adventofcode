3b862f0a7a3a7863a4a936d047943cbda0b18cc4
Jens Peters <jp7677@gmail.com>
2022-12-30 14:55:27 +0100

    2022: Add day 25 solution

---
+++ 2022/src/test/kotlin/Day25.kt
@@ -0,0 +1,40 @@
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import kotlin.math.pow
+
+class Day25 : StringSpec({
+    "puzzle part 01" {
+        val snafuNumber = getPuzzleInput("day25-input.txt")
+            .map { it.fromSnafuNumber() }
+            .sum().toSnafuNumber()
+
+        snafuNumber shouldBe "20=212=1-12=200=00-1"
+    }
+})
+
+private fun String.fromSnafuNumber() = this.reversed()
+    .mapIndexed { index, c ->
+        when (c) {
+            '-' -> -1
+            '=' -> -2
+            else -> c.digitToInt()
+        } * (5.0.pow(index)).toLong()
+    }
+    .sum()
+
+private fun Long.toSnafuNumber() = generateSequence(this.quotientToRemainder()) { (q, _) ->
+    (q / 5).let { r ->
+        if (r != 0L) r.quotientToRemainder()
+        else null
+    }
+}
+    .map { it.second }
+    .toList().reversed()
+    .joinToString("")
+    .trimStart('0')
+
+private fun Long.quotientToRemainder() = when (val m1 = this % 5) {
+    4L -> this + 1 to "-"
+    3L -> this + 2 to "="
+    else -> this to m1.toString()
+}
