26845df9ee3b3fd4930c2f69a76f829172cb8d3c
Jens Peters <jp7677@gmail.com>
2023-01-05 18:49:15 +0100

    2022: Enhance Dijkstra to A*

--- 2022/src/test/kotlin/Day12.kt
+++ 2022/src/test/kotlin/Day12.kt
@@ -1,9 +1,11 @@
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import java.util.PriorityQueue
+import kotlin.math.absoluteValue
 
 private data class Coord12(val x: Int, val y: Int)
 private val directions = listOf(Coord12(1, 0), Coord12(0, 1), Coord12(-1, 0), Coord12(0, -1))
+private data class QueueItem(val coord: Coord12, val totalDistance: Int, val priority: Int)
 
 class Day12 : StringSpec({
     "puzzle part 01" {
@@ -29,35 +31,37 @@ class Day12 : StringSpec({
 })
 
 private fun Map<Coord12, Int>.findShortestPathLength(start: Coord12, end: Coord12): Int? {
-    val queue = PriorityQueue<Pair<Coord12, Int>>(1, compareBy { (_, distance) -> distance })
-        .apply { offer(start to 0) }
+    val queue = PriorityQueue<QueueItem>(1, compareBy { it.priority })
+        .apply { offer(QueueItem(start, 0, end.x + end.y)) }
     val totals = mutableMapOf(start to 0)
     val visited = mutableSetOf<Coord12>()
 
     while (queue.isNotEmpty()) {
-        val (current, totalDistance) = queue.poll()
-        val distance = this[current]!! + 1
+        val current = queue.poll()
+        val distance = this[current.coord]!! + 1
 
         directions
-            .map { direction -> Coord12(current.x + direction.x, current.y + direction.y) }
+            .map { direction -> Coord12(current.coord.x + direction.x, current.coord.y + direction.y) }
             .filterNot { coord -> visited.contains(coord) }
             .mapNotNull { coord -> this[coord]?.let { coord to it } }.toMap()
             .filterValues { it <= distance }
             .keys.forEach { coord ->
-                val newTotalDistance = totalDistance + 1
+                val newTotalDistance = current.totalDistance + 1
                 val knownTotalDistance = totals[coord] ?: Int.MAX_VALUE
                 if (newTotalDistance < knownTotalDistance) {
                     totals[coord] = newTotalDistance
-                    queue.offer(coord to newTotalDistance)
+                    queue.offer(QueueItem(coord, newTotalDistance, newTotalDistance + coord.manhattenDistance(end)))
                 }
             }
 
-        if (current == end) break
-        visited.add(current)
+        if (current.coord == end) break
+        visited.add(current.coord)
     }
 
     return totals.filterKeys { it == end }.toList().firstOrNull()?.second
 }
+
+private fun Coord12.manhattenDistance(end: Coord12) = (end.x - x).absoluteValue + (end.y - y).absoluteValue
 
 private fun getMap() = getPuzzleInput("day12-input.txt").toList()
     .map {
