5f507af87dd888faa3f9d4aab3442cb840c59524
Jens Peters <jp7677@gmail.com>
2023-11-13 18:52:37 +0100

    2023: Bring util into scope

--- 2023/src/day00.rs
+++ 2023/src/day00.rs
@@ -1,9 +1,10 @@
 #[cfg(test)]
 mod test {
+    use crate::util;
 
     #[test]
     fn part01() {
-        let input = crate::util::read_input("day00-input.txt");
+        let input = util::read_input("day00-input.txt");
 
         assert_eq!(input.lines().count(), 1);
         assert_eq!(input, "0");
