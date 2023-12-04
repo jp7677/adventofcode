42074f2070ca3775dfce91c6ab35149a1ba5a183
Jens Peters <jp7677@gmail.com>
2023-12-04 20:07:02 +0100

    2023: Use hashmap and hashset
    Much better performance.

--- 2023/src/day03.rs
+++ 2023/src/day03.rs
@@ -1,15 +1,21 @@
 use crate::util::*;
-use std::rc::Rc;
+use std::collections::{HashMap, HashSet};
 
+#[derive(Eq, Hash, PartialEq, Clone)]
 struct Coord {
     x: i32,
     y: i32,
+}
+
+#[derive(Eq, Hash, PartialEq, Clone)]
+struct Adjacent {
+    coord: Coord,
     symbol: char,
 }
 
 struct Part {
     number: String,
-    adjacents: Vec<Rc<Coord>>,
+    adjacents: HashSet<Adjacent>,
 }
 
 impl Part {
@@ -25,7 +31,7 @@ fn part01() {
     let parts = parse_map(&input);
     let sum_of_parts = parts
         .iter()
-        .filter(|n| n.adjacents.len() > 0)
+        .filter(|p| p.adjacents.len() > 0)
         .fold(0, |acc, p| acc + p.number());
 
     assert_eq!(sum_of_parts, 528799);
@@ -39,7 +45,7 @@ fn part02() {
     let mut gears = parts
         .iter()
         .filter_map(|p| p.adjacents.iter().find(|a| a.symbol == '*'))
-        .map(|c| (c.x, c.y))
+        .map(|a| (a.coord.x, a.coord.y))
         .collect::<Vec<(i32, i32)>>();
 
     gears.sort();
@@ -48,7 +54,11 @@ fn part02() {
     let sum_of_gear_parts = gears.iter().fold(0, |acc, g| {
         let gear_parts = parts
             .iter()
-            .filter(|p| p.adjacents.iter().any(|a| a.x == g.0 && a.y == g.1))
+            .filter(|p| {
+                p.adjacents
+                    .iter()
+                    .any(|a| a.coord.x == g.0 && a.coord.y == g.1)
+            })
             .collect::<Vec<&Part>>();
         if gear_parts.len() == 2 {
             acc + gear_parts.first().unwrap().number() * gear_parts.last().unwrap().number()
@@ -61,22 +71,21 @@ fn part02() {
 }
 
 fn parse_map(input: &String) -> Vec<Part> {
-    let mut map: Vec<Rc<Coord>> = vec![];
+    let mut map: HashMap<Coord, char> = HashMap::new();
     for (y, line) in input.lines().enumerate() {
         for (x, symbol) in line.chars().enumerate() {
             if symbol != '.' {
-                let coord = Rc::new(Coord {
+                let coord = Coord {
                     x: x as i32,
                     y: y as i32,
-                    symbol,
-                });
-                map.push(coord);
+                };
+                map.insert(coord, symbol);
             }
         }
     }
 
     let map_size = input.lines().count() as i32;
-    let mut parts: Vec<Part> = vec![];
+    let mut parts: Vec<Part> = Vec::new();
     for (i, c) in input.chars().enumerate() {
         if c.is_digit(10) {
             let x = i as i32 % (map_size + 1);
@@ -85,27 +94,32 @@ fn parse_map(input: &String) -> Vec<Part> {
             let adjacents = adjacent(x, y)
                 .iter()
                 .filter_map(|(a_x, a_y)| {
-                    let adjacent_coord = map.iter().find(|c| c.x == *a_x && c.y == *a_y);
+                    let coord = Coord { x: *a_x, y: *a_y };
+                    let adjacent_coord = map.get(&coord);
                     match adjacent_coord {
                         None => None,
                         Some(v) => {
-                            if !v.symbol.is_digit(10) {
-                                Some(Rc::clone(v))
+                            if !v.is_digit(10) {
+                                Some(Adjacent { coord, symbol: *v })
                             } else {
                                 None
                             }
                         }
                     }
                 })
-                .collect::<Vec<Rc<Coord>>>();
+                .collect::<Vec<Adjacent>>();
 
             if i >= 1 && input.chars().nth(i - 1).unwrap_or('_').is_digit(10) {
                 parts.last_mut().unwrap().number.push(c);
-                parts.last_mut().unwrap().adjacents.extend(adjacents);
+                parts
+                    .last_mut()
+                    .unwrap()
+                    .adjacents
+                    .extend(adjacents.iter().cloned());
             } else {
                 parts.push(Part {
                     number: c.to_string(),
-                    adjacents,
+                    adjacents: HashSet::from_iter(adjacents.iter().cloned()),
                 });
             }
         }
