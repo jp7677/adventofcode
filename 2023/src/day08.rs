321c874d2b3263aa7864ae37c6d84307c0badf5e
Jens Peters <jp7677@gmail.com>
2023-12-08 19:49:54 +0100

    2023: Add day 8 part 2 solution

--- 2023/src/day08.rs
+++ 2023/src/day08.rs
@@ -3,43 +3,73 @@ use crate::util::*;
 struct Element<'a> {
     start: &'a str,
     left: &'a str,
-    right: &'a str
+    right: &'a str,
 }
 
 #[test]
 fn part01() {
     let input = read_input(DAYS::Day08);
 
-    let instructions = input.lines().nth(0).unwrap();
-    let map = input.lines().skip(2).map(|line|{
-        Element {
-            start: &line[0..3],
-            left: &line[7..10],
-            right: &line[12..15]
-        }
-    }).collect::<Vec<Element>>();
-
-    let mut steps = 0;
-    let mut current_instruction = 0;
-    let mut current_element = map.iter().find(|e| e.start == "AAA");
-    while current_element.unwrap().start != "ZZZ" {
-        current_element = map.iter().find(|e|
-            if instructions.chars().nth(current_instruction).unwrap() == 'L' {
-                e.start == current_element.unwrap().left
-            } else {
-                e.start == current_element.unwrap().right
-            }
-        );
-        current_instruction = (current_instruction + 1) % instructions.len();
-        steps = steps + 1
-    }
+    let (instructions, map) = get_instructions_and_elements(&input);
+    let start = map.iter().find(|e| e.start == "AAA").unwrap();
+    let steps = get_steps(start, |e| e == "ZZZ", &instructions, &map);
 
     assert_eq!(steps, 18673);
 }
 
 #[test]
 fn part02() {
-    let input = read_input(DAYS::Day00);
+    let input = read_input(DAYS::Day08);
 
-    assert_eq!(input.lines().count(), 1);
+    let (instructions, map) = get_instructions_and_elements(&input);
+    let steps = map
+        .iter()
+        .filter(|e| e.start.ends_with('A'))
+        .map(|e| get_steps(e, |e| e.ends_with('Z'), &instructions, &map))
+        .collect::<Vec<usize>>();
+
+    let steps = lcm(steps.as_slice());
+
+    assert_eq!(steps, 17972669116327);
+}
+
+fn get_steps(
+    start: &Element,
+    end: fn(start: &str) -> bool,
+    instructions: &str,
+    map: &Vec<Element>,
+) -> usize {
+    let mut steps = 0;
+    let mut current_instruction = 0;
+    let mut current_element = start;
+    while !end(current_element.start) {
+        current_element = map
+            .iter()
+            .find(|e| {
+                if instructions.chars().nth(current_instruction).unwrap() == 'L' {
+                    e.start == current_element.left
+                } else {
+                    e.start == current_element.right
+                }
+            })
+            .unwrap();
+        current_instruction = (current_instruction + 1) % instructions.len();
+        steps = steps + 1
+    }
+    steps
+}
+
+fn get_instructions_and_elements(input: &str) -> (&str, Vec<Element>) {
+    let instructions = input.lines().nth(0).unwrap();
+    let map = input
+        .lines()
+        .skip(2)
+        .map(|line| Element {
+            start: &line[0..3],
+            left: &line[7..10],
+            right: &line[12..15],
+        })
+        .collect::<Vec<Element>>();
+
+    (instructions, map)
 }
