ed86a4abad75f853368b0c1f8bdb1539aaf953a2
Jens Peters <jp7677@gmail.com>
2023-12-09 08:12:41 +0100

    2023: Add day 9 solution

---
+++ 2023/src/day09.rs
@@ -0,0 +1,54 @@
+use crate::util::*;
+
+#[test]
+fn part01() {
+    let input = read_input(DAYS::Day09);
+
+    let sum_of_values = parse_histories(&input).fold(0, |acc, history| acc + next_value(&history));
+
+    assert_eq!(sum_of_values, 1853145119);
+}
+
+#[test]
+fn part02() {
+    let input = read_input(DAYS::Day09);
+
+    let sum_of_values = parse_histories(&input)
+        .map(|h| h.iter().rev().map(|v| *v).collect::<Vec<i32>>())
+        .fold(0, |acc, history| acc + next_value(&history));
+
+    assert_eq!(sum_of_values, 923);
+}
+
+fn next_value(history: &[i32]) -> i32 {
+    let mut next_line = history.to_vec();
+    let mut values = Vec::new();
+
+    let mut sequence = 1;
+    while !next_line.iter().all(|v| *v == 0) {
+        next_line = next_line
+            .iter()
+            .enumerate()
+            .map(|(i, h)| {
+                if i > sequence {
+                    *h - next_line.iter().nth(i - 1).unwrap()
+                } else {
+                    0
+                }
+            })
+            .collect::<Vec<i32>>();
+
+        values.push(*next_line.last().unwrap());
+        sequence = sequence + 1;
+    }
+
+    return values.iter().rev().fold(0, |acc, it| acc + it) + history.last().unwrap();
+}
+
+fn parse_histories<'a>(input: &'a str) -> impl Iterator<Item = Vec<i32>> + 'a {
+    input.lines().map(|line| {
+        line.split_whitespace()
+            .map(|p| p.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>()
+    })
+}
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -10,6 +10,7 @@ mod day05;
 mod day06;
 mod day07;
 mod day08;
+mod day09;
 mod util;
 
 fn main() {
