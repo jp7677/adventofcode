48c38a699eb9986e3c94f912a295038bde393796
Jens Peters <jp7677@gmail.com>
2024-11-15 10:38:34 +0100

    2024: Add input setup

--- 2024/Day00.fs
+++ 2024/Day00.fs
@@ -5,4 +5,7 @@ open Xunit
 module Day00 =
 
     [<Fact>]
-    let ``Part 01`` () = Assert.True(true)
+    let ``Part 01`` () =
+        let lines = Util.ReadInput("day00-input.txt")
+        let first = lines |> Seq.head |> int
+        Assert.Equal(0, first)
