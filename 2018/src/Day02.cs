986ca4c15b4201364e724e2733e0f8839b0aa43c
Jens Peters <jp7677@gmail.com>
2024-11-30 23:19:15 +0100

    2018: Add day 2
    Also read input as sequence and formatting.

---
+++ 2018/src/Day02.cs
@@ -0,0 +1,48 @@
+using System.Linq;
+using Xunit;
+
+namespace aoc2018;
+
+public class Day02
+{
+    [Fact]
+    public void Part01()
+    {
+        var ids = Util.GetInputAsStrings("day02-input.txt")
+            .ToList()
+            .Select(s => s.ToCharArray().GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count()))
+            .ToList();
+
+        var twice = ids.Count(x => x.ContainsValue(2));
+        var three = ids.Count(x => x.ContainsValue(3));
+
+        Assert.Equal(7134, twice * three);
+    }
+
+    [Fact]
+    public void Part02()
+    {
+        var ids = Util.GetInputAsStrings("day02-input.txt").ToList();
+
+        var prototypes = ids.Where(
+                id1 =>
+                    ids.Count(
+                        id2 =>
+                            id1.ToCharArray()
+                                .Select((c, i) => (i, c))
+                                .Sum(t => t.c != id2[t.i] ? 1 : 0) == 1
+                    ) == 1
+            )
+            .ToList();
+
+        Assert.Equal(2, prototypes.Count);
+
+        var common = prototypes
+            .First()
+            .ToCharArray()
+            .Where((c, i) => c == prototypes.Last()[i])
+            .ToArray();
+
+        Assert.Equal("kbqwtcvzhmhpoelrnaxydifyb", new string(common));
+    }
+}
--- 2018/src/Util.cs
+++ 2018/src/Util.cs
@@ -1,21 +1,42 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace aoc2018
-{
-    public static class Util
-    {
-        public static async Task<IEnumerable<string>> GetInputAsStrings(string name)
-        {
-            var stream = typeof(Util).Assembly.GetManifestResourceStream($"aoc2018.data.{name}");
-            if (stream == null)
-                throw new ArgumentOutOfRangeException(nameof(name));
+namespace aoc2018;
 
-            var reader = new StreamReader(stream);
-            var content = await reader.ReadToEndAsync();
-            return content.Trim().Split(Environment.NewLine);
+public static class Util
+{
+    public static async IAsyncEnumerable<string> GetInputAsStrings(string name)
+    {
+        var stream = typeof(Util).Assembly.GetManifestResourceStream($"aoc2018.data.{name}");
+        if (stream == null)
+            throw new ArgumentOutOfRangeException(nameof(name));
+
+        var reader = new StreamReader(stream);
+        while (await reader.ReadLineAsync() is { } line)
+        {
+            yield return line;
         }
+    }
+
+    public static List<T> ToList<T>(
+        this IAsyncEnumerable<T> items,
+        CancellationToken cancellationToken = default
+    )
+    {
+        return items.ToListAsync(cancellationToken).Result;
+    }
+
+    public static async Task<List<T>> ToListAsync<T>(
+        this IAsyncEnumerable<T> items,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var results = new List<T>();
+        await foreach (var item in items.WithCancellation(cancellationToken).ConfigureAwait(false))
+            results.Add(item);
+        return results;
     }
 }
