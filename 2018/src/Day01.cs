de89082938a80fb4ee4b3a79e934162c8f258c3d
Jens Peters <jp7677@gmail.com>
2024-11-30 23:19:15 +0100

    2018: Add day 2
    Also read input as sequence and formatting.

--- 2018/src/Day01.cs
+++ 2018/src/Day01.cs
@@ -1,45 +1,44 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Xunit;
 
-namespace aoc2018
+namespace aoc2018;
+
+public class Day01
 {
-    public class Day01
+    [Fact]
+    public void Part01()
     {
-        [Fact]
-        public async Task Part01()
+        var result = Util.GetInputAsStrings("day01-input.txt").ToList().Sum(Convert.ToInt32);
+
+        Assert.Equal(500, result);
+    }
+
+    [Fact]
+    public void Part02()
+    {
+        var frequencyChanges = Util.GetInputAsStrings("day01-input.txt")
+            .ToList()
+            .Select(f => Convert.ToInt32(f))
+            .ToList();
+
+        var frequencies = new HashSet<int>();
+        var frequency = 0;
+        while (true)
         {
-            var result = (await Util.GetInputAsStrings("day01-input.txt")).Sum(Convert.ToInt32);
-
-            Assert.Equal(500, result);
-        }
-
-        [Fact]
-        public async Task Part02()
-        {
-            var frequencyChanges = (await Util.GetInputAsStrings("day01-input.txt"))
-                .Select(f => Convert.ToInt32(f))
-                .ToList();
-
-            var frequencies = new HashSet<int>();
-            var frequency = 0;
-            while (true)
+            foreach (var frequencyChange in frequencyChanges)
             {
-                foreach (var frequencyChange in frequencyChanges)
-                {
-                    frequency += frequencyChange;
-                    if (frequencies.Contains(frequency))
-                        goto done;
+                frequency += frequencyChange;
+                if (frequencies.Contains(frequency))
+                    goto done;
 
-                    frequencies.Add(frequency);
-                }
+                frequencies.Add(frequency);
             }
-
-            done:
-
-            Assert.Equal(709, frequency);
         }
+
+        done:
+
+        Assert.Equal(709, frequency);
     }
 }
