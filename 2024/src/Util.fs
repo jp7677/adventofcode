dcb2afec8216eb6b0dacfc82569b63544a13070a
Jens Peters <jp7677@gmail.com>
2024-12-01 16:02:23 +0100

    2024: Create countBy()

--- 2024/src/Util.fs
+++ 2024/src/Util.fs
@@ -7,3 +7,7 @@ module Util =
     let input (name: string) =
         let path = $"{Reflection.Assembly.GetExecutingAssembly().Location}/../../../../data/{name}"
         seq { yield! IO.File.ReadLines path }
+
+module List =
+    let inline countBy (predicate: 'T -> bool) list =
+        list |> List.filter predicate |> List.length
