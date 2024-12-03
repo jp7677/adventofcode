8d5825eb65fb1f29ff2d9631b84a432b82e4e4ee
Jens Peters <jp7677@gmail.com>
2024-12-03 07:02:49 +0100

    2024: Add day 3 part 1

---
+++ 2024/src/Day03.fs
@@ -0,0 +1,33 @@
+﻿namespace Aoc2024
+
+open System.Text.RegularExpressions
+open FsUnit.Xunit
+open Xunit
+open Util
+
+module Day03 =
+
+    let matchMul memory =
+        seq {
+            for m in Regex.Matches(memory, "(mul\(\d+,\d+\))") do
+                yield m.Value
+        }
+
+    let matchNumbers memory =
+        seq {
+            for m in Regex.Matches(memory, "(\d+)") do
+                yield m.Value
+        }
+
+    [<Fact>]
+    let ``part 01`` () =
+        let result =
+            input "day03-input.txt"
+            |> Seq.map matchMul
+            |> Seq.map (fun line ->
+                line
+                |> Seq.map (fun instr -> instr |> matchNumbers |> Seq.map int |> Seq.reduce (fun a b -> a * b))
+                |> Seq.reduce (fun a b -> a + b))
+            |> Seq.reduce (fun a b -> a + b)
+
+        result |> should equal 174960292
