9b2e82ffb48a058747d9ee72964097af1e32ebfc
Jens Peters <jp7677@gmail.com>
2024-12-03 19:19:07 +0100

    2024: Add day 3 part 2

--- 2024/src/Day03.fs
+++ 2024/src/Day03.fs
@@ -7,27 +7,58 @@ open Util
 
 module Day03 =
 
-    let matchMul memory =
+    type Instr =
+        | Do
+        | Dont
+        | Mul of int * int
+
+    type Status =
+        | Enabled
+        | Disabled
+
+    let matchNumbers instruction =
         seq {
-            for m in Regex.Matches(memory, "(mul\(\d+,\d+\))") do
+            for m in Regex.Matches(instruction, "(\d+)") do
                 yield m.Value
         }
 
-    let matchNumbers memory =
+    let matchInstructions memory =
         seq {
-            for m in Regex.Matches(memory, "(\d+)") do
-                yield m.Value
+            for m in Regex.Matches(memory, "(mul\(\d+,\d+\))|(do\(\))|(don't\(\))") do
+                if m.Value.StartsWith("mul") then
+                    let i = m.Value |> matchNumbers |> Seq.toList
+                    yield Mul(i[0] |> int, i[1] |> int)
+                else if m.Value.StartsWith("don't") then
+                    yield Dont
+                else
+                    yield Do
         }
+
+    let readInstructions = input "day03-input.txt" |> Seq.reduce (fun a b -> a + b) |> matchInstructions
 
     [<Fact>]
     let ``part 01`` () =
         let result =
-            input "day03-input.txt"
-            |> Seq.map matchMul
-            |> Seq.map (fun line ->
-                line
-                |> Seq.map (fun instr -> instr |> matchNumbers |> Seq.map int |> Seq.reduce (fun a b -> a * b))
-                |> Seq.reduce (fun a b -> a + b))
-            |> Seq.reduce (fun a b -> a + b)
+            readInstructions
+            |> Seq.fold
+                (fun acc instr ->
+                    match instr with
+                    | Mul(a, b) -> acc + (a * b)
+                    | _ -> acc)
+                0
 
         result |> should equal 174960292
+
+    [<Fact>]
+    let ``part 02`` () =
+        let result, _ =
+            readInstructions
+            |> Seq.fold
+                (fun (acc, status) instr ->
+                    match instr with
+                    | Mul(a, b) -> (acc + (if status = Enabled then a * b else 0), status)
+                    | Do -> (acc, Enabled)
+                    | Dont -> (acc, Disabled))
+                (0, Enabled)
+
+        result |> should equal 56275602
