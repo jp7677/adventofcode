774a427bc288144fea982a01aa7d73914e845904
Jens Peters <jp7677@gmail.com>
2024-12-06 16:11:39 +0100

    2024: Formatting

--- 2024/src/Day05.fs
+++ 2024/src/Day05.fs
@@ -9,31 +9,43 @@ module Day05 =
     let readManual () =
         let puzzle = input "day05-input.txt"
         let sepIndex = puzzle |> Seq.findIndex (fun s -> s = "")
-        let rules = puzzle |> Seq.take sepIndex |> Seq.map _.Split("|") |> Seq.map (fun p -> p[0] |> int, p[1] |> int) |> Set.ofSeq
-        let pages = puzzle |> Seq.skip (sepIndex + 1) |> Seq.map _.Split(",") |> Seq.map (fun s -> s |> Seq.map int) |> Seq.toList
+
+        let rules =
+            puzzle
+            |> Seq.take sepIndex
+            |> Seq.map _.Split("|")
+            |> Seq.map (fun p -> p[0] |> int, p[1] |> int)
+            |> Set.ofSeq
+
+        let pages =
+            puzzle
+            |> Seq.skip (sepIndex + 1)
+            |> Seq.map _.Split(",")
+            |> Seq.map (fun s -> s |> Seq.map int)
+            |> Seq.toList
+
         (rules, pages)
 
     let detectInvalidPages (rules: Set<'a>) pages =
-            pages
-            |> Seq.choose (fun p ->
-                let length = p |> Seq.length |> Int.minus 1
+        pages
+        |> Seq.choose (fun p ->
+            let length = p |> Seq.length |> Int.minus 1
 
-                let pairs =
-                    [0 .. length - 1]
-                    |> List.collect (fun i ->
-                        [i + 1 .. length]
-                        |> List.map (fun c -> (p |> Seq.item i, p |> Seq.item c)))
+            let pairs =
+                [ 0 .. length - 1 ]
+                |> List.collect (fun i ->
+                    [ i + 1 .. length ] |> List.map (fun c -> (p |> Seq.item i, p |> Seq.item c)))
 
-                let valid =
-                    pairs
-                    |> List.countBy (fun pair -> rules.Contains((pair |> snd, pair |> fst)))
-                    |> (fun count -> count = 0)
+            let valid =
+                pairs
+                |> List.countBy (fun pair -> rules.Contains((pair |> snd, pair |> fst)))
+                |> (fun count -> count = 0)
 
-                if not valid then Some p else None)
+            if not valid then Some p else None)
 
     [<Fact>]
     let ``part 01`` () =
-        let rules, pages = readManual()
+        let rules, pages = readManual ()
         let invalidPages = detectInvalidPages rules pages
 
         let sum =
@@ -45,20 +57,20 @@ module Day05 =
 
     [<Fact>]
     let ``part 02`` () =
-        let rules, pages = readManual()
+        let rules, pages = readManual ()
         let invalidPages = detectInvalidPages rules pages
 
         let sum =
             invalidPages
             |> Seq.map (fun p ->
                 rules
-                    |> Seq.filter (fun rule -> (p |> Seq.contains (rule |> fst)) && (p |> Seq.contains (rule |> snd))))
+                |> Seq.filter (fun rule -> (p |> Seq.contains (rule |> fst)) && (p |> Seq.contains (rule |> snd))))
             |> Seq.sumBy (fun rules ->
                 let groups =
                     rules
                     |> Seq.map fst
                     |> Seq.groupBy id
-                    |> Seq.sortByDescending (fun (_,g) -> g |> Seq.length)
+                    |> Seq.sortByDescending (fun (_, g) -> g |> Seq.length)
 
                 groups |> Seq.map fst |> Seq.item ((groups |> Seq.length) / 2))
 
