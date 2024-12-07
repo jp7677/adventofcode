5958099459f5c39a9b2894702ba17646d55321da
Jens Peters <jp7677@gmail.com>
2024-12-07 06:56:22 +0100

    2024: Add day 6 part 2
    Disabled since it needs a few seconds.

--- 2024/src/Day06.fs
+++ 2024/src/Day06.fs
@@ -18,6 +18,29 @@ module Day06 =
     [<Struct>]
     type Guard = { coord: Coord; direction: Direction }
 
+    let getMap =
+        let map = input "day06-input.txt" |> Seq.toList
+        let max = map |> List.length |> Int.minus 1
+
+        let guard =
+            map
+            |> Seq.mapi (fun y s ->
+                match s |> Seq.tryFindIndex (fun c -> c = '^') with
+                | Some x -> Some { x = x; y = y }
+                | None -> None)
+            |> Seq.find _.IsSome
+            |> Option.get
+            |> (fun c -> { coord = c; direction = NORTH })
+
+        let obstructions =
+            [ 0..max ]
+            |> Seq.collect (fun x ->
+                ([ 0..max ]
+                 |> Seq.choose (fun y -> if map[y][x] = '#' then Some { x = x; y = y } else None)))
+            |> Set.ofSeq
+
+        max, obstructions, guard
+
     let moveNorth state =
         { coord =
             { x = state.coord.x
@@ -60,44 +83,94 @@ module Day06 =
         { coord = { x = state.coord.x; y = state.coord.y }
           direction = direction }
 
+    let getSteps max (obstructions: Set<Coord>) guard =
+        guard
+        |> Seq.unfold (fun it ->
+            if it.coord.x < 0 || it.coord.x > max || it.coord.y < 0 || it.coord.y > max then
+                None
+            else
+                let peek = move it
+
+                let next =
+                    if obstructions.Contains({ x = peek.coord.x; y = peek.coord.y }) then
+                        turn it
+                    else
+                        peek
+
+                Some(it, next))
+
     [<Fact>]
     let ``part 01`` () =
-        let map = input "day06-input.txt" |> Seq.toList
-        let max = map |> List.length |> Int.minus 1
+        let max, obstructions, guard = getMap
+        let steps = getSteps max obstructions guard |> Seq.map _.coord |> Set.ofSeq
+        let distinctSteps = steps |> Set.count
 
-        let guard =
-            map
-            |> Seq.mapi (fun y s ->
-                match s |> Seq.tryFindIndex (fun c -> c = '^') with
-                | Some x -> Some { x = x; y = y }
-                | None -> None)
-            |> Seq.find _.IsSome
-            |> Option.get
-            |> (fun c -> { coord = c; direction = NORTH })
-
-        let obstructions =
-            [ 0..max ]
-            |> Seq.collect (fun x ->
-                ([ 0..max ]
-                 |> Seq.choose (fun y -> if map[y][x] = '#' then Some { x = x; y = y } else None)))
-            |> Set.ofSeq
-
-        let steps =
-            guard
-            |> Seq.unfold (fun it ->
-                if it.coord.x < 0 || it.coord.x > max || it.coord.y < 0 || it.coord.y > max then
-                    None
-                else
-                    let peek = move it
-
-                    let next =
-                        if (obstructions.Contains({ x = peek.coord.x; y = peek.coord.y })) then
-                            turn it |> move
-                        else
-                            peek
-
-                    Some(it, next))
-
-        let distinctSteps = steps |> Seq.map _.coord |> Set.ofSeq |> Set.count
+        // [ 0..max ]
+        // |> List.iter (fun y1 ->
+        //     printf " "
+        //
+        //     [ 0..max ]
+        //     |> List.iter (fun x1 ->
+        //         if obstructions.Contains { x = x1; y = y1 } then printf "#"
+        //         else if steps.Contains { x = x1; y = y1 } then printf "X"
+        //         else if guard.coord = { x = x1; y = y1 } then printf "^"
+        //         else printf ".")
+        //
+        //     printfn "")
+        //
+        // printfn "_"
 
         distinctSteps |> should equal 5212
+
+    [<Fact(Skip = "Slow")>]
+    let ``part 02`` () =
+        let max, obstructions, guard = getMap
+
+        let steps = seq { guard } |> Seq.append (getSteps max obstructions guard)
+
+        let possibleObstructions =
+            steps
+            |> Seq.map (fun it ->
+                let newObstruction = it |> move |> _.coord
+
+                if obstructions.Contains(newObstruction) || newObstruction = guard.coord then
+                    None
+                else
+                    let alternativeObstructions = obstructions |> Set.add newObstruction
+                    let alternativeSteps = getSteps max alternativeObstructions guard
+
+                    let mutable alternativeGuardStepsSet: Set<Guard> = Set.empty
+
+                    let findRepeat =
+                        alternativeSteps
+                        |> Seq.tryFind (fun alternativeStep ->
+                            let foundRepeatedStep = alternativeGuardStepsSet.Contains(alternativeStep)
+                            alternativeGuardStepsSet <- alternativeGuardStepsSet |> Set.add alternativeStep
+                            foundRepeatedStep)
+
+                    match findRepeat with
+                    | None -> None
+                    | Some _ -> Some newObstruction)
+            |> Seq.choose id
+            |> Set.ofSeq
+
+        // [ 0..max ]
+        // |> List.iter (fun y1 ->
+        //     printf " "
+        //
+        //     [ 0..max ]
+        //     |> List.iter (fun x1 ->
+        //         if possibleObstructions.Contains { x = x1; y = y1 } then
+        //             printf "O"
+        //         else if obstructions.Contains { x = x1; y = y1 } then
+        //             printf "#"
+        //         else if guard.coord = { x = x1; y = y1 } then
+        //             printf "^"
+        //         else
+        //             printf ".")
+        //
+        //     printfn "")
+        //
+        // printfn "_"
+
+        possibleObstructions |> Set.count |> should equal 1767
