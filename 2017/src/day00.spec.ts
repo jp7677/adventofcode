e8183f15abb63439f659393c3919e9b6b5d56feb
Jens Peters <jp7677@gmail.com>
2024-12-15 14:17:44 +0100

    2017: Set up eslint and prettier

--- 2017/src/day00.spec.ts
+++ 2017/src/day00.spec.ts
@@ -1,13 +1,10 @@
-import {describe, expect, test} from '@jest/globals';
-import {readInput} from "./util";
+import { describe, expect, test } from "@jest/globals";
+import { readInput } from "./util";
 
+describe("day 00", () => {
+  test("part 1", async () => {
+    const input = await readInput("day00-input.txt");
 
-describe('day 00', () => {
-
-  test('part 1', async () => {
-    const input = await readInput('day00-input.txt')
-
-    expect(input[0]).toBe('0')
+    expect(input[0]).toBe("0");
   });
-
 });
--- 2017/src/day01.spec.ts
+++ 2017/src/day01.spec.ts
@@ -1,40 +1,31 @@
-import {describe, expect, test} from '@jest/globals';
-import {readInput} from "./util";
+import { describe, expect, test } from "@jest/globals";
+import { readInput } from "./util";
 
+describe("day 01", () => {
+  test("part 1", async () => {
+    const input = await readInput("day01-input.txt");
+    const captcha = input[0].split("").map((c) => parseInt(c));
 
-describe('day 01', () => {
+    const sum = [...captcha, captcha[0]].reduce((acc, it, idx, array) => {
+      if (it == array[idx - 1]) return acc + it;
+      else return acc;
+    }, 0);
 
-  test('part 1', async () => {
-    const input = await readInput('day01-input.txt')
-    const captcha = input[0].split('').map(c => parseInt(c))
-
-    const sum = [...captcha, captcha[0]]
-      .reduce((acc, it, idx, array) => {
-        if (it == array[idx-1])
-          return acc + it
-        else
-          return acc
-      }, 0)
-
-    expect(sum).toBe(1029)
+    expect(sum).toBe(1029);
   });
 
-  test('part 2', async () => {
-    const input = await readInput('day01-input.txt')
-    const captcha = input[0].split('').map(c => parseInt(c))
+  test("part 2", async () => {
+    const input = await readInput("day01-input.txt");
+    const captcha = input[0].split("").map((c) => parseInt(c));
 
-    const forward = captcha.length / 2
-    const array = [...captcha, ...captcha]
+    const forward = captcha.length / 2;
+    const array = [...captcha, ...captcha];
 
-    const sum = captcha
-        .reduce((acc, it, idx) => {
-          if (it == array[idx+forward])
-            return acc + it
-          else
-            return acc
-        }, 0)
+    const sum = captcha.reduce((acc, it, idx) => {
+      if (it == array[idx + forward]) return acc + it;
+      else return acc;
+    }, 0);
 
-    expect(sum).toBe(1220)
+    expect(sum).toBe(1220);
   });
-
 });
