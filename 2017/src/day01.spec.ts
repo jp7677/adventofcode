b5ef05d1c32633cba2376236f435e2665ca3b5e2
Jens Peters <jp7677@gmail.com>
2024-12-15 14:04:16 +0100

    2017: Add day 1 part 2

--- 2017/src/day01.spec.ts
+++ 2017/src/day01.spec.ts
@@ -19,4 +19,22 @@ describe('day 01', () => {
     expect(sum).toBe(1029)
   });
 
+  test('part 2', async () => {
+    const input = await readInput('day01-input.txt')
+    const captcha = input[0].split('').map(c => parseInt(c))
+
+    const forward = captcha.length / 2
+    const array = [...captcha, ...captcha]
+
+    const sum = captcha
+        .reduce((acc, it, idx) => {
+          if (it == array[idx+forward])
+            return acc + it
+          else
+            return acc
+        }, 0)
+
+    expect(sum).toBe(1220)
+  });
+
 });
