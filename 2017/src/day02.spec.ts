babc95000554d354e0ae8a68e35843d58162d243
Jens Peters <jp7677@gmail.com>
2024-12-15 18:37:42 +0100

    2017: Add day 2 part 2

--- 2017/src/day02.spec.ts
+++ 2017/src/day02.spec.ts
@@ -4,25 +4,42 @@ import { readInput } from "./util";
 describe("day 02", () => {
   test("part 1", async () => {
     const input = await readInput("day02-input.txt");
-
     const spreadsheet = input.map(
-      (line) => line.split(RegExp("\t")).map((c) => parseInt(c)), // eslint-disable-line no-control-regex
+      // eslint-disable-next-line no-control-regex
+      (line) => line.split(RegExp("\t")).map((c) => parseInt(c)),
     );
 
-    const diff = spreadsheet.map((row) => {
-      const largest = row.reduce((acc, it) => {
-        if (it > acc) return it;
-        else return acc;
-      }, 0);
-      const smallest = row.reduce((acc, it) => {
-        if (it < acc) return it;
-        else return acc;
-      });
-      return largest - smallest;
-    });
-
-    const checksum = diff.reduce((acc, it) => acc + it);
+    const checksum = spreadsheet
+      .map((row) => row.max() - row.min())
+      .reduce((acc, it) => acc + it);
 
     expect(checksum).toBe(45158);
+  });
+
+  test("part 2", async () => {
+    const input = await readInput("day02-input.txt");
+    const spreadsheet = input.map(
+      // eslint-disable-next-line no-control-regex
+      (line) => line.split(RegExp("\t")).map((c) => parseInt(c)),
+    );
+
+    const checksum = spreadsheet
+      .map((row) => {
+        let n1 = 0;
+        let n2 = 0;
+        row.forEach((a) => {
+          row.forEach((b) => {
+            if (a != b && a % b == 0) {
+              n1 = a;
+              n2 = b;
+            }
+          });
+        });
+
+        return n1 > n2 ? n1 / n2 : n2 / n1;
+      })
+      .reduce((acc, it) => acc + it);
+
+    expect(checksum).toBe(294);
   });
 });
--- 2017/src/util.ts
+++ 2017/src/util.ts
@@ -6,3 +6,25 @@ export const readInput = async (input: string) =>
     .toString()
     .trimEnd()
     .split("\n");
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface Array<T> {
+    max(): number;
+    min(): number;
+  }
+}
+
+Array.prototype.max = function () {
+  return this.reduce((acc, it) => {
+    if (it > acc) return it;
+    else return acc;
+  }, 0);
+};
+
+Array.prototype.min = function () {
+  return this.reduce((acc, it) => {
+    if (it < acc) return it;
+    else return acc;
+  });
+};
