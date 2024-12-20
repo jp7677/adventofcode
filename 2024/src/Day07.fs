ffc92bdddb9d5d89f8a9128a9d63ff211cf8cb2a
Jens Peters <jp7677@gmail.com>
2024-12-20 11:01:48 +0100

    2024: Add day 7

---
+++ 2024/src/Day07.fs
@@ -0,0 +1,51 @@
+﻿namespace Aoc2024
+
+open Xunit
+open FsUnit.Xunit
+open Util
+
+module Day07 =
+
+    let opAdd (a: int64, b: int64) = a + b
+
+    let opMul (a: int64, b: int64) =
+        (if a = 0 then 1L else a) * (if b = 0 then 1L else b)
+
+    let opConcat (a: int64, b: int64) = $"{a}{b}" |> int64
+
+    let rec evaluate (operands, acc, index, maxIndex, maxResult, ops) =
+        if index = maxIndex then
+            seq [ acc ]
+        else if acc > maxResult then
+            seq [ 0L ]
+        else
+            let operand = operands |> Seq.item index
+
+            ops
+            |> Seq.collect (fun op -> evaluate (operands, op (acc, operand), index + 1, maxIndex, maxResult, ops))
+
+    let calculateCalibrationResult ops =
+        input "day07-input.txt"
+        |> Seq.map _.Split(':')
+        |> Seq.map (fun p -> p[0] |> int64, (p[1].Trim().Split(' ') |> Seq.map int64))
+        |> Seq.filter (fun equation ->
+            let result = fst equation
+            let operands = snd equation
+            let operand = operands |> Seq.item 0
+            let maxLength = operands |> Seq.length
+
+            evaluate (operands, operand, 1, maxLength, result, ops) |> Seq.contains result)
+        |> Seq.map fst
+        |> Seq.sum
+
+    [<Fact>]
+    let ``part 01`` () =
+        let calibrationResult = calculateCalibrationResult [ opAdd; opMul ]
+
+        calibrationResult |> should equal 1153997401072L
+
+    [<Fact>]
+    let ``part 02`` () =
+        let calibrationResult = calculateCalibrationResult [ opAdd; opMul; opConcat ]
+
+        calibrationResult |> should equal 97902809384118L
