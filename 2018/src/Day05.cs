79ca7a8a89884a327ad9a6fa43301e9b22338bb7
Jens Peters <jp7677@gmail.com>
2024-12-22 10:23:46 +0100

    2018: Add day 5

---
+++ 2018/src/Day05.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace aoc2018;
+
+public class Day05
+{
+    [Fact]
+    public void Part01()
+    {
+        var polymer = Util.GetInputAsStrings("day05-input.txt").ToBlockingEnumerable().Single();
+
+        polymer = GetReactingPolymer(polymer);
+
+        Assert.Equal(11590, polymer.Length);
+    }
+
+    [Fact]
+    public void Part02()
+    {
+        var polymer = Util.GetInputAsStrings("day05-input.txt").ToBlockingEnumerable().Single();
+        const string units = "abcdefghijklmnopqrstuvwxyz";
+
+        var shortest = units
+            .ToArray()
+            .Where(u => polymer.ToLower().Contains(u))
+            .Select(u =>
+                GetReactingPolymer(
+                    new string(polymer.Where(c => char.ToLower(c) != u).ToArray())
+                ).Length
+            )
+            .Min();
+
+        Assert.Equal(4504, shortest);
+    }
+
+    private static string GetReactingPolymer(string polymer)
+    {
+        HashSet<int> indexes;
+        while ((indexes = GetDestroyIndexes(polymer)).Count > 0)
+        {
+            polymer = new string(
+                polymer.Where((_, i) => !indexes.Contains(i) && !indexes.Contains(i - 1)).ToArray()
+            );
+        }
+
+        return polymer;
+    }
+
+    private static HashSet<int> GetDestroyIndexes(string polymer)
+    {
+        var destroyIndexes = polymer
+            .Remove(polymer.Length - 1)
+            .Select((c, i) => (c, polymer[i + 1]))
+            .Select((t, i) => Destroys(t) ? i : -1)
+            .Where(i => i != -1)
+            .ToHashSet();
+        var sanitizedDestroyIndexes = destroyIndexes
+            .Where(i => !destroyIndexes.Contains(i - 1))
+            .ToHashSet();
+        return sanitizedDestroyIndexes;
+    }
+
+    private static bool Destroys((char c, char) t) =>
+        t.c.ToString().Equals(t.Item2.ToString(), StringComparison.CurrentCultureIgnoreCase)
+        && (
+            (char.IsLower(t.c) && char.IsUpper(t.Item2))
+            || (char.IsUpper(t.c) && char.IsLower(t.Item2))
+        );
+}
