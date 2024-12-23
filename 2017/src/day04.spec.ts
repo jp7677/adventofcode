63163fbf2b421d2eeaedaf724c9910c0068bc6eb
Jens Peters <jp7677@gmail.com>
2024-12-23 22:14:30 +0100

    2017: Add day 4

---
+++ 2017/src/day04.spec.ts
@@ -0,0 +1,31 @@
+import { describe, expect, test } from "@jest/globals";
+import { readInput } from "./util";
+
+describe("day 04", () => {
+  test("part 1", async () => {
+    const input = await readInput("day04-input.txt");
+
+    const passphrases = input.map((p) => p.split(" "));
+    const valid = passphrases.reduce(
+      (acc, it) => acc + (it.length == new Set(it).size ? 1 : 0),
+      0,
+    );
+
+    expect(valid).toBe(386);
+  });
+
+  test("part 2", async () => {
+    const input = await readInput("day04-input.txt");
+
+    const passphrases = input
+      .map((p) => p.split(" "))
+      .map((p) => p.map((p) => p.split("").sort().join("")));
+
+    const valid = passphrases.reduce(
+      (acc, it) => acc + (it.length == new Set(it).size ? 1 : 0),
+      0,
+    );
+
+    expect(valid).toBe(208);
+  });
+});
