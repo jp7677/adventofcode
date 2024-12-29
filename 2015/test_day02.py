b62b3e1831bdb4f5ebcf8b24800cfe80eafc03bb
Jens Peters <jp7677@gmail.com>
2024-12-29 10:02:35 +0100

    2015: Add day 2

---
+++ 2015/test_day02.py
@@ -0,0 +1,23 @@
+import re
+from util import read_input
+
+
+def test_part01():
+    input_data = read_input("day02-input.txt").split('\n')
+    presents = map(lambda it: list(map(int, re.findall(r'(\d+)', it))) , input_data)
+
+    square_feets = map(lambda it: [it[0]*it[1], it[0]*it[2], it[1]*it[2]], presents)
+    square_feets = map(lambda it: 2*it[0] + 2*it[1] + 2*it[2] + min(it), square_feets)
+    square_feets = sum(square_feets)
+
+    assert square_feets == 1588178
+
+def test_part02():
+    input_data = read_input("day02-input.txt").split('\n')
+    presents = map(lambda it: list(map(int, re.findall(r'(\d+)', it))) , input_data)
+
+    square_feets = map(lambda it:
+        2 * min([it[0]+it[1], it[0]+it[2], it[1]+it[2]]) + (it[0] * it[1] * it[2]), presents)
+    square_feets = sum(square_feets)
+
+    assert square_feets == 3783758
