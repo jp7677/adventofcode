e028a0901ec7399662b56d44c3790491c6dc61d3
Jens Peters <jp7677@gmail.com>
2025-03-21 11:56:45 +0100

    2015: Add day 5 part 2

--- 2015/test_day05.py
+++ 2015/test_day05.py
@@ -6,7 +6,10 @@ from util import read_input
 def test_part01():
     input_data = read_input("day05-input.txt").split('\n')
 
-    nice = filter(is_nice, input_data)
+    nice = filter(
+        lambda it: at_least_3_vowels(it) and one_letter_twice(it) and no_forbidden(it),
+        input_data
+    )
     nice = list(nice)
 
     assert len(nice) == 238
@@ -38,5 +41,36 @@ def no_forbidden(line: str):
     return True
 
 
-def is_nice(line):
-    return at_least_3_vowels(line) and one_letter_twice(line) and no_forbidden(line)
+def test_part02():
+    input_data = read_input("day05-input.txt").split('\n')
+
+    nice = filter(
+        lambda it: pair_of_any_two_letter(it) and one_letter_twice_with_one_between(it),
+        input_data
+    )
+    nice = list(nice)
+
+    assert len(nice) == 69
+
+
+def pair_of_any_two_letter(line: str):
+    firsts = line[1:]
+    seconds = line[:-1]
+    for first, second in zip(firsts, seconds):
+        s = second + first
+        li = line.find(s)
+        ri = line.rfind(s)
+        if ri > li + 1:
+            return True
+
+    return False
+
+
+def one_letter_twice_with_one_between(line: str):
+    firsts = line[2:]
+    seconds = line[:-2]
+    for first, second in zip(firsts, seconds):
+        if first == second:
+            return True
+
+    return False
