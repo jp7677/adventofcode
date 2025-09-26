e81f1c09f3dbd5c4c68af47bed5a2716c5741d0c
Jens Peters <jp7677@gmail.com>
2025-09-26 17:22:41 +0200

    2025: Setup Lua

---
+++ 2025/src/day00_spec.lua
@@ -0,0 +1,18 @@
+-- luacheck: ignore _dbg
+local _dbg = require "debugger"
+local bstd = require "busted"
+local insp = require "inspect"
+local fun = require "fun"
+local util = require "util"
+
+bstd.describe("day 00", function()
+    bstd.it("solves part 1", function()
+        local input = util.load_input("00")
+
+        local fn_tonumber = function(x) return tonumber(x) end
+        input = fun.map(fn_tonumber, input):totable()
+
+        bstd.assert.are.same({0}, input)
+        bstd.assert.are.same("{ 0 }", insp(input))
+    end)
+end)
---
+++ 2025/src/main.lua
@@ -0,0 +1,1 @@
+print("Use `LUA_INIT=@src/setup.lua ./lua_modules/bin/busted` to run tests.")
