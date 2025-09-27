ddf9745442e2066db05ab0be3ab0088d3357d046
Jens Peters <jp7677@gmail.com>
2025-09-27 21:15:16 +0200

    2025: Read lines using Lua Fun

--- 2025/src/util.lua
+++ 2025/src/util.lua
@@ -1,11 +1,25 @@
+local fun = require "fun"
+
 local util = {}
 
-function util.load_input(day)
-    local lines = {}
-    for line in io.lines("./data/day" .. day .. "-input.txt") do
-        lines[#lines + 1] = line
+-- from https://github.com/luafun/luafun/issues/20#issuecomment-207777263
+local function dup(n, f, s, var)
+    local dup_helper = function(i, var_1, ...)
+        if var_1 ~= nil then
+            return var_1, select(i, var_1, ...), ...
+        end
     end
-    return lines
+
+    if type(n) ~= "number" then
+        n, f, s, var = 1, n, f, s
+    end
+    return function(s2, v)
+        return dup_helper(n, f( s2, v ))
+    end, s, var
+end
+
+function util.load_input(day)
+    return fun.totable(dup(io.lines("./data/day" .. day .. "-input.txt")))
 end
 
 return util
