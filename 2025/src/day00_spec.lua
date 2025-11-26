e5f2ccd8cba3b12be3e97c68e10c23928b83d468
Jens Peters <jp7677@gmail.com>
2025-11-26 17:56:00 +0100

    2025: Try different style

--- 2025/src/day00_spec.lua
+++ 2025/src/day00_spec.lua
@@ -3,14 +3,14 @@ local insp = require "inspect"
 local fun = require "fun"
 local util = require "util"
 
-bstd.describe("day 00", function()
-    bstd.it("solves part 1", function()
-        local input = util.load_input("00")
+local fn_day00 = function ()
+    local input = util.load_input("00")
 
-        local fn_tonumber = function(x) return tonumber(x) end
-        input = fun.map(fn_tonumber, input):totable()
+    local fn_tonumber = function(x) return tonumber(x) end
+    input = fun.map(fn_tonumber, input):totable()
 
-        bstd.assert.are.same({0}, input)
-        bstd.assert.are.same("{ 0 }", insp(input))
-    end)
-end)
+    bstd.assert.same({0}, input)
+    bstd.assert.same("{ 0 }", insp(input))
+end
+
+bstd.it("solves day 00", fn_day00)
