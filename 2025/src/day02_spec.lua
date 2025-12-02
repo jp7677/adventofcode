03916b4e42db32d4b05fb3af64659d4b85dc868c
Jens Peters <jp7677@gmail.com>
2025-12-02 21:11:33 +0100

    2025: Optimize

--- 2025/src/day02_spec.lua
+++ 2025/src/day02_spec.lua
@@ -5,9 +5,7 @@ local util = require "util"
 
 local chuncked = function(str, size)
     local res = {}
-    if #str % size ~= 0 then
-        return res
-    end
+    if #str % size ~= 0 then return res end
 
     local parts = #str / size
     for _, i in fun.range(0, parts - 1) do
@@ -24,52 +22,41 @@ bstd.it("chuncked", function()
     bstd.assert.same(insp(chuncked('112233', 3)), '{ "112", "233" }')
 end)
 
-local fn_day00_part1 = function()
-    local input = util.load_input("02")[1]
+local sum_of_invalid = function(fn_filter)
+    local input = util.load_input("02")
+    local ranges = util.stringsplit(input[1], ',')
 
-    local ranges = util.stringsplit(input, ',')
-
-    local invalid = fun.iter(ranges)
-        :map(function(x)
+    return fun.iter(ranges)
+        :foldl(function(acc, x)
             local p1, p2 = table.unpack(util.stringsplit(x, '-'))
-            return fun.range(tonumber(p1), tonumber(p2))
-                :filter(function (y)
-                    if #tostring(y) % 2 ~= 0 then return false end
-                    local s = tostring(y)
-                    local c1, c2 = table.unpack(chuncked(s, #s / 2))
-                    return c1 == c2
-                end)
-                :map(function(y) return tonumber(y) end)
-                :sum()
-        end)
-        :sum()
+            return acc + fun.range(tonumber(p1), tonumber(p2))
+                :filter(function (y) return fn_filter(y) end)
+                :sum(function(y) return y end)
+        end, 0)
+end
+
+local fn_day00_part1 = function()
+    local invalid = sum_of_invalid(function (y)
+        local s = tostring(y)
+        if #s % 2 ~= 0 then return false end
+        local c1, c2 = table.unpack(chuncked(s, #s / 2))
+        return c1 == c2
+    end)
 
     bstd.assert.same(19605500130, invalid)
 end
 
 local fn_day00_part2 = function()
-    local input = util.load_input("02")[1]
-
-    local ranges = util.stringsplit(input, ',')
-
-    local invalid = fun.iter(ranges)
-        :map(function(x)
-            local p1, p2 = table.unpack(util.stringsplit(x, '-'))
-            return fun.range(tonumber(p1), tonumber(p2))
-                :filter(function(y)
-                    local s = tostring(y)
-                    for _, p in fun.range(1, #s / 2) do
-                        if #s > 1 and #s % p == 0 then
-                            local chuncks = chuncked(s, p)
-                            local same = fun.all(function(a) return a == chuncks[1] end, chuncks)
-                            if same == true then return true end
-                        end
-                    end
-                end)
-                :map(function(y) return tonumber(y) end)
-                :sum()
-        end)
-        :sum()
+    local invalid = sum_of_invalid(function(y)
+        local s = tostring(y)
+        for p in fun.range(#s / 2) do
+            if #s > 1 and #s % p == 0 then
+                local chuncks = chuncked(s, p)
+                local same = fun.all(function(a) return a == chuncks[1] end, chuncks)
+                if same == true then return true end
+            end
+        end
+    end)
 
     bstd.assert.same(36862281418, invalid)
 end
