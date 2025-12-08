f7b0d34d864eaa8179415abc5dba4672a944ab5a
Jens Peters <jp7677@gmail.com>
2025-12-08 22:07:39 +0100

    2025: Add day 07 part 2
    Actually merge 1 and 2 together.

--- 2025/src/day07_spec.lua
+++ 2025/src/day07_spec.lua
@@ -1,5 +1,4 @@
 local bstd = require "busted"
-local fun = require "fun"
 local util = require "util"
 local set = require "set"
 
@@ -9,91 +8,57 @@ local function load_coords()
     local start
     local coords = {}
     local y_max
-    for y, r in ipairs(input) do
+    for y, row in ipairs(input) do
         y_max = y
-        for x = 1, #r do
-            if r:sub(x, x) == 'S' then
-                start = { x = x, y = y}
+        for x = 1, #row do
+            if row:sub(x, x) == 'S' then
+                start = { x = x, y = y }
             end
-            if r:sub(x, x) == '^' then
-                coords[util.pack_coord({ x = x, y = y})] = true
+            if row:sub(x, x) == '^' then
+                set.add(coords, util.pack_coord({ x = x, y = y }))
             end
         end
     end
     return start, coords, y_max
 end
 
-local fn_day07_part1 = function()
+local fn_day07 = function()
     local start, coords, y_max = load_coords()
 
-    local splitted = 0
-    local acc = {}
-    set.add(acc, start.x)
+    local splits = 0
+    local timelines = 0
+
+    local beams = {}
+    beams[start.x] = 1
     for y = start.y, y_max, 2 do
-        local splits = {}
-        for i, v in pairs(acc) do
-            if v ~= nil and coords[util.pack_coord({x = i, y = y})] then set.add(splits, i) end
+        local splitters = {}
+        for i, _ in pairs(beams) do
+            if set.contains(coords, util.pack_coord({x = i, y = y})) then set.add(splitters, i) end
         end
 
-        for i, v in pairs(splits) do
-            if v ~= nil then
-                set.remove(acc, i)
-                set.add(acc, i - 1)
-                set.add(acc, i + 1)
-            end
+        for i, _ in pairs(splitters) do
+            local tl0 = beams[i]
+            local tl1 = beams[i - 1]
+            local tl2 = beams[i + 1]
+
+            if tl0 == nil then tl0 = 0 end
+            if tl1 == nil then tl1 = 0 end
+            if tl2 == nil then tl2 = 0 end
+
+            beams[i] = nil
+            beams[i - 1] = tl0 + tl1
+            beams[i + 1] = tl0 + tl2
         end
 
-        splitted = splitted + set.length(splits)
+        splits = splits + set.length(splitters)
     end
 
-    bstd.assert.same(1533, splitted)
-end
-
-local function traverse(start, coords, y_max)
-    local acc = {}
-    set.add(acc, start.x)
-
-    local timelines = {}
-    for y = start.y, y_max, 2 do
-        local splits = {}
-        for i, v in pairs(acc) do
-            if v ~= nil and coords[util.pack_coord({x = i, y = y})] then set.add(splits, i) end
-        end
-
-        for i, v in pairs(splits) do
-            if v ~= nil then
-                set.remove(acc, i)
-                set.add(acc, i - 1)
-                timelines[#timelines + 1] = { x = i + 1, y = y }
-            end
-        end
+    for _, v in pairs(beams) do
+        timelines = timelines + v
     end
 
-    return timelines
+    bstd.assert.same(1533, splits)
+    bstd.assert.same(10733529153890, timelines)
 end
 
-local fn_day07_part2 = function()
-    local start, coords, y_max = load_coords()
-
-    local timelines = { start }
-    local total_timelines = 1
-    while #timelines > 0 do
-        local new_timelines = fun.iter(timelines)
-            :map(function(t)
-                return traverse(t, coords, y_max)
-            end)
-            :totable()
-
-        if #new_timelines == 0 then break end
-
-        timelines = fun.iter(new_timelines)
-            :foldl(function(acc, x) return fun.chain(acc, x) end, {})
-            :totable()
-        total_timelines = total_timelines + #timelines
-    end
-
-    bstd.assert.same(40, total_timelines) -- sample input
-end
-
-bstd.it("solves #day07 part 1", fn_day07_part1)
-bstd.it("solves #day07 part 2, #ignore because works only for small room", fn_day07_part2)
+bstd.it("solves #day07 part 1 & part 2", fn_day07)
