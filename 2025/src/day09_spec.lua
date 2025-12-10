c525c0e6a55cdeddacdfd5718755479c9b91427d
Jens Peters <jp7677@gmail.com>
2025-12-10 20:34:28 +0100

    2025: Add day 09 part 2
    Ugly solution...

--- 2025/src/day09_spec.lua
+++ 2025/src/day09_spec.lua
@@ -2,21 +2,25 @@ local bstd = require "busted"
 local fun = require "fun"
 local util = require "util"
 
-local fn_day09_part1 = function()
+local function load_coords()
     local input = util.load_input("09")
 
-    local coords = fun.iter(input)
-    :map(function(line)
-        local x, y, z = table.unpack(util.stringsplit(line, ','))
-        return { x = tonumber(x), y = tonumber(y), z = tonumber(z) }
-    end)
-    :totable()
+    return fun.iter(input)
+        :map(function(line)
+            local x, y = table.unpack(util.stringsplit(line, ','))
+            return { x = tonumber(x), y = tonumber(y) }
+        end)
+        :totable()
+end
+
+local fn_day09_part1 = function()
+    local coords = load_coords()
 
     local areas = {}
     for _, coord in pairs(coords) do
         for _, other in pairs(coords) do
             if coord.x ~= other.x and coord.y ~= other.y  then
-                areas[#areas + 1] = math.abs(coord.x - other.x + 1) * math.abs(coord.y - other.y + 1)
+                areas[#areas + 1] = (math.abs(coord.x - other.x) + 1) * (math.abs(coord.y - other.y) + 1)
             end
         end
     end
@@ -26,4 +30,43 @@ local fn_day09_part1 = function()
     bstd.assert.same(4771532800, areas[1])
 end
 
+local fn_day09_part2 = function()
+    local coords = load_coords()
+
+    -- our fix point from the data which "divides" the oval, not cool to hardcode those :(
+    local other1 = { x = 94872, y = 48511 }
+    local other2 = { x = 94872, y = 50262 }
+
+    local areas1 = fun.iter(coords)
+        :filter(function(x) return x.y < other1.y end)
+        :map(function(coord)
+            if not fun.any(function(x)
+                    return ((x.x > coord.x and x.x < other1.x) and (x.y > coord.y and x.y < other1.y))
+                end, coords)
+            then
+                return (math.abs(coord.x - other1.x) + 1) * (math.abs(coord.y - other1.y) + 1)
+            else
+                return 0
+            end
+        end)
+
+    local areas2 = fun.iter(coords)
+        :filter(function(x) return x.y > other2.y end)
+        :map(function(coord)
+            if not fun.any(function(x)
+                    return ((x.x < other2.x and x.x > coord.x) and (x.y < coord.y and x.y > other2.y))
+                end, coords)
+            then
+                return (math.abs(coord.x - other2.x) + 1) * (math.abs(coord.y - other2.y) + 1)
+            else
+                return 0
+            end
+        end)
+
+    local max_area = fun.max(fun.chain(areas1, areas2))
+
+    bstd.assert.same(1544362560, max_area)
+end
+
 bstd.it("solves #day09 part 1", fn_day09_part1)
+bstd.it("solves #day09 part 2", fn_day09_part2)
