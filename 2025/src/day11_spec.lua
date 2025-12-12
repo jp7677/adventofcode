5f194162df49f8b91c0bef075e713864e43cc0d3
Jens Peters <jp7677@gmail.com>
2025-12-12 14:03:22 +0100

    2025: Optimize

--- 2025/src/day11_spec.lua
+++ 2025/src/day11_spec.lua
@@ -3,33 +3,34 @@ local fun = require "fun"
 local util = require "util"
 
 local function follow(list, device, count)
-    if device == 'out' then
-        count[1] = count[1] + 1
-        return
-    end
+    if device == 'out' then return count + 1 end
 
     local connection = fun.iter(list)
         :filter(function(x) return x.device == device end)
         :totable()
 
-    if #connection == 0 then return end
+    if #connection == 0 then return count end
 
-    fun.each(function(x) follow(list, x, count) end, connection[1].outputs)
+    return fun.iter(connection[1].outputs)
+        :foldl(function(acc, x)
+            return acc + follow(list, x, count)
+        end, count)
 end
 
 local fn_day11_part1 = function()
     local input = util.load_input("11")
 
-    local list = fun.map(function(x)
-        local device = string.sub(x, 1, 3)
-        local outputs = util.stringsplit(string.sub(x, 6), ' ')
-        return { device = device, outputs = outputs }
-    end, input):totable()
+    local list = fun.iter(input)
+        :map(function(x)
+            local device = string.sub(x, 1, 3)
+            local outputs = util.stringsplit(string.sub(x, 6), ' ')
+            return { device = device, outputs = outputs }
+        end)
+        :totable()
 
-    local count = { 0 }
-    follow(list, 'you', count)
+    local count = follow(list, 'you', 0)
 
-    bstd.assert.same(423, count[1])
+    bstd.assert.same(423, count)
 end
 
 bstd.it("solves #day11 part 1", fn_day11_part1)
