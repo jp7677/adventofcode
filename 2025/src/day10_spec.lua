c78d555c4bbcd768a83fa03a2db532a4327067aa
Jens Peters <jp7677@gmail.com>
2025-12-13 16:44:06 +0100

    2025: DFS to BFS
    It's getting usable.

--- 2025/src/day10_spec.lua
+++ 2025/src/day10_spec.lua
@@ -17,54 +17,12 @@ local function copy(buttons)
     return c
 end
 
-local function diff(diagram, lights)
-    local d = 0
-    for i, _ in ipairs(diagram) do
-        d = d + ((lights[i] ~= diagram[i]) and 1 or 0)
-    end
-    return d
-end
-
 local function switch(lights, buttons)
     local l = copy(lights)
     for _, v in ipairs(buttons) do
         l[v + 1] = not l[v + 1]
     end
     return l
-end
-
-local function push_buttons(diagram, lights, buttons, pushes, min_pushes)
-    if min_pushes[1] <= pushes then return end
-    if #buttons == 0 then return end
-
-    if equals(diagram, lights) then
-        if pushes < min_pushes[1] then min_pushes[1] = pushes end
-        return
-    end
-
-    local sorted = {}
-    for _, v in ipairs(buttons) do
-        sorted[#sorted + 1] = { buttons = v, diff = diff(diagram, switch(lights, v)) }
-    end
-
-    table.sort(sorted, function(a, b) return a.diff < b.diff end)
-
-    if sorted[1].diff == 0 then
-        min_pushes[1] = pushes + 1
-        return
-    end
-
-    for _, v in ipairs(sorted) do
-        local l = switch(lights, v.buttons)
-        local b = copy(buttons)
-        for i, w in ipairs(b) do
-            if w == v.buttons then
-                table.remove(b, i)
-            end
-        end
-
-        push_buttons(diagram, l, b, pushes + 1, min_pushes)
-    end
 end
 
 local fn_day10_part1 = function()
@@ -98,14 +56,41 @@ local fn_day10_part1 = function()
         end, input)
         :totable()
 
-    local fewest_pushes = fun.foldl(function(acc, x)
-        local min_pushes = { math.maxinteger }
-        push_buttons(x.diagram, x.lights, x.buttons, 0, min_pushes)
+    local fewest_pushes = fun.foldl(function(acc, machine)
+        local min_pushes = math.maxinteger
+        local states = { { lights = machine.lights, tested_buttons = {} } }
 
-        return acc + min_pushes[1]
+        for pushes = 1, #machine.buttons do
+            local new_states = {}
+            for _, state in ipairs(states) do
+                local remaining_buttons = fun.iter(machine.buttons)
+                    :filter(function(x)
+                        return not fun.any(function(y)
+                            return table.concat(x) == table.concat(y)
+                        end, state.tested_buttons)
+                    end)
+                    :totable()
+
+                for _, v in ipairs(remaining_buttons) do
+                    local lights = switch(state.lights, v)
+                    if equals(lights, machine.diagram) then
+                        min_pushes = pushes
+                        goto found
+                    else
+                        local tested = copy(state.tested_buttons)
+                        table.insert(tested, v)
+                        table.insert(new_states, { lights = lights, tested_buttons = tested })
+                    end
+                end
+            end
+            states = new_states
+        end
+
+        ::found::
+        return acc + min_pushes
     end, 0, machines)
 
     bstd.assert.same(542, fewest_pushes)
 end
 
-bstd.it("solves #day10 part 1, #ignore because very very slow", fn_day10_part1)
+bstd.it("solves #day10 part 1", fn_day10_part1)
