6053ffdb6ec7b6a720c7e64bc56a2e046c388e2e
Jens Peters <jp7677@gmail.com>
2025-12-13 17:07:16 +0100

    2025: Use bit magic

--- 2025/src/day10_spec.lua
+++ 2025/src/day10_spec.lua
@@ -2,27 +2,12 @@ local bstd = require "busted"
 local fun = require "fun"
 local util = require "util"
 
-local function equals(diagram, lights)
-    for i, v in ipairs(diagram) do
-        if lights[i] ~= v then return false end
-    end
-    return true
-end
-
 local function copy(buttons)
     local c = {}
     for i, v in ipairs(buttons) do
         c[i] = v
     end
     return c
-end
-
-local function switch(lights, buttons)
-    local l = copy(lights)
-    for _, v in ipairs(buttons) do
-        l[v + 1] = not l[v + 1]
-    end
-    return l
 end
 
 local fn_day10_part1 = function()
@@ -32,6 +17,7 @@ local fn_day10_part1 = function()
         :map(function(x)
             local d1 = string.find(x, ']')
             local d2 = string.find(x, '{')
+
             local diagram = {}
             for i = 1, d1 - 2 do
                 if string.sub(x, i + 1, i + 1) == '#' then
@@ -40,10 +26,12 @@ local fn_day10_part1 = function()
                     diagram[i] = false
                 end
             end
-            local lights = {}
-            for i = 1, d1 - 2 do
-                lights[i] = false
-            end
+            local diagram_packed = fun.foldl(function(acc, y)
+                return (acc << 1) + (y and 1 or 0)
+            end, 0, diagram)
+
+            local lights = 0
+
             local buttons = fun.iter(util.stringsplit(string.sub(x, d1 + 1, d2 - 1), ' '))
                 :map(function(y)
                     return fun.iter(util.stringsplit(string.sub(y, 2, #y - 1), ','))
@@ -51,46 +39,59 @@ local fn_day10_part1 = function()
                         :totable()
                 end)
                 :totable()
+            local buttons_packed = fun.iter(buttons)
+                :map(function(b)
+                    return fun.iter(fun.range(0, #diagram - 1))
+                        :foldl(function(acc, y)
+                            return (acc << 1) + (fun.index(y, b) and 1 or 0)
+                        end, 0)
+                end)
+                :totable()
+
             local joltages = util.stringsplit(string.sub(x, d2 + 1, #x - 1), ',')
-            return { diagram = diagram, lights = lights, buttons = buttons, joltages = joltages }
+
+            return { diagram = diagram_packed, lights = lights, buttons = buttons_packed, joltages = joltages }
         end, input)
         :totable()
 
-    local fewest_pushes = fun.foldl(function(acc, machine)
+    local min_pushes = fun.foldl(function(acc, machine)
         local min_pushes = math.maxinteger
         local states = { { lights = machine.lights, tested_buttons = {} } }
 
         for pushes = 1, #machine.buttons do
-            local new_states = {}
+            local next_states = {}
             for _, state in ipairs(states) do
-                local remaining_buttons = fun.iter(machine.buttons)
-                    :filter(function(x)
-                        return not fun.any(function(y)
-                            return table.concat(x) == table.concat(y)
-                        end, state.tested_buttons)
-                    end)
-                    :totable()
+                local untested_buttons = {}
+                for _, b in ipairs(machine.buttons) do
+                    for _, t in ipairs(state.tested_buttons) do
+                        if b == t then
+                            goto next
+                        end
+                    end
+                    table.insert(untested_buttons, b)
+                    ::next::
+                end
 
-                for _, v in ipairs(remaining_buttons) do
-                    local lights = switch(state.lights, v)
-                    if equals(lights, machine.diagram) then
+                for _, v in ipairs(untested_buttons) do
+                    local lights = state.lights ~ v
+                    if lights == machine.diagram then
                         min_pushes = pushes
-                        goto found
+                        goto machine_started
                     else
                         local tested = copy(state.tested_buttons)
                         table.insert(tested, v)
-                        table.insert(new_states, { lights = lights, tested_buttons = tested })
+                        table.insert(next_states, { lights = lights, tested_buttons = tested })
                     end
                 end
             end
-            states = new_states
+            states = next_states
         end
 
-        ::found::
+        ::machine_started::
         return acc + min_pushes
     end, 0, machines)
 
-    bstd.assert.same(542, fewest_pushes)
+    bstd.assert.same(542, min_pushes)
 end
 
 bstd.it("solves #day10 part 1", fn_day10_part1)
