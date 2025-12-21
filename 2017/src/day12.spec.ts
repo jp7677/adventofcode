2ca8a2225694af310ce501f3f862cb8a7db55410
Jens Peters <jp7677@gmail.com>
2025-12-21 16:44:15 +0100

    2017: Add day 12

---
+++ 2017/src/day12.spec.ts
@@ -0,0 +1,56 @@
+import { describe, expect, test } from "@jest/globals";
+import { readInput } from "./util";
+
+interface Pipe {
+  id: number;
+  connectedIds: Set<number>;
+}
+
+async function readGroups() {
+  const input = await readInput("day12-input.txt");
+  return input.map((line) => {
+    const parts = line.split(" <-> ");
+    const id = parseInt(parts[0]);
+    const connectedIds = parts[1].split(", ").map((n) => parseInt(n));
+    return { id: id, connectedIds: new Set(connectedIds) } as Pipe;
+  });
+}
+
+function discoverGroup(programs: Pipe[], id: number) {
+  const group = new Set<number>([id]);
+  let discoveredIds = new Array<number>();
+  do {
+    discoveredIds = group
+      .values()
+      .flatMap((id) => {
+        return programs.filter((p) => p.id === id).flatMap((v) => v.connectedIds.values().toArray());
+      })
+      .filter((id) => !group.has(id))
+      .toArray();
+
+    discoveredIds.forEach((id) => group.add(id));
+  } while (discoveredIds.length !== 0);
+  return group;
+}
+
+describe("day 12", () => {
+  test("part 1", async () => {
+    const programs = await readGroups();
+
+    const zeroGroup = discoverGroup(programs, 0);
+
+    expect(zeroGroup.size).toBe(115);
+  });
+
+  test("part 2", async () => {
+    const programs = await readGroups();
+
+    const groups = programs.reduce((acc, pipe) => {
+      const discoveredIds = new Set(acc.flatMap((p) => p.values().toArray()));
+      if (!discoveredIds.has(pipe.id)) acc.push(discoverGroup(programs, pipe.id));
+      return acc;
+    }, new Array<Set<number>>());
+
+    expect(groups.length).toBe(221);
+  });
+});
