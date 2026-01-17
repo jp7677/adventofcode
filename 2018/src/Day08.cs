04a04a5978fd6fcf9c259ee75f65c8743ff479b5
Jens Peters <jp7677@gmail.com>
2026-01-17 21:00:41 +0100

    2018: Add day 8 part 2

--- 2018/src/Day08.cs
+++ 2018/src/Day08.cs
@@ -7,45 +7,65 @@ namespace aoc2018;
 
 public class Day08
 {
-    private record Header
-    {
-        public int ChildrenCount;
-        public int MetadataCount;
-    }
+    private record Header(int Children, int Metadata);
 
     private record Node
     {
-        public readonly Header Header = new();
+        public readonly Header Header;
         public readonly List<Node> Children = [];
         public readonly List<int> Metadata = [];
 
+        public Node(int children, int metadata)
+        {
+            Header = new Header(children, metadata);
+        }
+
         public int MetadataSum() =>
             Children.Select(c => c.MetadataSum()).Sum() + Metadata.Select(c => c).Sum();
+
+        public int Value() =>
+            Children.Count == 0
+                ? Metadata.Select(c => c).Sum()
+                : Metadata.Select(c => c > Children.Count ? 0 : Children[c - 1].Value()).Sum();
     }
 
     [Fact]
     public async Task Part01()
     {
-        var numbers = await Util.GetInputAsStrings("day08-input.txt")
-            .Select(line => line.Split(' ').Select(int.Parse).ToArray())
-            .SingleAsync();
-
-        var position = -1;
-        var tree = ParseNode(numbers, ref position);
+        var numbers = await GetNumbers();
+        var tree = GetTree(numbers);
 
         Assert.Equal(42196, tree.MetadataSum());
     }
 
-    private static Node ParseNode(int[] numbers, ref int position)
+    [Fact]
+    public async Task Part02()
     {
-        var node = new Node();
-        node.Header.ChildrenCount = numbers[++position];
-        node.Header.MetadataCount = numbers[++position];
-        foreach (var _ in Enumerable.Range(0, node.Header.ChildrenCount))
-            node.Children.Add(ParseNode(numbers, ref position));
-        foreach (var _ in Enumerable.Range(0, node.Header.MetadataCount))
-            node.Metadata.Add(numbers[++position]);
+        var numbers = await GetNumbers();
+        var tree = GetTree(numbers);
+
+        Assert.Equal(33649, tree.Value());
+    }
+
+    private static Node GetTree(int[] numbers)
+    {
+        var idx = -1;
+        return ParseNode(numbers, ref idx);
+    }
+
+    private static Node ParseNode(int[] numbers, ref int idx)
+    {
+        var node = new Node(numbers[++idx], numbers[++idx]);
+        foreach (var _ in Enumerable.Range(0, node.Header.Children))
+            node.Children.Add(ParseNode(numbers, ref idx));
+        foreach (var _ in Enumerable.Range(0, node.Header.Metadata))
+            node.Metadata.Add(numbers[++idx]);
 
         return node;
     }
+
+    private static ValueTask<int[]> GetNumbers() =>
+        Util.GetInputAsStrings("day08-input.txt")
+            .Select(line => line.Split(' ').Select(int.Parse).ToArray())
+            .SingleAsync();
 }
