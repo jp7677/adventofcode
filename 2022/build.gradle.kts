942743454d563caa35d665ea07b984ae653441f1
Jens Peters <jp7677@gmail.com>
2026-03-28 11:34:58 +0100

    2022: Update dependencies

--- 2022/build.gradle.kts
+++ 2022/build.gradle.kts
@@ -1,28 +1,26 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 group = "me.jp7677"
 version = "0.1-SNAPSHOT"
 
 plugins {
     application
-    kotlin("jvm") version "2.2.21"
+    kotlin("jvm") version "2.3.20"
     id("com.adarshr.test-logger") version "4.0.0"
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
 }
 
 repositories { mavenCentral() }
 dependencies {
-    testImplementation("io.kotest:kotest-runner-junit5:6.0.5")
-    testImplementation("io.kotest:kotest-assertions-core:6.0.5")
+    testImplementation("io.kotest:kotest-runner-junit5:6.1.9")
+    testImplementation("io.kotest:kotest-assertions-core:6.1.9")
 }
 
-application { mainClass.set("MainKt") }
 testlogger { theme = com.adarshr.gradle.testlogger.theme.ThemeType.PLAIN }
+
+kotlin { jvmToolchain(25) }
+application { mainClass = "MainKt" }
 tasks {
-    withType<Jar> { manifest { attributes["Main-Class"] = "MainKt" } }
-    withType<KotlinCompile>().configureEach { compilerOptions { jvmTarget = JvmTarget.JVM_21 } }
-    withType<Test>().configureEach {
+    jar { manifest { attributes["Main-Class"] = "MainKt" } }
+    test {
         useJUnitPlatform()
         testLogging { showStandardStreams = true }
     }
