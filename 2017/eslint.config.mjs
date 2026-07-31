f55d704857e6cac628f2a25ebc8f119f94459198
Jens Peters <jp7677@gmail.com>
2026-08-01 01:05:38 +0200

    2017: Update eslint config

--- 2017/eslint.config.mjs
+++ 2017/eslint.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
-);
+export default defineConfig({
+  files: ["src/**/*"],
+  extends: [
+    eslint.configs.recommended,
+    tseslint.configs.strict,
+    tseslint.configs.stylistic,
+  ],
+});
