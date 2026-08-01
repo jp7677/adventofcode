a044a0d32a57b22c13f015759d81a782a0205b83
Jens Peters <jp7677@gmail.com>
2026-08-01 10:54:50 +0200

    2017: Integrate prettier into eslint

--- 2017/eslint.config.mjs
+++ 2017/eslint.config.mjs
@@ -3,12 +3,14 @@
 import eslint from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default defineConfig({
-  files: ["src/**/*"],
+  files: ["src/**/*", "eslint.config.mjs"],
   extends: [
     eslint.configs.recommended,
     tseslint.configs.strict,
     tseslint.configs.stylistic,
+    prettierRecommended,
   ],
 });
